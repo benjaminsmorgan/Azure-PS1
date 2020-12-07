0291c7515218b244add74de00349a116c26afaf7
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2020-12-07 13:01:40 -0500

    Create GetAzResource.ps1

---
+++ Resource Groups/GetAzResource.ps1
@@ -0,0 +1,63 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
+    Get-AzResource:             https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresource?view=azps-5.1.0
+} #>
+<# Required Functions Links: {
+    GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
+} #>
+<# Functions Description: {
+    GetAzResourceGroup:         Collects resource group object
+    GetAzResource:              Collects resources within a resource group
+} #>
+<# Variables: {
+    GetAzResourceGroup {
+        $RGObject:              Resource group object
+        $RGObjectInput:         Operator input for the resource group name
+        $RGList:                Variable used for printing all resource groups to screen if needed
+    }
+    GetAzResource {
+        $RGObject:              Resource group object
+        $RSObject:              Resource object
+        $RSObjectInput:         Operator input for the resource name
+        $RSList:                Variable used for printing all resources to screen if needed
+    }
+} #>
+<# Process Flow {
+    Function                        ->          [Call] GetAzResource                    ->          [Request] $RSObject
+        GetAzResource                   ->          [Return] Function                       ->          [Return] $RSObject
+    GetAzResource                   ->          [Call] GetAzResourceGroup               ->          [Request] $RGObject
+}#>
+function GetAzResource { # Function to get a resource, can pipe $RSObject to another function
+    Begin {
+        $ErrorActionPreference='silentlyContinue' # Disables Errors
+        $RSObject = $null # Sets $RSObject to $null from previous uses
+        if (!$RGObject) { # If statement if $RGObject is $null
+            $RGObject = GetAzResourceGroup # Calls function GetAzResourceGroup and assigns to $RGObject
+            if (!$RGObject) { # If statement if $RGObject is $null after calling GetAzResourceObject
+                Write-Host "GetAzResource function was terminated" # Message write to screen
+                Return # Returns to calling function
+            } # End if statement
+        } # End if statement
+        while (!$RSObject) { # Loop to continue getting a resource until the operator provided name matches an existing resource
+            $RSObjectInput = Read-Host "Resource name" # Operator input of the resource name
+            if ($RSObjectInput -eq 'exit') { # Operator input for exit
+                Write-Host "GetAzResource function was terminated"
+                Return # Returns to calling function
+            } # End if statement
+            $RSObject = Get-AzResource -Name $RSObjectInput -ResourceGroupName $RGObject.ResourceGroupName # Collection of the resource from the operator input
+            if (!$RSObject) { # Error reporting if input does not match an existing group
+                Write-Host "The name provided does not match an existing resource" # Error note
+                Write-Host "This is the list of available resources" # Error note
+                $RSList = Get-AzResource -ResourceGroupName $RGObject.ResourceGroupName # Collects all resource objects and assigns to a variable
+                Write-Host "" # Error reporting
+                Write-Host $RSList.Name -Separator `n # Write-host used so list is written to screen when function is used as $RGObject = GetAzResourceGroup
+                Write-Host "" # Error reporting
+            } # End of if statement
+            else { # Else for when $RSObject is assigned
+                Write-Host $RSObject.Name 'Has been assigned to "$RSObject"' # Writes the resource name to the screen before ending function
+            } # End of else statement
+        } # End of while statement
+        Return $RSObject  # Returns the value of $RSObject to a function that called it
+    } # End begin statement
+} # End function 
