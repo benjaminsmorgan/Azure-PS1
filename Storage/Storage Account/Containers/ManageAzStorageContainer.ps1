a0b867caf0a3d6f91432a4f37e242196dcb41d07
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-04 07:23:22 -0500

    Merged RemoveAzStorageContainer

--- Storage/Storage Account/Containers/ManageAzStorageContainer.ps1
+++ Storage/Storage Account/Containers/ManageAzStorageContainer.ps1
@@ -2,7 +2,7 @@
 <# Ref: { Mircosoft docs links
     New-AzStorageContainer:     https://docs.microsoft.com/en-us/powershell/module/az.storage/new-azstoragecontainer?view=azps-5.2.0
     Get-AzStorageContainer:     https://docs.microsoft.com/en-us/powershell/module/az.storage/get-azstoragecontainer?view=azps-5.2.0
-    Remove-AzStorageContainer:  TBD
+    Remove-AzStorageContainer:  https://docs.microsoft.com/en-us/powershell/module/az.storage/remove-azstoragecontainer?view=azps-5.2.0
     Get-AzStorageAccount:       https://docs.microsoft.com/en-us/powershell/module/az.storage/get-azstorageContainer?view=azps-5.2.0
     Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
     Get-AzResourceLock:         https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcelock?view=azps-5.0.0
@@ -11,7 +11,7 @@
 <# Required Functions Links: {
     NewAzStorageContainer:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/Containers/NewAzStorageContainer.ps1
     GetAzStorageContainer:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/Containers/GetAzStorageContainer.ps1
-    RemoveAzStorageContainer:   TBD
+    RemoveAzStorageContainer:   https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/Containers/RemoveAzStorageContainer.ps1
     GetAzStorageAccount:        https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Container/GetAzStorageContainer.ps1
     GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
     GetAzResourceLocksAll:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Locks/GetAzResourceLocksAll.ps1
@@ -21,7 +21,7 @@
     ManageAzStorageContainer:   Management function for storage containers
     NewAzStorageContainer:      Creates new storage container(s) in a storage account 
     GetAzStorageContainer:      Collects storage container in a storage account  
-    RemoveAzStorageContainer:   TBD
+    RemoveAzStorageContainer:   Removes existing storage container
     GetAzStorageAccount:        Collects the storage Container object
     GetAzResourceGroup:         Collects resource group object
     RemoveAzResourceLocks:      Removes locks
@@ -77,8 +77,31 @@
             } End GetAzStorageAccount
         } End GetAzStorageContainer
         RemoveAzStorageContainer{
-            $    
-        } End RemoveAzStorageContainer
+            :RemoveAzureStorageCon      Outer loop for managing function
+            $StorageConObject:          Storage container object
+            $OperatorConfirm:           Operator confirmation to remove the storage container
+            GetAzStorageContainer {
+                :GetAzureStorageContainer   Outer loop for managing function
+                :GetAzureStorageConName     Inner loop for getting the storage container
+                $StorageAccObject:          Storage account object    
+                $StorageConNameInput:       Operator input for the storage container name
+                $StorageConObject:          Storage container object
+                $StorageConList:            List of all containers in storage account
+                GetAzStorageAccount{
+                    :GetAzureStorageAccByName   Outer loop for managing funciton
+                    :GetAzureStorageAcc         Inner loop for getting the storage account
+                    $RGObject:                  Resource group object
+                    $StorageAccObjectInput:     Operator input for the name of the storage account
+                    $SAList:                    List of all storage accounts within $RGObject
+                    $StorageAccObject:          Storage account object    
+                    GetAzResourceGroup {
+                        $RGObject:                  Resource group object
+                        $RGObjectInput:             Operator input for the resource group name
+                        $RGList:                    Variable used for printing all resource groups to screen if needed
+                    } End GetAzResourceGroup
+                } End GetAzStorageAccount
+            } End GetAzStorageContainer
+        } End RemoveAzStorageContainer      
     } End ManageAzStorageContainer
 } #>
 <# Process Flow {
@@ -101,7 +124,27 @@
                     End GetAzStorageAccount 
                         Return GetAzStorageContainer > Send $StorageAccObject
                 End GetAzStorageContainer 
-                    Return ManageAzStorageContainer > Send $StorageConObject      
+                    Return ManageAzStorageContainer > Send $StorageConObject
+            Call RemoveAzStorageContainer > Get $null
+                Call GetAzStorageContainer > Get $StorageConObject
+                    Call GetAzStorageAccount > Get $StorageAccObject
+                        Call GetAzResourceGroup > Get $RGObject
+                        End GetAzResourceGroup
+                            Return GetAzStorageAccount > Send $RGObject
+                End RemoveAzStorageContainer      
+                    Return ManageAzStorageContainer > Send $null
+            End ManageAzStorageContainer > Send $null
+}#>
+<# Process Flow {
+    function
+        Call RemoveAzStorageContainer > Get $null
+            Call GetAzStorageContainer > Get $StorageConObject
+                Call GetAzStorageAccount > Get $StorageAccObject
+                    Call GetAzResourceGroup > Get $RGObject
+                    End GetAzResourceGroup
+                        Return GetAzStorageAccount > Send $RGObject
+                End RemoveAzStorageContainer 
+                    Return Function > Send $null
 }#>
 function ManageAzStorageContainer {
     Begin {
@@ -275,3 +318,37 @@ function GetAzStorageContainer { # Collects storage container in a storage accou
         Return # Returns to calling function with $null
     } # End Begin
 } # End function GetAzStorageContainer
+function RemoveAzStorageContainer { # Removes existing storage container
+    Begin {
+        :RemoveAzureStorageCon while ($true) { # Outer loop for function
+            <#if (!$StorageAccObject) { # If $StorageAccObject is $null
+                $StorageAccObject = GetAzStorageAccount ($RGObject) # Calls function and assigns to $var
+                    if (!$StorageAccObject) { # If $StorageAccObject is still $null after calling function
+                        Break RemoveAzureStorageCon # Breaks :RemoveAzureStorageCon
+                    } # End if (!$StorageAccObject)
+            } # End if (!$StorageAccObject) #>
+            if (!$StorageConObject) { # If $StorageConObject is $null
+                $StorageConObject = GetAzStorageContainer ($StorageAccObject) # Calls function and assigns to $var
+                    if (!$StorageConObject) { # If $StorageConObject is still $null after calling function
+                        Break RemoveAzureStorageCon # Breaks :RemoveAzureStorageCon
+                    } # End if (!$StorageAccObject)
+            } # End if (!$StorageAccObject)
+            Write-Host "***WARNING RESOURCELOCKS WILL NOT PROTECT THIS STORAGECONTAINER FROM BEING DELETED***"
+            $OperatorConfirm = Read-Host "Remove the following storage container" $StorageConObject.Name "in" $StorageAccObject.StorageAccountName # Operator confimation to remove the storage container
+                if (!($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes')) { # If Operator confirm is not (equal 'y' or 'yes')
+                    Break RemoveAzureStorageCon # Breaks RemoveAzureStorageCon
+                } # End if (!($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes'))
+                $StoreConName = $StorageConObject.Name
+                Try { # Try to execute Remove-AzStorageAccount
+                    Remove-AzStorageContainer -Context $StorageAccObject.Context -Name $StorageConObject.Name -Force -ErrorAction Stop # Removes storage container, -ErrorAction Stop used for catch statement
+                } # End Try
+                catch { # Try fails
+                    Write-Host "You may not have the permissions to remove this storage account" # Write message to screen
+                    Break RemoveAzureStorageCon # Breaks RemoveAzureStorageAcc
+                } # End Catch
+                Write-Host $StoreConName" has been removed" # Write message to screen
+                Return # Returns to calling function
+        } # End :RemoveAzureStorageCon while ($true)
+        Return # Returns to calling function
+    } # End Begin
+} # End function RemoveAzStorageContainer
