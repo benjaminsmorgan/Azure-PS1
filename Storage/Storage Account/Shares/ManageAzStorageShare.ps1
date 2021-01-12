59fe4209324c84b2bce525219aae5e44f609dccc
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-12 07:24:31 -0500

    Folder structure update

--- Storage/Storage Account/Shares/ManageAzStorageShare.ps1
+++ Storage/Storage Account/Shares/ManageAzStorageShare.ps1
@@ -2,6 +2,7 @@
 <# Ref: { Mircosoft docs links
     New-AzStorageShare:         https://docs.microsoft.com/en-us/powershell/module/az.storage/new-azstorageshare?view=azps-5.3.0
     Get-AzStorageShare:         https://docs.microsoft.com/en-us/powershell/module/az.storage/get-azstorageshare?view=azps-5.3.0
+    Remove-AzStorageShare:      https://docs.microsoft.com/en-us/powershell/module/az.storage/remove-azstorageshare?view=azps-5.3.0
     Get-AzStorageAccount:       https://docs.microsoft.com/en-us/powershell/module/az.storage/get-azstorageaccount?view=azps-5.2.0
     Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
 } #>
@@ -9,7 +10,7 @@
     NewAzStorageShare:          https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/File%20Shares/NewAzStorageShare.ps1
     GetAzStorageShare:          https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/File%20Shares/GetAzStorageShare.ps1
     GetAzStorageShareAll:       https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/File%20Shares/GetAzStorageShareAll.ps1
-    RemoveAzStorageShare:       TBD
+    RemoveAzStorageShare:       https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/File%20Shares/RemoveAzStorageShare.ps1
     ManageAzStorageShareItems:  TBD
     GetAzStorageAccObject:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/Storage%20Account/GetAzStorageAccount.ps1
     GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
@@ -19,7 +20,7 @@
     NewAzStorageShare:          Creates a new storage share
     GetAzStorageShare:          Gets a storage share
     GetAzStorageShareAll:       Lists all storage shares in a subscription
-    RemoveAzStorageShare:       Removes selected storage share
+    RemoveAzStorageShare:       Removes a storage share
     ManageAzStoreShareItems:    Management function for storage share items
     GetAzStorageAccount:        Collects the storage account object
     GetAzResourceGroup:         Collects resource group object
@@ -78,6 +79,32 @@
             $StorageAccObject:          All storage accounts in subscription
             $StorageShareObject:        All storage shares in a storage account
         } End GetAzStorageShareAll
+        RemoveAzStorageShare {
+            $StorageAccObject:          Storage account object
+            $StorageShareObject:        Storage share object
+            $OperatorConfirm:           Operator confirmation
+            GetAzStorageShare {               
+                :GetAzureStorageShare       Outer loop for managing function
+                :GetAzureStorageShareName   Inner loop for getting the named storage share
+                $StorageAccObject:          Storage account object
+                $ShareNameInput:            Operator input for the share name
+                $StorageShareObject:        Storage share object
+                $StorageShareList:          List of all storage shares in storage account
+                GetAzStorageAccount{
+                    :GetAzureStorageAccByName   Outer loop for managing funciton
+                    :GetAzureStorageAcc         Inner loop for getting the storage account
+                    $RGObject:                  Resource group object
+                    $StorageAccObjectInput:     Operator input for the name of the storage account
+                    $SAList:                    List of all storage accounts within $RGObject
+                    $StorageAccObject:          Storage account object    
+                    GetAzResourceGroup {
+                        $RGObject:                  Resource group object
+                        $RGObjectInput:             Operator input for the resource group name
+                        $RGList:                    Variable used for printing all resource groups to screen if needed
+                    } End GetAzResourceGroup
+                } End GetAzStorageAccount
+            } End GetAzStorageShare
+        } End RemoveAzStorageShare
     } End ManageAzStorageShare
 } #>
 <# Process Flow {
@@ -104,6 +131,17 @@
             Call GetAzStorageShareAll > Get $null
             End GetAzStorageShareAll 
                 Return Function > Send $null
+            Call RemoveAzStorageShare > Get $null
+                Call GetAzStorageShare > Get $StorageShareObject
+                    Call GetAzStorageAccount > Get $StorageAccObject
+                        Call GetAzResourceGroup > Get $RGObject
+                        End GetAzResourceGroup
+                            Return GetAzStorageAccount > Send $RGObject
+                    End GetAzStorageAccount
+                        Return NewAzStorageShare > Send $StorageAccObject
+                End GetAzStorageShare
+            End RemoveAzStorageShare      
+                Return ManageAzStorageShare > Send $null            
         End ManageAzStorageShare
             Return function > Send $null
 }#>
@@ -129,15 +167,15 @@ function ManageAzStorageShare { # Manages storage blob functions
             } # End elseif ($ManageAzStorageShare -eq '1')
             elseif ($ManageAzStorageShare -eq '2') { # Elseif statement for managing storage containers
                 Write-Host "Get Storage Share" # Write message to screen
-                $StorageShareObject = GetAzStorageShare ($StorageAccObject, $RGObject) # Calls function and assigns to $var 
+                $StorageShareObject, $StorageAccObject = GetAzStorageShare ($StorageAccObject, $RGObject) # Calls function and assigns to $var 
             } # End elseif ($ManageAzStorageShare -eq '2')
             elseif ($ManageAzStorageShare -eq '3') { # Elseif statement for managing Blobs
                 Write-Host "List All Storage Shares" # Write message to screen
                 GetAzStorageShareAll # Calls function 
             } # End elseif ($ManageAzStorageShare -eq '3')
             elseif ($ManageAzStorageShare -eq '4') { # Elseif statement for managing Storage shares
-                Write-Host "Remove Storage Blobs" # Write message to screen
-                RemoveAzStorageBlob ($StorageAccObject, $StorageAccObject, $StorageBlobObject) # Calls function and assigns to $var
+                Write-Host "Remove Storage Share" # Write message to screen
+                RemoveAzStorageShare ($StorageAccObject, $StorageShareObject) # Calls function and assigns to $var
             } # End elseif ($ManageAzStorageShare -eq '4')
             elseif ($ManageAzStorageShare -eq '0') { # Elseif statement for managing disks
                 if ($RGObject) { # If $var is not $null
@@ -230,26 +268,31 @@ Function GetAzStorageShare { # Gets a storage share
                     Break GetAzureStorageShare # Breaks :GetAzureStorageShare
                 } # End if (!$StorageAccObject) 
             } # End if (!$StorageAccObject) {
+            $StorageShareList = Get-AzStorageShare -Context $StorageAccObject.Context # Collects all shares in selected storage account
             :GetAzureStorageShareName while ($true) { # Inner loop for getting the named storage share
+                $StorageShareObject = $null # Clears from previous use
                 $ShareNameInput = Read-Host "Storage share name" # Operator input for the share name
                 if ($ShareNameInput -eq 'exit') { # If $ShareNameInput is 'exit'
                     Break GetAzureStorageShare # Breaks :GetAzureStorageShare
                 } # End if ($ShareNameInput -eq 'exit')
-                Try { # Try to get $StorageShareObject
-                    $StorageShareObject = Get-AzStorageShare -Context $StorageAccObject.Context -Name $ShareNameInput # Collects the Storage share and assigns to $StorageShareObject
-                } # End Try
-                Catch { # If Try fails
-                    $StorageShareObject = $null # Clears from previous use
+                if ($ShareNameInput -notin $StorageShareList.Name) {
                     Write-Host "The name provided does not match an existing share" # Write message to screen
                     Write-Host "Please select from the following" # Write message to screen
                     Write-Host "" # Write message to screen
-                    $StorageShareList = Get-AzStorageShare -Context $StorageAccObject.Context # Collects all shares in selected storage account
                     Write-Host $StorageShareList.Name -Separator `n # Writes $StorageShareList to screen
                     Write-Host "" # Write message to screen
-                } # End catch
-                if ($StorageShareObject) { # If $StorageShareObject is not $null
-                    Return $StorageShareObject # Returns $StorageShareObject to calling function
-                } # End if ($StorageShareObject) 
+                } # End if ($ShareNameInput -notin $StorageShareList)
+                else {
+                    Try { # Try to get $StorageShareObject
+                        $StorageShareObject = Get-AzStorageShare -Context $StorageAccObject.Context -Name $ShareNameInput # Collects the Storage share and assigns to $StorageShareObject
+                    } # End Try
+                    Catch { # If Try fails
+                        Write-Host "The share was not collected, you may not have the permissions" # Write message to screen
+                    } # End catch
+                    if ($StorageShareObject) { # If $StorageShareObject is not $null
+                        Return $StorageShareObject, $StorageAccObject # Returns $StorageShareObject to calling function
+                    } # End if ($StorageShareObject)
+                } # End else (if ($ShareNameInput -notin $StorageShareList)) 
             } # End :GetAzureStorageShareName while ($true)
         } # End :GetAzureStorageShare while ($true)
         Return # Returns nothing
@@ -276,6 +319,43 @@ Function GetAzStorageShareAll { # Lists all storage shares in a subscription
         Return # Returns nothing
     } # End Begin
 } # End Function GetAzStorageShareAll
+function RemoveAzStorageShare { # Removes a storage share
+    Begin {
+        if (!$StorageAccObject) { # If $StorageAccObject is $null
+            $StorageAccObject = GetAzStorageAccount # Call function and assign result to $var
+            if (!$StorageAccObject) { # If $StorageAccObject is $null
+                Return # Returns to calling function with $null
+            } # End if (!$StorageAccObject)
+        } # End if (!$StorageShareObject)
+        if (!$StorageShareObject) { # If $StorageShareObject is $null 
+            $StorageShareObject = GetAzStorageShare ($StorageAccObject) # Call function and assign result to $var
+            if (!$StorageShareObject) { # If $StorageShareObject is $null 
+                Return # Returns to calling function with $null
+            } # End if (!$StorageShareObject)
+        } # End if (!$StorageShareObject)
+        Write-Host "Remove the storage share:"$StorageShareObject.Name"in storage account:"$StorageAccObject.StorageAccountName # Write message to screen
+        $OperatorConfirm = Read-Host "[Y] or [N]" # Operator confirmation to remove storage share
+        if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes') { # If $OperatorConfirm is equal to 'y' or 'yes'
+            Try { # Try to do the following
+                Remove-AzStorageShare -Name $StorageShareObject.Name -Context $StorageAccObject.Context # Remove the selected share
+            } # End Try
+            Catch { # If try fails
+                Write-Host "" # Write message to screen
+                Write-Host "The storage share was not removed, you may not have the permissions to do so" # Write message to screen
+                Write-Host "" # Write message to screen
+                Return # Returns to calling function with $null
+            } # End Catch
+            Write-Host "" # Write message to screen
+            Write-Host "The storage share was removed" # Write message to screen
+            Write-Host "" # Write message to screen
+            Return # Returns to calling function with $null
+        } # End if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes')
+        else { # Returns to calling function with $null
+            Write-Host "No action taken, returning to menu" # Write message to screen
+            Return # Returns to calling function with $null
+        } # End else (if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes'))
+    } # End Begin
+} # End function RemoveAzStorageShare
 function GetAzResourceGroup { # Function to get a resource group, can pipe $RGObject to another function
     Begin {
         $ErrorActionPreference='silentlyContinue' # Disables Errors
