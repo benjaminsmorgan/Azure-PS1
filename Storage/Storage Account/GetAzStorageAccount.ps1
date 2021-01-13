034daf97cf9ef4c000a26f5c4a30747466efc246
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-13 08:40:40 -0500

    Overhaul of selecting storage account

--- Storage/Storage Account/GetAzStorageAccount.ps1
+++ Storage/Storage Account/GetAzStorageAccount.ps1
@@ -36,6 +36,7 @@
 }#>
 function GetAzStorageAccount { # Function to get a storage account, can pipe $StorageAccObject to another function
     Begin {
+        $ErrorActionPreference = 'silentlyContinue' # Disables errors
         :GetAzureStorageAccByName while ($true) { # Outer loop for function
             $ErrorActionPreference ='silentlyContinue' # Disables errors
             if (!$RGObject) { # If $RGObject is $null
@@ -44,25 +45,70 @@ function GetAzStorageAccount { # Function to get a storage account, can pipe $St
                     Break GetAzureStorageACCByName # Ends :GetAzureStorageAccByName
                 } # End if (!$RGObject) 
             } # End if (!$RGObject)
-            :GetAzureStorageAcc while ($true) { # Loop to continue getting a storage account until the operator provided name matches an existing account
-                $StorageAccObjectInput = Read-Host "Storage account name" # Operator input of the storage account name
-                if ($StorageAccObjectInput -eq 'exit') { # If $StorageAccObjectInput is 'exit
-                    Break GetAzureStorageAccByName # Break :GetAzureStorageAccByName
-                } # Endif ($StorageAccObjectInput -eq 'exit')
-                $StorageAccObject = Get-AzStorageAccount -ResourceGroupName $RGObject.ResourceGroupName -Name $StorageAccObjectInput # Collection of the storage account from the operator input
-                if (!$StorageAccObject) { # Error reporting if input does not match and existing account
-                    Write-Host "The name provided does not match an existing storage account" # Error reporting
-                    $SAList = Get-AzStorageAccount -ResourceGroupName $RGObject.ResourceGroupName # Collects all storage accounts within $RGObject and assigns to $SAList
-                    Write-Host "" # Error reporting
-                    Write-Host $SAList.Storageaccountname -Separator `n # Write-host used so list is written to screen when function is used as $StorageAccObject = GetAzStorageAccount
-                    Write-Host "" # Error reporting
-                } # End (!$StorageAccObject)
-                else { # Else for when $StorageAccObject is assigned
-                    Write-Host $StorageAccObject.StorageAccountName 'Has been assigned to "$StorageAccObject"' # Writes the storage account name to the screen before ending function
-                    Return $StorageAccObject # Returns $var to calling function
-                } #End else ((!$StorageAccObject))
-            } # End :GetAzureStorageAcc while ($true)
+            $StorageAccList = Get-AzStorageAccount -ResourceGroupName $RGObject.ResourceGroupName # Collects all storage accounts in $RGObject and assigns to $StorageAccList
+            if (!$StorageAccList) { # If $StorageAcclist returns empty
+                Write-Host "No storage accounts found" # Message write to screen
+                Break GetAzureStorageACCByName # Ends :GetAzureStorageAccByName
+            } # End if (!$StorageAccList)
+            $StorageAccListNumber = 1 # Sets the base value of $StorageAccListNumber
+            Write-Host "0. Exit" # Writes exit option to screen
+            foreach ($_ in $StorageAccList) { # For each item in $StorageAccList
+                Write-Host $StorageAccListNumber"." $_.StorageAccountName # Writes $StorageAccList to screen
+                $StorageAccListNumber = $StorageAccListNumber+1 # Adds 1 to $StorageAccListNumber
+            } # End foreach ($_ in $StorageAccList)
+            :GetAzureStorageAccount while ($true) {
+                $StorageAccListNumber = 1 # Sets the base value of $StorageAccListNumber
+                $StorageAccListSelect = Read-Host "Please enter the number of the storage account" # Operator input for the storage account selection
+                if ($StorageAccListSelect -eq '0') { # If $StorageAccList is '0'
+                    Break GetAzureStorageACCByName # Ends :GetAzureStorageAccByName
+                } # if ($StorageAccListSelect -eq '0')
+                foreach ($_ in $StorageAccList) { # For each item in $StorageAccList
+                    if ($StorageAccListSelect -eq $StorageAccListNumber) { # If the operator input matches the current $StorageAccListNumber
+                        $StorageAccObject = $_ # Assigns current item in $StorageAccList to $StorageAccObject
+                        Break GetAzureStorageAccount # Breaks :GetAzureStorageAccount
+                    } # End if ($StorageAccListSelect -eq $StorageAccListNumber)
+                    else { # If user input does not match the current $StorageAccListNumber
+                        $StorageAccListNumber = $StorageAccListNumber+1 # Adds 1 to $StorageAccListNumber
+                    } # End else (if ($StorageAccListSelect -eq $StorageAccListNumber))
+                } # End foreach ($_ in $StorageAccList)
+                Write-Host "That was not a valid entry" # Write message to screen
+            } # End :GetAzureStorageAccount while ($true) {
+            Return $StorageAccObject # Returns to calling function with $StorageAccObject
         } # End :GetAzureStorageAccByName while ($true)
         Return # Returns to calling function with $null
     } # End begin 
 } # End function GetAzStorageAccount
+function GetAzResourceGroup { # Function to get a resource group, can pipe $RGObject to another function
+    Begin {
+        $ErrorActionPreference = 'silentlyContinue' # Disables error reporting
+        $RGList = Get-AzResourceGroup # Gets all resource groups and assigns to $RGList
+        if (!$RGList) { # If $RGList returns empty
+            Write-Host "No resource groups found" # Message write to screen
+            Return # Returns to calling function with $null
+        } # End if (!$RGList)
+        $RGListNumber = 1 # Sets the base value of the list
+        Write-Host "0. Exit" # Adds exit option to beginning of list
+        foreach ($_ in $RGList) { # For each item in list
+            Write-Host $RGListNumber"." $_.ResourceGroupName # Writes the option number and resource group name
+            $RGListNumber = $RGListNumber+1 # Adds 1 to $RGListNumber
+        } # End foreach ($_ in $RGList)
+        :GetAzureResourceGroup while ($true) { # Loop for selecting the resource group object
+            $RGListNumber = 1 # Resets list number to 1
+            $RGListSelect = Read-Host "Please enter the number of the resource group" # Operator input for selecting which resource group
+            if ($RGListSelect -eq '0') { # If $RGListSelect is equal to 0
+                Return # Returns to calling function with $null
+            } # End if ($RGListSelect -eq '0')
+            foreach ($_ in $RGList) { # For each item in list
+                if ($RGListSelect -eq $RGListNumber) { # If the operator input matches the current $RGListNumber
+                    $RGObject = $_ # Currently selected item in $RGList is assigned to $RGObject
+                    Return $RGObject # Returns $RGObject to calling function
+                } # End if ($RGListSelect -eq $RGListNumber)
+                else { # If user input does not match the current $RGListNumber
+                    $RGListNumber = $RGListNumber+1 # Adds 1 to $RGListNumber
+                } # End else (if ($RGListSelect -eq $RGListNumber))
+            } # End foreach ($_ in $RGList)
+            Write-Host "That was not a valid selection, please try again" # Write message to screen
+        } # End :GetAzureResourceGroup while ($true)
+        Return $RGObject # Returns $RGObject to calling function
+    } # End of begin statement
+} # End of function
