ec484e6d6a49fbd1de9cc9fe350886c071c803d9
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-14 06:45:06 -0500

    Comment update

--- Storage/Storage Account/GetAzStorageAccount.ps1
+++ Storage/Storage Account/GetAzStorageAccount.ps1
@@ -11,19 +11,14 @@
     GetAzResourceGroup:         Collects resource group object
 } #>
 <# Variables: {
-    GetAzStorageAccount{
         :GetAzureStorageAccByName   Outer loop for managing funciton
-        :GetAzureStorageAcc         Inner loop for getting the storage account
+        :GetAzureStorageAccount     Inner loop for selecting the storage account
         $RGObject:                  Resource group object
-        $StorageAccObjectInput:     Operator input for the name of the storage account
-        $SAList:                    List of all storage accounts within $RGObject
+        $StorageAccList:            List of all storage accounts in $RGObject
+        $StorageAccListNumber:      Used for listing and selecting a storage account
+        $StorageAccListSelect:      Operator input to select the storage account
         $StorageAccObject:          Storage account object    
-        GetAzResourceGroup {
-            $RGObject:                  Resource group object
-            $RGObjectInput:             Operator input for the resource group name
-            $RGList:                    Variable used for printing all resource groups to screen if needed
-        } End GetAzResourceGroup
-    } End GetAzStorageAccount
+        GetAzResourceGroup{}        Gets $RGObject
 } #>
 <# Process Flow {
     function
@@ -78,37 +73,3 @@ function GetAzStorageAccount { # Function to get a storage account, can pipe $St
         Return # Returns to calling function with $null
     } # End begin 
 } # End function GetAzStorageAccount
-function GetAzResourceGroup { # Function to get a resource group, can pipe $RGObject to another function
-    Begin {
-        $ErrorActionPreference = 'silentlyContinue' # Disables error reporting
-        $RGList = Get-AzResourceGroup # Gets all resource groups and assigns to $RGList
-        if (!$RGList) { # If $RGList returns empty
-            Write-Host "No resource groups found" # Message write to screen
-            Return # Returns to calling function with $null
-        } # End if (!$RGList)
-        $RGListNumber = 1 # Sets the base value of the list
-        Write-Host "0. Exit" # Adds exit option to beginning of list
-        foreach ($_ in $RGList) { # For each item in list
-            Write-Host $RGListNumber"." $_.ResourceGroupName # Writes the option number and resource group name
-            $RGListNumber = $RGListNumber+1 # Adds 1 to $RGListNumber
-        } # End foreach ($_ in $RGList)
-        :GetAzureResourceGroup while ($true) { # Loop for selecting the resource group object
-            $RGListNumber = 1 # Resets list number to 1
-            $RGListSelect = Read-Host "Please enter the number of the resource group" # Operator input for selecting which resource group
-            if ($RGListSelect -eq '0') { # If $RGListSelect is equal to 0
-                Return # Returns to calling function with $null
-            } # End if ($RGListSelect -eq '0')
-            foreach ($_ in $RGList) { # For each item in list
-                if ($RGListSelect -eq $RGListNumber) { # If the operator input matches the current $RGListNumber
-                    $RGObject = $_ # Currently selected item in $RGList is assigned to $RGObject
-                    Return $RGObject # Returns $RGObject to calling function
-                } # End if ($RGListSelect -eq $RGListNumber)
-                else { # If user input does not match the current $RGListNumber
-                    $RGListNumber = $RGListNumber+1 # Adds 1 to $RGListNumber
-                } # End else (if ($RGListSelect -eq $RGListNumber))
-            } # End foreach ($_ in $RGList)
-            Write-Host "That was not a valid selection, please try again" # Write message to screen
-        } # End :GetAzureResourceGroup while ($true)
-        Return $RGObject # Returns $RGObject to calling function
-    } # End of begin statement
-} # End of function
