c0995fd17b99c8db473f0aa326587268f31662d7
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-15 07:19:17 -0500

    Create ListAzKeyVault.ps1

---
+++ Storage/KeyVault/ListAzKeyVault.ps1
@@ -0,0 +1,34 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzKeyVault:             https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvault?view=azps-5.3.0
+} #>
+<# Required Functions Links: {
+    None
+} #>
+<# Functions Description: {
+    ListAzKeyVault:             Lists all key vaults in subscription
+} #>
+<# Variables: {      
+        $KVList:                    List of all jey vaults in $RGObject
+} #>
+<# Process Flow {
+    function
+        Call ListAzKeyVault > Get $null
+        End ListAzKeyVault     
+            Return Function > Send $null
+}#>
+function ListAzKeyVault { # Lists all key vaults in subscription
+    Begin {
+        #$ErrorActionPreference = 'silentlyContinue' # Disables error reporting
+        $KVList = Get-AzKeyVault # Gets all key vaults and assigns to $KVList
+        if (!$KVList) { # If $KVList returns empty
+            Write-Host "No key vaults found" # Message write to screen
+        } # End if (!$KVList)
+        else { # If $KVList has a value
+            foreach ($_ in $KVList) { # For each item in $KVList
+                Write-Host "VaultName:" $_.VaultName "ResourceGroup:" $_.ResourceGroupName # Write message to screen 
+            } # End else 
+        } # End else (if (!$KVList))
+        Return # Returns to calling function with $null
+    } # End Begin
+} # End function ListAzKeyVault
