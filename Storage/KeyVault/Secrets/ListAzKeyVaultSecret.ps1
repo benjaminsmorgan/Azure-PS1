5d037f1bbd818a68dd2f2d491bc1f0661ca8b6ae
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-19 06:57:01 -0500

    Create ListAzKeyVaultSecret.ps1

---
+++ Storage/KeyVault/Secrets/ListAzKeyVaultSecret.ps1
@@ -0,0 +1,43 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzKeyVaultSecret:       https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvaultsecret?view=azps-5.1.0
+    Get-AzKeyVault:             https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvault?view=azps-5.1.0
+} #>
+<# Required Functions Links: {
+    None
+} #>
+<# Functions Description: {
+    ListAzKeyVaultSecret:       Lists all key vault secrets
+} #>
+<# Variables: {
+    $KVList:                    List of all key vaults in subscription
+    $KVSecretlist:              List of all secrets from current object in $KVList
+} #>
+<# Process Flow {
+    Function
+        Call ListAzKeyVaultSecret > Get $null
+        End ListAzKeyVaultSecret
+            Return Function > Send $null
+}#>
+function ListAzKeyVaultSecret { # Lists all key vault secrets
+    Begin {
+        $KVList = Get-AzKeyVault # Creates lists of all key vaults
+        foreach ($VaultName in $KVList) { # For each key vault in $KVList
+            Write-Host "-----------------------------------" # Write message to screen
+            $KVSecretlist = Get-AzKeyVaultSecret -VaultName $VaultName.VaultName # Creates a list of all keys in current $VaultName
+            Write-Host "Vault Name: " $VaultName.VaultName # Write message to screen
+            Write-Host "" # Write message to screen
+            foreach ($Name in $KVSecretlist) { # For each secret name in $KVSecretList
+                Write-Host "Secret Name:" $Name.Name # Write message to screen
+                Write-Host "Enabled:    " $Name.Enabled # Write message to screen
+                Write-Host "Created:    " $Name.Created # Write message to screen
+                Write-Host "Updated:    " $Name.Updated # Write message to screen
+                if ($Name.Expires) { # If Selected key has an existing expiration
+                    Write-Host "Expires:    " $Name.Expires # Write message to screen
+                } # End if ($Name.Expires)
+                Write-Host "" # Write message to screen
+            } # End foreach ($Name in $KVSecretlist)
+        } # End foreach ($VaultName in $KVList)
+        Write-Host "-----------------------------------" # Write message to screen
+    } # End Begin 
+} # End function ListAzKeyVaultSecret
