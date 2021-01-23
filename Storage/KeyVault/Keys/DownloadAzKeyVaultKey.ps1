ce0498beacc5f30414a1fabb2516d6357e511425
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-01-23 07:10:16 -0500

    Update DownloadAzKeyVaultKey.ps1

--- Storage/KeyVault/Keys/DownloadAzKeyVaultKey.ps1
+++ Storage/KeyVault/Keys/DownloadAzKeyVaultKey.ps1
@@ -1,3 +1,48 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzKeyVaultKey:          https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvaultKey?view=azps-5.1.0
+    Get-AzKeyVault:             https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvault?view=azps-5.1.0
+    Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
+} #>
+<# Required Functions Links: {
+    GetAzKeyVaultKey:           https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/KeyVault/Keys/GetAzKeyVaultKey.ps1
+        GetAzKeyVault:              https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/KeyVault/GetAzKeyVault.ps1
+            GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
+} #>
+<# Functions Description: {
+    DownloadAzKeyVaultKey:      Downloads a key vault key
+    GetAzKeyVaultKey:           Gets a key vault Key
+    GetAzKeyVault:              Gets key vault object
+    GetAzResourceGroup:         Gets resource group object
+} #>
+<# Variables: {
+    :DownloadAzureKVKey         Outer loop for managing function
+    :SetLocalDownloadPath       Inner loop for setting the download path
+    :SetLocalFileName           Inner loop for setting the key local name
+    $KeyVaultKeyObject          Key vault key being downloaded
+    $LocalPath                  Operator input for the download path
+    $LocalFileName              Operator input for the key name
+    $OperatorConfirm            Operator confirmation of file name
+    $Fullpath                   Creates the full download path and name $var
+    GetAzKeyVaultKey{}          Gets $KeyVaultKeyObject
+        GetAzKeyVault{}             Gets $KeyVaultObject
+            GetAzResourceGroup{}        Gets $RGObject      
+} #>
+<# Process Flow {
+    Function
+        Call DownloadKeyVaultKey > Get $null
+            Call GetAzKeyVaultKey > Get $KeyVaultKeyObject
+                Call GetAzKeyVault > Get $KeyVault
+                    Call GetAzResourceGroup > Get $RGObject
+                    End GetAzResourceGroup
+                        Return GetAzKeyVault > Send $RGObject
+                End GetAzKeyVault
+                    Return GetAzKeyVaultKey > Send $KeyVault
+            End GetAzKeyVaultKey
+                Return DownloadKeyVaultKey > Send $KeyVaultKeyObject
+        End DownloadKeyVaultKey
+            Return function > Send $null
+}#>
 function DownloadAzKeyVaultKey { # Downloads a selected key vault key
     Begin {
         $ErrorActionPreference = 'silentlyContinue' # Disables error reporting
@@ -48,122 +93,3 @@ function DownloadAzKeyVaultKey { # Downloads a selected key vault key
         Return # Returns to calling function with $null
     } # End Begin
 } # End function DownloadAzKeyVaultKey
-
-function GetAzKeyVaultKey { # Function to get a key vault Key
-    Begin {
-        $WarningPreference = "silentlyContinue" # Disables key vault warnings
-        $ErrorActionPreference = 'silentlyContinue' # Disables error reporting
-        :GetAzureKeyVaultKey while ($true) { # Outer loop for managing function
-            if (!$KeyVaultObject) { # If $KeyVaultObject is $null 
-                $KeyVaultObject = GetAzKeyVault # Calls function and assigns output to $var
-                if (!$KeyVaultObject) { # If $var is still $null
-                    Break GetAzureKeyVaultKey # Breaks :GetAzureKeyVaultKey    
-                } # End if (!$KeyVaultObject)
-            } # End if (!$KeyVaultObject)
-            $KVKeylist = Get-AzKeyVaultKey -VaultName $KeyVaultObject.VaultName # Creates list of all Keys in vault
-            if (!$KVKeylist) { # If $KVKeylist returns empty
-                Write-Host "No Keys found" # Message write to screen
-                Break GetAzureKeyVaultKey # Breaks :GetAzureKeyVaultKey
-            } # End if (!$KVKeylist)
-            $KVKeylistNumber = 1 # Sets the base value of the list
-            Write-Host "0. Exit" # Write message to screen
-            foreach ($_ in $KVKeylist) { # For each item in list
-                Write-Host $KVKeylistNumber"." $_.Name # Writes list to screen
-                $KVKeylistNumber = $KVKeylistNumber+1 # Adds 1 to $KVKeylistNumber
-            } # End foreach ($_ in $KVKeylist) 
-            $KVKeylistNumber = 1 # Resets list number to 1
-            $KVKeylistSelect = Read-Host "Enter the option number" # Operator input to select from list
-            if ($KVKeylistSelect -eq '0') { # If $KVKeyListSelect is 0
-                Break GetAzureKeyVaultKey # Breaks :GetAzureKeyVaultKey
-            } # End if ($KVKeylistSelect -eq '0')
-            :SelectAzureKeyVaultKey foreach ($_ in $KVKeylist) { # For each item in list
-                if ($KVKeylistSelect -eq $KVKeylistNumber) { # If the user input matches the current $KVKeylistNumber
-                    $KeyVaultKeyObject = Get-AzKeyVaultKey -VaultName $KeyVaultObject.VaultName -Name $_.Name # Collects the full $KeyVaultKeyObject
-                    Break SelectAzureKeyVaultKey # Breaks :SelectAzureKeyVaultKey
-                } # End if ($KVKeylistSelect -eq $KVKeylistNumber)
-                else { # If user input does not match the current $KVKeylistNumber
-                    $KVKeylistNumber = $KVKeylistNumber+1 # Adds 1 to $KVKeylistNumber
-                } # End else (if ($KVKeylistSelect -eq $KVKeylistNumber))
-            } # End :SelectAzureKeyVaultKey
-            Return $KeyVaultKeyObject # Returns $KeyVaultKeyObject to calling function
-        } # End :GetAzureKeyVaultKey while ($true) {
-        Return # Returns $null to calling function
-    } # End begin statement
-} # End GetAzKeyVaultKey
-function GetAzKeyVault { # Collects a key vault object
-    Begin {
-        $ErrorActionPreference = 'silentlyContinue' # Disables error reporting
-        :GetAzureKeyVault while ($true) { # Outer loop for managing function
-            if (!$RGObject) { # If $RGObject is empty
-                $RGObject = GetAzResourceGroup # Calls function and assigns output to $var
-                if (!$RGObject) { # If $RGObject is still empty after returning
-                    Break GetAzureKeyVault # Breaks :GetAzureKeyVault
-                } # End if (!$RGObject)
-            } # End if (!$RGObject)
-            $KVList = Get-AzKeyVault -ResourceGroupName $RGObject.ResourceGroupName # Gets all key vaults in resource group and assigns to $KVList
-            if (!$KVList) { # If $KVList returns empty
-                Write-Host "No key vaults found" # Message write to screen
-                Break GetAzureKeyVault # Breaks :GetAzureKeyVault
-            } # End if (!$KVList)
-            $KVListNumber = 1 # Sets the base value of the list
-            Write-Host "0. Exit" # Adds exit option to beginning of list
-            foreach ($_ in $KVList) { # For each item in list
-                Write-Host $KVListNumber"." $_.VaultName # Writes the option number and key vault name
-                $KVListNumber = $KVListNumber+1 # Adds 1 to $KVListNumber
-            } # End foreach ($_ in $KVList)
-            :SelectAzureKeyVault while ($true) { # Loop for selecting the key vault object
-                $KVListNumber = 1 # Resets list number to 1
-                $KVListSelect = Read-Host "Enter the option number" # Operator input for selecting which key vault
-                if ($KVListSelect -eq '0') { # If $KVListSelect is equal to 0
-                    Break GetAzureKeyVault # Breaks :GetAzureKeyVault
-                } # End if ($KVListSelect -eq '0')
-                foreach ($_ in $KVList) { # For each item in list
-                    if ($KVListSelect -eq $KVListNumber) { # If the operator input matches the current $KVListNumber
-                        $KeyVaultObject = Get-AzKeyVault -VaultName $_.VaultName # Currently selected item in $KVList is assigned to $KeyVaultObject
-                        Break SelectAzureKeyVault # Breaks :SelectAzureKeyVault
-                    } # End if ($KVListSelect -eq $KVListNumber)
-                    else { # If user input does not match the current $KVListNumber
-                        $KVListNumber = $KVListNumber+1 # Adds 1 to $KVListNumber
-                    } # End else (if ($KVListSelect -eq $KVListNumber))
-                } # End foreach ($_ in $KVList)
-                Write-Host "That was not a valid selection, please try again" # Write message to screen
-            } # End :SelectAzureKeyVault while ($true)
-            Return $KeyVaultObject # Returns $RGObject to calling function
-        } # End :GetAzureKeyVault while ($true)
-        Return # Returns to calling function with $null
-    } # End Begin
-} # End function GetAzKeyVault
-function GetAzResourceGroup { # Function to get a resource group, can pipe $RGObject to another function
-    Begin {
-        $ErrorActionPreference = 'silentlyContinue' # Disables error reporting
-        $RGList = Get-AzResourceGroup # Gets all resource groups and assigns to $RGList
-        if (!$RGList) { # If $RGList returns empty
-            Write-Host "No resource groups found" # Message write to screen
-            Return # Returns to calling function with $null
-        } # End if (!$RGList)
-        $RGListNumber = 1 # Sets the base value of the list
-        Write-Host "0. Exit" # Adds exit option to beginning of list
-        foreach ($_ in $RGList) { # For each item in list
-            Write-Host $RGListNumber"." $_.ResourceGroupName # Writes the option number and resource group name
-            $RGListNumber = $RGListNumber+1 # Adds 1 to $RGListNumber
-        } # End foreach ($_ in $RGList)
-        :GetAzureResourceGroup while ($true) { # Loop for selecting the resource group object
-            $RGListNumber = 1 # Resets list number to 1
-            $RGListSelect = Read-Host "Enter the option number" # Operator input for selecting which resource group
-            if ($RGListSelect -eq '0') { # If $RGListSelect is equal to 0
-                Return # Returns to calling function with $null
-            } # End if ($RGListSelect -eq '0')
-            foreach ($_ in $RGList) { # For each item in list
-                if ($RGListSelect -eq $RGListNumber) { # If the operator input matches the current $RGListNumber
-                    $RGObject = $_ # Currently selected item in $RGList is assigned to $RGObject
-                    Break GetAzureResourceGroup # Breaks :GetAzureResourceGroup
-                } # End if ($RGListSelect -eq $RGListNumber)
-                else { # If user input does not match the current $RGListNumber
-                    $RGListNumber = $RGListNumber+1 # Adds 1 to $RGListNumber
-                } # End else (if ($RGListSelect -eq $RGListNumber))
-            } # End foreach ($_ in $RGList)
-            Write-Host "That was not a valid selection, please try again" # Write message to screen
-        } # End :GetAzureResourceGroup while ($true)
-        Return $RGObject # Returns $RGObject to calling function
-    } # End of begin statement
-} # End of function
