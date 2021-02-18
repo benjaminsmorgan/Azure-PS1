fcc5c7e386970e97c9da3d5c6a8b4ee900208dcd
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-02-18 07:07:49 -0500

    Create NewAzPublicIpAddress.ps1

---
+++ Networking/Public IP/NewAzPublicIpAddress.ps1
@@ -0,0 +1,102 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    New-AzPublicIpAddress:      https://docs.microsoft.com/en-us/powershell/module/az.network/new-azpublicipaddress?view=azps-5.5.0
+    Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0    
+} #>
+<# Required Functions Links: {
+    GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
+} #>
+<# Functions Description: {
+    NewAzPublicIpAddress:       Creates a new public IP address
+    GetAzResourceGroup:         Gets a resource group
+} #>
+<# Variables: {      
+    :NewAzurePublicIP           Outer loop for managing function
+    :SetAzurePublicIPName       Inner loop for setting public IP name
+    :SetAzurePublicIPAlloc      Inner loop for setting public IP allocation type
+    $RGObject:                  Resource group object
+    $PublicIPNameObject:        Public IP name object
+    $OperatorConfirm:           Operator confirmation on $PublicIPObject
+    $PublicIPObject:            Created public IP address
+    GetAzResourceGroup{}        Gets $RGObject
+} #>
+<# Process Flow {
+    function
+        Call NewAzPublicIpAddress > Get $PublicIPObject
+            Call GetAzResourceGroup > Get RGObject
+            End GetAzResourceGroup
+                Return NewAzPublicIpAddress > Send RGObject
+        End NewAzPublicIpAddress
+            Return function > Send $PublicIPObject
+}#>
+function NewAzPublicIpAddress {                                                             # Creates a new public IP address
+    Begin {                                                                                 # Begin function
+        $ErrorActionPreference='silentlyContinue'                                           # Turns off error reporting
+        :NewAzurePublicIP while ($true) {                                                   # Outer loop for managing function
+            if (!$RGObject) {                                                               # If $RGObject is $null
+                $RGObject = GetAzResourceGroup                                              # Calls function and assigns output to $var
+                if (!$RGObject) {                                                           # If $RGObject is $null
+                    Break NewAzurePublicIP                                                  # Breaks :NewAzurePublicIP
+                }                                                                           # End if (!$RGObject)
+            }                                                                               # End if (!$RGObject)
+            :SetAzurePublicIPName while ($true) {                                           # Inner loop for setting the public IP name
+                Try {                                                                       # Try the following
+                    [ValidatePattern('^[a-z][a-z0-9-]{1,61}[a-z0-9]$')]$PublicIPNameObject `
+                    = [string](Read-Host "Enter the public IP name").ToLower()              # Operator input for the public IP name
+                }                                                                           # End try
+                catch {                                                                     # Error reporting for try statement
+                    Write-Host "! That enty was not valid"                                  # Write message to screen
+                    Write-Host "! Valid entries are 1-61 characters"                        # Write message to screen
+                    Write-Host "! The name must start with a letter"                        # Write message to screen
+                    Write-Host "! Special charaters are not allowed"                        # Write message to screen
+                    Write-Host ""                                                           # Write message to screen
+                }                                                                           # End catch
+                if ($PublicIPNameObject) {
+                    if ($PublicIPNameObject -eq 'exit') {                                   # IF $PublicIPNameObject equals 'exit'
+                        Break NewAzurePublicIP                                              # Breaks :NewAzurePublicIP
+                    }                                                                       # End if ($PublicIPNameObject -eq 'exit')
+                    Write-Host $PublicIPNameObject                                          # Write message to screen
+                    $OperatorConfirm = Read-Host "Use this name [Y] or [N]"                 # Operator confirmation of the name
+                    if ($OperatorConfirm -eq 'y') {                                         # If $OperatorConfirm equals 'y'
+                        Break SetAzurePublicIPName                                          # Breaks :SetAzurePublicIPName
+                    }                                                                       # End if ($OperatorConfirm -eq 'y')
+                    else {                                                                  # If $OperatorConfirm does not equal 'y'
+                        Remove-Variable PublicIPNameObject                                  # Removes $PublicIPNameObject
+                    }                                                                       # End else if ($OperatorConfirm -eq 'y')
+                }                                                                           # End if ($PublicIPNameObject) 
+            }                                                                               # End :SetAzurePublicIPName while ($true)
+            :SetAzurePublicIPAlloc while ($true) {                                          # Inner loop for setting the public IP allocation method
+                Write-Host '[0] Exit'                                                       # Write message to screen
+                Write-Host '[1] Dynamic'                                                    # Write message to screen
+                Write-Host '[2] Static'                                                     # Write message to screen
+                $PublicIPAllocationObject = Read-Host "[0], [1], or [2]"                    # Operator input for the allocation method
+                if ($PublicIPAllocationObject -eq '0') {                                    # If $PublicIPAllocationObject equals '0'
+                    Break NewAzurePublicIP                                                  # Breaks :NewAzurePublicIP
+                }                                                                           # End if ($PublicIPAllocationObject -eq '0')
+                elseif ($PublicIPAllocationObject -eq '1') {                                # Elseif $PublicIPAllocationObject equals 1
+                    $PublicIPAllocationObject = 'dynamic'                                   # Reassigns value of $PublicIPAllocationObject
+                    Break SetAzurePublicIPAlloc                                             # Breaks :SetAzurePublicIPAlloc    
+                }                                                                           # End elseif ($PublicIPAllocationObject -eq '1')
+                elseif ($PublicIPAllocationObject -eq '2') {                                # Elseif $PublicIPAllocationObject equals 2
+                    $PublicIPAllocationObject = 'static'                                    # Reassigns value of $PublicIPAllocationObject
+                    Break SetAzurePublicIPAlloc                                             # Breaks :SetAzurePublicIPAlloc
+                }                                                                           # End elseif ($PublicIPAllocationObject -eq '2')
+                else {                                                                      # All other inputs
+                    Write-Host "That was not a valid option"                                # Write message to screen
+                }                                                                           # End else (($PublicIPAllocationObject -eq '0'))
+            }                                                                               # End :SetAzurePublicIPAlloc while ($true)
+            $PublicIPObject = New-AzPublicIpAddress -Name $PublicIPNameObject `
+                -ResourceGroupName $RGObject.ResourceGroupName -Location `
+                $RGObject.Location -AllocationMethod $PublicIPAllocationObject `
+                -DomainNameLabel $PublicIPNameObject -Force                                 # Creates the new public IP address
+            if ($PublicIPObject) {                                                          # If $PublicIPObject is not $null
+                Return $PublicIPObject                                                      # Returns to calling function with $PublicIPObject
+            }                                                                               # End if ($PublicIPObject)
+            else {                                                                          # If $PublicIPObject is $null 
+                Write-Host "An error has occured"                                           # Write message to screen
+                Break NewAzurePublicIP                                                      # Breaks :NewAzurePublicIP
+            }                                                                               # End else (if ($PublicIPObject))
+        }                                                                                   # End :NewAzurePublicIP while ($true)
+        Return                                                                              # Returns to calling function with # null
+    }                                                                                       # End Begin
+}                                                                                           # End function NewAzPublicIpAddress
