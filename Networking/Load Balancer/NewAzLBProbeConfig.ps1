804cea3766f525a0a1abfd31819ab5fa6a87dc46
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-02-22 06:57:19 -0500

    Create NewAzLBProbeConfig.ps1

---
+++ Networking/Load Balancer/NewAzLBProbeConfig.ps1
@@ -0,0 +1,119 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    New-AzLoadBalancerProbeConfig:  https://docs.microsoft.com/en-us/powershell/module/az.network/new-azloadbalancerbackendaddresspoolconfig?view=azps-5.5.0
+      
+} #>
+<# Required Functions Links: {
+    None
+} #>
+<# Functions Description: {
+    NewAzLBProbeConfig:         Creates a load balancer back end IP configuration
+} #>
+<# Variables: {      
+    :NewAzureLBProbeConfig      Outer loop for managing function
+    :SetAzureProbeName          Inner loop for setting the probe name
+    :SetAzureProbeProtocol      Inner loop for setting the probe type
+    :SetAzureProbeInterval      Inner loop for setting the probe interval
+    :SetAzureProbeCount         Inner loop for setting the probe count
+    $ProbeNameObject:           Probe name object
+    $ProbeTypeObject:           Probe type object
+    $ProbeProtocol:             Array containing probe port and protocol
+    $ProbeProtocolInput:        Loads items into the $ProbeProtocol array
+    $ProbeIntervalObject:       Probe interval object
+    $ProbeCountObject:          Probe count object
+    $OperatorConfirm:           Operator confirmation of inputs
+    $HealthProbeObject:         Health probe configuation
+} #>
+<# Process Flow {
+    function
+        Call NewAzLBProbeConfig > Get $HealthProbeObject    
+        End NewAzLBProbeConfig
+            Return function > Send $HealthProbeObject
+}#>
+function NewAzLBProbeConfig {                                                               # Function to sett up load balancer health probes
+    Begin {
+        :NewAzureLBProbeConfig while ($true) {
+            :SetAzureProbeName while ($true) {                                              # Inner loop for setting the probe name
+                $ProbeNameObject = Read-Host "Probe Name"                                   # Operator input for the probe name
+                if ($ProbeNameObject -eq 'exit') {                                          # If $ProbeNameObject equals $null
+                    Break NewAzureLBProbeConfig                                             # Breaks :NewAzureLBProbeConfig
+                }                                                                           # End if ($ProbeNameObject -eq 'exit')
+                Write-Host $ProbeNameObject                                                 # Writes message to screen
+                $OperatorConfirm = Read-Host "Use as the probe name? [Y] or [N]"            # Operator confirmation of the probe name
+                if ($OperatorConfirm -eq 'y') {                                             # If $OperatorConfirm equals 'y'
+                    Break SetAzureProbeName                                                 # Breaks :SetAzureProbeName
+                }                                                                           # End if ($OperatorConfirm -eq 'y')
+            }                                                                               # End :SetAzureProbeName while ($true)
+            :SetAzureProbeProtocol while ($true) {                                          # Inner loop for setting the probe type
+                Write-Host '[0] Exit'                                                       # Write message to screen
+                Write-Host '[1] Http (80)'                                                  # Write message to screen
+                Write-host '[2] Https(443)'                                                 # Write message to screen
+                $ProbeTypeObject = Read-Host '[0], [1], or [2]'                             # Operator input for the probe type object
+                if ($ProbeTypeObject -eq '0') {                                             # If $ProbeTypeObject equals 0
+                    Break NewAzureLBProbeConfig                                             # Breaks :NewAzureLBProbeConfig 
+                }                                                                           # End if ($ProbeTypeObject -eq '0')
+                elseif ($ProbeTypeObject -eq '1') {                                         # Elseif $ProbeTypeObject equals 1
+                    [System.Collections.ArrayList]$ProbeProtocol = @()                      # Creates array for list to be loaded into
+                    $ProbeProtocolInput = [PSCustomObject]@{'Protocol' = 'http';`
+                        'port' = '80'}                                                      # Creates the item to loaded into array
+                    $ProbeProtocol.Add($ProbeProtocolInput) | Out-Null                      # Loads item into array, out-null removes write to screen         
+                    Break SetAzureProbeProtocol                                             # Breaks :SetAzureProbeProtocol 
+                }                                                                           # End elseif ($ProbeTypeObject -eq '1')
+                elseif ($ProbeTypeObject -eq '2') {                                         # Elseif $ProbeTypeObject equals 1
+                    [System.Collections.ArrayList]$ProbeProtocol = @()                      # Creates array for list to be loaded into
+                    $ProbeProtocolInput = [PSCustomObject]@{'Protocol' = 'https';`
+                        'port' = '443'}                                                     # Creates the item to loaded into array
+                    $ProbeProtocol.Add($ProbeProtocolInput) | Out-Null                      # Loads item into array, out-null removes write to screen         
+                    Break SetAzureProbeProtocol                                             # Breaks :SetAzureProbeProtocol 
+                }                                                                           # End elseif ($ProbeTypeObject -eq '2')
+                else {                                                                      # All other inputs 
+                    Write-Host "That was not a valid option"                                # Write message to screen
+                }                                                                           # End else 
+            }                                                                               # End :SetAzureProbeProtocol while ($true)
+            :SetAzureProbeInterval while ($true) {                                          # Inner loop for setting the probe interval time
+                Try {                                                                       # Try the following
+                    [int]$ProbeIntervalObject = Read-Host "Probe interval in seconds"       # Operator input for the probe interval
+                }                                                                           # End try
+                Catch {}                                                                    # If try fails
+                if ($ProbeIntervalObject -le 0) {                                           # If $ProbeIntervalObject is less than or equal to 0
+                    Write-Host "Please enter a number"                                      # Write message to screen
+                }                                                                           # End if ($ProbeIntervalObject -le 0) 
+                elseif ($ProbeIntervalObject -ge 1) {                                       # If $ProbeIntervalObject is greater than or equal to `
+                    $OperatorConfirm = Read-Host 'Probe interval will be set at' `
+                    $ProbeIntervalObject 'seconds [Y] or [N]'                               # Operator confirmation of the probe interval
+                    if ($OperatorConfirm -eq 'y') {                                         # If $OperatorConfrim equals 'y'
+                        Break SetAzureProbeInterval                                         # Breaks :SetAzureProbeInterval        
+                    }                                                                       # End if ($OperatorConfirm -eq 'y')
+                }                                                                           # End elseif ($ProbeIntervalObject -ge 1)
+            }                                                                               # End :SetAzureProbeInterval while ($true)
+            :SetAzureProbeCount while ($true) {                                             # Inner loop for setting the probe count
+                Try {                                                                       # Try the following
+                    [int]$ProbeCountObject = Read-Host "Probe count"                        # Operator input for the probe count
+                }                                                                           # End try
+                Catch {}                                                                    # If try fails
+                if ($ProbeCountObject -le 0) {                                              # If $ProbeCountObject is less than or equal to 0
+                    Write-Host "Please enter a number"                                      # Write message to screen
+                }                                                                           # End if ($ProbeCountObject -le 0) 
+                elseif ($ProbeCountObject -ge 1) {                                          # If $ProbeCountObject is greater than or equal to `
+                    $OperatorConfirm = Read-Host 'Probe count will be set at' `
+                    $ProbeCountObject  '[Y] or [N]'                                         # Operator confirmation of the probe count
+                    if ($OperatorConfirm -eq 'y') {                                         # If $OperatorConfrim equals 'y'
+                        Break SetAzureProbeCount                                            # Breaks :SetAzureProbeCount        
+                    }                                                                       # End if ($OperatorConfirm -eq 'y')
+                }                                                                           # End elseif ($ProbeCountObject -ge 1)
+            }                                                                               # End :SetAzureProbeCount while ($true)
+            $HealthProbeObject = New-AzLoadBalancerProbeConfig -Name $ProbeNameObject `
+                -RequestPath healthcheck.aspx -Protocol $ProbeProtocol.Protocol `
+                -Port $ProbeProtocol.Port -IntervalInSeconds $ProbeIntervalObject `
+                -ProbeCount $ProbeCountObject                                               # Creates the Health probe object
+            if ($HealthProbeObject) {                                                       # If $HealthProbeObject has a value
+                Return $HealthProbeObject                                                   # Returns $HealthProbeObject to calling function
+            }                                                                               # End if ($HealthProbeObject)
+            else {                                                                          # If $HealthProbeObject does not have a value
+                Write-Host "An error has occured while setting the probe"                   # Write message to screen
+                Break NewAzureLBProbeConfig                                                 # Breaks :NewAzureLBProbeConfig
+            }                                                                               # End else (if ($HealthProbeObject))
+        }                                                                                   # End :NewAzureLBProbeConfig while ($true)
+        Return                                                                              # Returns to calling function with $null
+    }                                                                                       # End begin
+}                                                                                           # End function NewAzLBProbeConfig
