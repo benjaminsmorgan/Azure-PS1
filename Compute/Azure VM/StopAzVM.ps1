c46b7aec10286e83ef0451ab77a343ad62e183ab
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-03-14 12:18:48 -0400

    Comment change

--- Compute/Azure VM/StopAzVM.ps1
+++ Compute/Azure VM/StopAzVM.ps1
@@ -25,24 +25,25 @@
         End StopAzVM
             Return function > Send $null
 }#>
-function StopAzVM { # Function to deallocate a VM
-    Begin {
-        :StopAzureVM while ($true) { # Outer loop for managing function
-            if (!$VMObject) { # If $VMObject is $null
-                $VMObject = GetAzVM # Calls function and assigns output to $var
-                if (!$VMObject) { # If $VMObject is $null
-                    Break StopAzureVM # Breaks :StopAzureVM
-                } # End if (!$VMVM)
-            } # End if (!$VMVM)
-            $OperatorConfirm = Read-Host "Shutdown"$VMObject.Name "[Y] or [N]"
-            if (!($OperatorConfirm -eq 'y')) { # If OperatorConfirm does not equal 'y'
-                Write-Host "No action taken" # Write message to screen
-                Break StopAzureVM # Breaks :StopAzureVM
-            } # End if (!($OperatorConfirm -eq 'y'))
-            Write-Host "Attempting to shut off" $VMObject.Name # Write message to screen
-            Stop-AzVM -Name $VMObject.Name -ResourceGroup $VMObject.ResourceGroupName -force # Stops the selected VM
-            Break StopAzureVM # Breaks :StopAzureVM
-        } # End :StopAzureVM while ($true)
-        Return # Returns to calling function with $null
-    } # End Begin
-} # End function StopAzVM
+function StopAzVM {                                                                         # Function to deallocate a VM
+    Begin {                                                                                 # Begin function
+        :StopAzureVM while ($true) {                                                        # Outer loop for managing function
+            if (!$VMObject) {                                                               # If $VMObject is $null
+                $VMObject = GetAzVM                                                         # Calls function and assigns output to $var
+                if (!$VMObject) {                                                           # If $VMObject is $null
+                    Break StopAzureVM                                                       # Breaks :StopAzureVM
+                }                                                                           # End if (!$VMObject)
+            }                                                                               # End if (!$VMObject)
+            $OperatorConfirm = Read-Host "Shutdown"$VMObject.Name "[Y] or [N]"              # Operator input to shutdown the VM
+            if (!($OperatorConfirm -eq 'y')) {                                              # If OperatorConfirm does not equal 'y'
+                Write-Host "No action taken"                                                # Write message to screen
+                Break StopAzureVM                                                           # Breaks :StopAzureVM
+            }                                                                               # End if (!($OperatorConfirm -eq 'y'))
+            Write-Host "Attempting to shut off" $VMObject.Name                              # Write message to screen
+            Stop-AzVM -Name $VMObject.Name -ResourceGroup $VMObject.ResourceGroupName `
+                -force                                                                      # Stops the selected VM
+            Break StopAzureVM                                                               # Breaks :StopAzureVM
+        }                                                                                   # End :StopAzureVM while ($true)
+        Return                                                                              # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function StopAzVM
