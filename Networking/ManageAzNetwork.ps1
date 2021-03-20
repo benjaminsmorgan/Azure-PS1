fd7de58cbfaafe327c2168413103a3793245d8ba
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-03-20 09:59:22 -0400

    Update ManageAzNetwork.ps1

--- Networking/ManageAzNetwork.ps1
+++ Networking/ManageAzNetwork.ps1
@@ -3,6 +3,9 @@
     New-AzVirtualNetwork:       https://docs.microsoft.com/en-us/powershell/module/az.network/new-azvirtualnetwork?view=azps-5.4.0
     Get-AzVirtualNetwork:       https://docs.microsoft.com/en-us/powershell/module/az.network/get-azvirtualnetwork?view=azps-5.4.0
     Remove-AzVirtualNetwork:    https://docs.microsoft.com/en-us/powershell/module/az.network/remove-azvirtualnetwork?view=azps-5.4.0
+    Add-AzVirtualNetworkSubnetConfig: https://docs.microsoft.com/en-us/powershell/module/az.network/add-azvirtualnetworksubnetconfig?view=azps-5.4.0
+    Get-AzVirtualNetworkSubnetConfig: https://docs.microsoft.com/en-us/powershell/module/az.network/get-azvirtualnetworksubnetconfig?view=azps-5.4.0
+    Remove-AzVirtualNetworkSubnetConfig: https://docs.microsoft.com/en-us/powershell/module/az.network/remove-azvirtualnetworksubnetconfig?view=azps-5.6.0
     Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
     Get-AzLocation:             https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azlocation?view=azps-5.4.0
 } #>
@@ -15,11 +18,11 @@
         GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
         GetAzLocation:              https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzLocation.ps1
         AddAzVNetSubnetConfig:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/SubNet/AddAzVNetSubnetConfig.ps1
-    ManageAzVNetSubnetConfig:   TBD
-        AddAzVNetSubnetConfig:      TBD    
-        ListAzVNetSubnetConfig:     TBD
-        GetAzVNetSubnetConfig:      TBD
-        RemoveAzVNetSubnetConfig:   TBD
+    ManageAzVNetSubnetConfig:   https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/SubNet/ManageAzVNetSubnetConfig.ps1
+        AddAzVNetSubnetConfig:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/SubNet/AddAzVNetSubnetConfig.ps1
+        GetAzVNetSubnetConfig:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/SubNet/GetAzVNetSubnetConfig.ps1
+        RemoveAzVNetSubnetConfig:   https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/SubNet/RemoveAzVNetSubnetConfig.ps1
+        GetAzVirtualNetwork:        https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/VNet/GetAzVirtualNetwork.ps1
 } #>
 <# Functions Description: {
     ManageAzNetwork:            Management function for azure networking
@@ -31,11 +34,10 @@
         NewAzSubnet:                Creates a new subnet in an Azure virtual network
         GetAzResourceGroup:         Gets a resource group object
         GetAzLocation:              Gets an Azure location
-    ManageAzVNetSubnetConfig:   Management function for azure subnets
-        AddAzVNetSubnetConfig:      TBD    
-        ListAzVNetSubnetConfig:     TBD
-        GetAzVNetSubnetConfig:      TBD
-        RemoveAzVNetSubnetConfig:   TBD
+    ManageAzVNetSubnetConfig:   Management function for subnets    
+        AddAzVNetSubnetConfig:      Adds an azure virtual network subnet
+        GetAzVNetSubnetConfig:      Gets an azure virtual network subnet
+        RemoveAzVNetSubnetConfig:   Removes an azure virtual network subnet
 } #>
 <# Variables: {
     :ManageAzureNetwork         Outer loop for managing function
@@ -51,6 +53,12 @@
         GetAzVirtualNetwork{}       Gets $VNetObject
         RemoveAzVirtualNetwork{}   Removes $VnetObject
             GetAzVirtualNetwork{}       Gets $VNetObject
+    ManageAzVNetSubnetConfig{}      Gets $SubnetObject
+        AddAzVNetSubnetConfig{}     Creates $SubnetObject
+            GetAzVirtualNetwork{}       Gets $VnetObject
+        GetAzVNetSubnetConfig{}     Gets $SubnetObject, $VnetObject
+        RemoveAzVNetSubnetConfig{}  Removes $SubnetObject
+            GetAzVNetSubnetConfig{}     Gets $SubnetObject, $VnetObject
 } #>
 <# Process Flow {
     Function
@@ -82,6 +90,24 @@
                     Return ManageAzVirtualNetwork > Send $null
             End ManageAzVirtualNetwork
                 Return ManageAzNetwork > Send $VNetObject
+            Call ManageAzVnetSubnetConfig > Get $SubnetObject
+                Call AddAzVNetSubnetConfig > Get $SubnetObject
+                    Call GetAzVirtualNetwork > Get $VNetObject
+                    End GetAzVirtualNetwork
+                        Return AddAzVNetSubnetConfig > Send $VNetObject
+                End AddAzVNetSubnetConfig
+                    Return ManageAzVnetSubnetConfig > Send $SubnetObject
+                Call GetAzVNetSubnetConfig > Get $SubnetObject
+                End GetAzVNetSubnetConfig
+                    Return ManageAzVnetSubnetConfig > Send $SubnetObject
+                Call RemoveAzVNetSubnetConfig > Get $null
+                    Call GetAzVNetSubnetConfig > Get $SubnetObject
+                    End GetAzVNetSubnetConfig
+                        Return RemoveAzVNetSubnetConfig > Send $SubnetObject
+                End RemoveAzVNetSubnetConfig
+                    Return ManageAzVnetSubnetConfig > Send $null
+            End ManageAzVnetSubnetConfig
+                Return ManageAzNetwork > Send $SubnetObject 
         End ManageAzNetwork
             Return function > Send $Null
 }#>
@@ -483,6 +509,38 @@ function GetAzVNetSubnetConfig {                                                
         Return                                                                              # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function GetAzVNetSubnetConfig
+function RemoveAzVNetSubnetConfig {                                                         # Function to remove a subnet
+    Begin {                                                                                 # Begin function
+        :RemoveAzureSubnet while ($true) {                                                  # Outer loop for managing function
+            if (!$SubnetObject) {                                                           # If $SubnetObject is $null
+                $SubnetObject, $VNetObject = GetAzVNetSubnetConfig                          # Call function and assign output to $var
+                if (!$SubnetObject) {                                                       # If $SubnetObject is $null
+                    Break RemoveAzureSubnet                                                 # Breaks :RemoveAzureSubnet
+                }                                                                           # End if (!$SubnetObject)
+            }                                                                               # End if (!$SubnetObject)
+            Write-Host 'Remove the subnet'$SubnetObject.Name                                # Write message to screen
+            $OperatorConfirm = Read-Host '[Y] or [N]'                                       # Operator confirmation for removing the subnet
+            if ($OperatorConfirm -eq 'y') {                                                 # If $OperatorConfirm equals 'y'
+                Try {                                                                       # Try the following
+                    Remove-AzVirtualNetworkSubnetConfig -Name $SubnetObject.Name `
+                        -VirtualNetwork $VNetObject | Set-AzVirtualNetwork `
+                        -ErrorAction 'Stop'                                                 # Removes the subnet
+                }                                                                           # End try
+                catch {                                                                     # If try fails
+                    write-Host 'An error has occured'                                       # Write message to screen
+                    Write-Host 'No changes have been made'                                  # Write message to screen
+                    Break RemoveAzureSubnet                                                 # Breaks :RemoveAzureSubnet
+                }                                                                           # End catch
+                Write-Host 'The subnet has been removed'                                    # Write message to screen
+                Break RemoveAzureSubnet                                                     # Breaks :RemoveAzureSubnet
+            }                                                                               # End if ($OperatorConfirm -eq 'y') 
+            else {                                                                          # If $OperatorConfirm does not equal 'y'
+                Break RemoveAzureSubnet                                                     # Breaks :RemoveAzureSubnet
+            }                                                                               # End else (if ($OperatorConfirm -eq 'y'))
+        }                                                                                   # End :RemoveAzureSubnet while ($true)
+        Return                                                                              # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function RemoveAzVNetSubnetConfig
 # Additional functions required for ManageAzVirtualNetwork
 function GetAzResourceGroup {                                                               # Function to get a resource group, can pipe $RGObject to another function
     Begin {                                                                                 # Begin function
