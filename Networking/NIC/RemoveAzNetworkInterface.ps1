201e800e2148396992dd3d4f58c5fca665ded76e
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-03-23 07:39:17 -0400

    Create RemoveAzNetworkInterface.ps1

---
+++ Networking/NIC/RemoveAzNetworkInterface.ps1
@@ -0,0 +1,61 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Remove-AzNetworkInterface:  https://docs.microsoft.com/en-us/powershell/module/az.network/remove-aznetworkinterface?view=azps-5.6.0
+    Get-AzNetworkInterface:     https://docs.microsoft.com/en-us/powershell/module/az.network/get-aznetworkinterface?view=azps-5.4.0
+} #>
+<# Required Functions Links: {
+    GetAzNetworkInterface:      https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/NIC/GetAzNetworkInterface.ps1
+} #>
+<# Functions Description: {
+    RemoveAzNetworkInterface:   Removes a network interface   
+    GetAzNetworkInterface:      Gets an existing network interface
+} #>
+<# Variables: {
+    :RemoveAzureNic             Outer loop for managing function
+    $NicObject:                 Network interface object
+    $CallingFunction:           Name of the current function
+    $OperatorConfirm:           Operator confirmation for removing the nic
+    GetAzNetworkInterface{}     Get $NicObject
+} #>
+<# Process Flow {
+    Function
+        Call RemoveAzNetworkInterface > Get $null
+            Call GetAzNetworkInterface > Get $NicObject
+            End GetAzNetworkInterface
+                Return RemoveAzNetworkInterface > Send $NicObject
+        End RemoveAzNetworkInterface
+            Return function > Send $null
+}#>
+function RemoveAzNetworkInterface {                                                         # Function to remove a network interface
+    Begin {                                                                                 # Begin function
+        :RemoveAzureNic while ($true) {                                                     # Outer loop for managing function
+            if (!$NicObject) {                                                              # If $NicObject does not have a value
+                $CallingFunction = 'RemoveAzNetworkInterface'                               # Sets $CallingFunction
+                $NicObject = GetAzNetworkInterface ($CallingFunction)                       # Calls function and assigns output to $var
+                if (!$NicObject) {                                                          # If $NicObject does not have a value
+                    Break RemoveAzureNic                                                    # Breaks :RemoveAzureNic
+                }                                                                           # End if (!$NicObject)
+            }                                                                               # End if (!$NicObject)
+            Write-Host 'Remove the network interface named:'$NicObject.name                 # Write message to screen
+            Write-Host 'from the resource group:'$NicObject.ResourceGroupName               # Write message to screen
+            $OperatorConfirm = Read-Host '[Y] or [N]'                                       # Operator confirmation to remove the Nic
+            if ($OperatorConfirm -eq 'y') {                                                 # If $OperatorConfirm equals 'y;
+                Try {                                                                       # Try the following
+                    Remove-AzNetworkInterface -Name $NicObject.Name -ResourceGroupName `
+                        $NicObject.ResourceGroupName -Force -ErrorAction 'Stop'             # Removes the selected NIC
+                }                                                                           # End try
+                Catch {                                                                     # If try fails
+                    Write-Host 'An error has occured'                                       # Write message to screen
+                    Write-Host 'No changes have been made'                                  # Write message to screen
+                    Break RemoveAzureNic                                                    # Breaks :RemoveAzureNic
+                }                                                                           # End catch
+                Write-Host 'The selected Nic has been removed'                              # Write message to screen
+                Break RemoveAzureNic                                                        # Breaks :RemoveAzureNic
+            }                                                                               # End if ($OperatorConfirm -eq 'y')
+            else {                                                                          # All other inputs for $OperatorConfirm
+                Break RemoveAzureNic                                                        # Breaks :RemoveAzureNic
+            }                                                                               # End else (If ($OperatorConfirm -eq 'y'))
+        }                                                                                   # End :RemoveAzureNic while ($true)
+        Return                                                                              # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function RemoveAzNetworkInterface
