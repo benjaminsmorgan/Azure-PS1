529392c9164e7d8eba6682fe899ede8029ba9fc2
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-04-05 07:46:10 -0400

    Update RemoveAzResourceGroup.ps1

--- Resource Groups/Resource Groups/RemoveAzResourceGroup.ps1
+++ Resource Groups/Resource Groups/RemoveAzResourceGroup.ps1
@@ -18,9 +18,9 @@
     RemoveAzResourceLocks:      Removes all items in $Locks
 } #>
 <# Variables: {
-    :ManageAzureRG              Outer loop for managing function
+    :RemoveAzResourceGroup      Outer loop for managing function
     $RGObject:                  Resource group object
-
+    $CallingFunction:           Name of this function1
     GetAzResourceGroup{}        Gets $RGObject
     GetAzRGLocksAll{}           Gets $Locks
     RemoveAzResourceLocks{}     Gets $LocksRemoved
@@ -64,17 +64,18 @@ function RemoveAzResourceGroup {                                                
             $ErrorActionPreference='silentlyContinue'                                       # Disables Errors
             if (!$RGObject) {                                                               # If $RGObject does not have a value
                 $CallingFunction = 'RemoveAzResourceGroup'                                  # Creates $CallingFunction
-                $RGObject = GetAzResourceGoup ($CallingFunction)                            # Calls function and assigns output to $var
+                $RGObject = GetAzResourceGroup ($CallingFunction)                           # Calls function and assigns output to $var
                 if (!$RGObject) {                                                           # If $RGObject does not have a value
                     Break RemoveAzureRGObject                                               # Breaks :RemoveAzureRGObject
                 }                                                                           # End if (!$RGObject) | Inner
             }                                                                               # End if (!$RGObject) | Outer
-            Write-Host "|////////////////////////////WARNING\\\\\\\\\\\\\\\\\\\\\\\\\\\\|"  # Write message to screen
+            Clear-Host                                                                      # Clears screen
+            Write-Host "////////////////////////////WARNING\\\\\\\\\\\\\\\\\\\\\\\\\\\\"    # Write message to screen
             Write-Host `
-                "|"$RGObject.ResourceGroupName "will be deleted, this cannot be undone"     # Write message to screen
-            Write-Host "| All resource locks will be removed automatically if confirmed |"  # Write message to screen
-            Write-Host "| All resources within the resource group will also be deleted  |"  # Write message to screen
-            Write-Host "|\\\\\\\\\\\\\\\\\\\\\\\\\\\\WARNING////////////////////////////|"  # Write message to screen
+                ""$RGObject.ResourceGroupName "will be deleted, this cannot be undone"      # Write message to screen
+            Write-Host " All resource locks will be removed automatically if confirmed "    # Write message to screen
+            Write-Host " All resources within the resource group will also be deleted  "    # Write message to screen
+            Write-Host "\\\\\\\\\\\\\\\\\\\\\\\\\\\\WARNING////////////////////////////"    # Write message to screen
             $OperatorConfirm = Read-Host '[Y] or [N]'                                       # Operator input on confirming deletion of the resource group
             $RGObjectName = $RGObject.ResourceGroupName                                     # Collects the name of the resource group for later use
             if ($OperatorConfirm -eq "Y") {                                                 # If $OperatorConfirm equals 'y'
@@ -96,7 +97,7 @@ function RemoveAzResourceGroup {                                                
             Write-Host $RGObject.ResourceGroupName"is being removed, this may take a while" # Message write to screen
             Try {                                                                           # Try the following
                 Remove-AzResourceGroup -Name $RGObject.ResourceGroupName -Force `
-                    -ErrorAction 'Stop'                                                     # Removes the resource group
+                    -ErrorAction 'Stop' | Out-Null                                          # Removes the resource group
             }                                                                               # End Try
             Catch {                                                                         # If try fails
                 Write-Host 'An error has occured'                                           # Write message to screen
@@ -107,6 +108,8 @@ function RemoveAzResourceGroup {                                                
             Write-Host $RGObjectName'has been removed'                                      # Write message to screen
             Break RemoveAzureRGObject                                                       # Break RemoveAzureRGObject
         }                                                                                   # End :RemoveAzureRGObject while ($True)
+        Start-Sleep(5)                                                                      # Pauses actions for 5 seconds
+        Clear-Host                                                                          # Clears screen
         Return                                                                              # Returns to calling function with $null
     }                                                                                       # End begin statement
 }                                                                                           # End function RemoveAzResourceGroup
