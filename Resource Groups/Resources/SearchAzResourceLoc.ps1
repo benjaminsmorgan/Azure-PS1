8daabd19e52bd92feb0035252d3a4b4aeb1de987
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-04-09 07:31:22 -0400

    Update SearchAzResourceLoc.ps1

--- Resource Groups/Resources/SearchAzResourceLoc.ps1
+++ Resource Groups/Resources/SearchAzResourceLoc.ps1
@@ -4,28 +4,30 @@
     Get-AzLocation:             https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azlocation?view=azps-5.2.0
 } #>
 <# Required Functions Links: {
-    None
+    GetAzLocation:              https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/Locations/GetAzLocation.ps1
 } #>    
 <# Function Description: {
     SearchAzResourceLoc:        Searchs for resource using location matches on a resource
+    GetAzLocation:              Gets an Azure location
 } #>
 <# Variables: {
-    SearchAzResourceLoc {
-        :SearchAzureRSByLoc     Outer loop for function
-        :SearchAzureRSLoc       Inner loop for finding resource by location
-        :SetLocation            Inner loop for setting $Location
-        :GetAzureRSObject       Inner loop for collecting the resource object
-        $ValidLocation:         List of all valid Azure locations
-        $Location:              Operator input for the resource location
-        $RSObject:              Resource object
-        $RSObjectInput:         Operator input for the resource name 
-        $RGObjectInput:         Operator input for the resource group name 
-        $ForEachCount:          Number used in foreach statement for each found resource
-    } End SearchAzResourceLoc
+    :SearchAzureRSByLoc         Outer loop for managing function
+    :SelectAzureResource        Inner loop for selecting a resource
+    $CallingFunction:           Name of this function
+    $LocationObject:            Azure location object
+    $RSObject:                  Resource object
+    $ListNumber:                $var used for number in $ListArray
+    $ListArray:                 Array used to list and select the resource
+    $ListInput:                 $var used to load info into $ListArray
+    $Number:                    Current item .Number
+    $RSSelect:                  Operator input to select item from $ListArray
 } #>
 <# Process Flow {
     Function               
         Call SearchAzResourceLoc  > Get $RSObject
+            Call GetAzLocation > Get $LocationObject
+            End GetAzLocation
+                Return SearchAzResourceLoc > Send $LocationObject
         End SearchAzResourceLoc
             Return Function > Send $RSObject
 }#>
@@ -94,51 +96,3 @@ function SearchAzResourceLoc {                                                  
         Return                                                                              # Returns to calling function with $null
     }                                                                                       # End begin
 }                                                                                           # End function SearchAzResourceLoc
-function GetAzLocation {                                                                    # Function to get azure location
-    Begin {                                                                                 # Begin function
-        :GetAzureLocation while ($true) {                                                   # Outer loop for managing function
-            $ListObject = Get-AzLocation                                                    # Gets a list of all Azure locations
-            $ListNumber = 1                                                                 # $Var for selecting the location
-            [System.Collections.ArrayList]$ListArray = @()                                  # Creates the list array
-            foreach ($_ in $ListObject) {                                                   # For each $_ in $ListObject
-                $ListInput = [PSCustomObject]@{'Number' = $ListNumber; 'Location' `
-                = $_.DisplayName}                                                           # Creates the item to load into array
-                $ListArray.Add($ListInput) | Out-Null                                       # Loads item into array, out-null removes write to screen
-                $ListNumber = $ListNumber + 1                                               # Increments $ListNumber by 1
-            }                                                                               # End foreach ($_ in $ListObject)
-            Write-Host "[ 0 ] Exit"                                                         # Write message to screen
-            foreach ($_ in $ListArray) {                                                    # For each $_ in $ListArray
-                Write-Host '['$_.Number']' $_.Location                                      # Writes number and location to screen
-            }                                                                               # End foreach ($_ in $ListArray)
-            :SelectAzureLocation while ($true) {                                            # Inner loop for selecting location from list
-                if ($CallingFunction) {                                                     # If $CallingFunction exists
-                    Write-Host "You are selecting the location for"$CallingFunction         # Write message to screen
-                }                                                                           # End if ($CallingFunction)
-                $LocationSelect = Read-Host "Please enter [#] of the location"              # Operator input for the selection
-                if ($LocationSelect -eq '0') {                                              # If $LocationSelect is 0
-                    Break GetAzureLocation                                                  # Breaks :GetAzureLocation
-                }                                                                           # End if ($LocationSelect -eq '0')
-                elseif ($LocationSelect -in $ListArray.Number) {                            # If $LocationSelect in $ListArray.Number
-                    $LocationSelect = $ListArray | Where-Object {$_.Number -eq `
-                        $LocationSelect}                                                    # LocationSelect is equal to $ListArray where $LocationSelect equals $ListArray.Number
-                    Try {                                                                   # Try the following
-                        $LocationObject = Get-AzLocation | Where-Object {$_.DisplayName `
-                            -eq $LocationSelect.Location} -ErrorAction 'Stop'               # Pulls the full $LocationObject
-                    }                                                                       # End try
-                    catch {                                                                 # If try fails
-                        Write-Host 'An error has occured'                                   # Write message to screen
-                        Write-Host 'Please try again later'                                 # Write message to screen
-                        Break GetAzureLocation                                              # Breaks :GetAzureLocation 
-                    }                                                                       # End catch
-                    Clear-Host                                                              # Clears screen
-                    Return $LocationObject                                                  # Returns $LocationObject to calling function
-                }                                                                           # End elseif ($LocationSelect -in $ListArray.Number) 
-                else {                                                                      # All other inputs for $LocationSelect
-                    Write-Host "That was not a valid selection"                             # Write message to screen   
-                }                                                                           # End else (if ($LocationSelect -eq '0'))
-            }                                                                               # End :SelectAzureLocation while ($true)
-        }                                                                                   # End :GetAzureLocation while ($true)
-        Clear-Host                                                                          # Clears screen
-        Return                                                                              # Returns with $null 
-    }                                                                                       # End Begin
-}                                                                                           # End function GetAzLocation
