122b944ac9ac79c74314dc1e803452067ebcd578
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-04-12 07:14:31 -0400

    Removed calling function

--- Resource Groups/Locks/RemoveAzResourceLocks.ps1
+++ Resource Groups/Locks/RemoveAzResourceLocks.ps1
@@ -1,16 +1,12 @@
 # Benjamin Morgan benjamin.s.morgan@outlook.com 
 <# Ref: { Mircosoft docs links
     Remove-AzResourceLock:      https://docs.microsoft.com/en-us/powershell/module/az.resources/remove-azresourcelock?view=azps-5.0.0
-    Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
-    Get-AzResource:             https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresource?view=azps-5.1.0
-    Get-AzResourceLock:         https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcelock?view=azps-5.0.0
 } #>
 <# Required Functions Links: {
-    GetAzResourceLocks:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/Locks/GetAzResourceLocks.ps1
+    None
 } #>
 <# Functions Description: {
     RemoveAzResourceLocks:      Removes locks
-    GetAzResourceLocks:         Management function to get different sets of locks
 } #>
 <# Variables: {
     :RemoveAzureLocks           Outer loop for managing function
@@ -18,14 +14,10 @@
     $OperatorConfirm:           Operator confirmation that the lock found was the intended target
     $Temp:                      Holds the response from Remove-AzResourceLock
     $LocksRemoved:              Confirmation that all selected locks were removed
-    GetAzResourceLocks{}        Gets $Locks
 } #>
 <# Process Flow {
     Function
         Call RemoveAzResourceLocks > Get $LocksRemoved
-            Call GetAzResourceLocks > Get $Locks
-            End GetAzResourceLocks
-                Return RemoveAzResourceLocks > Send $Locks
         End RemoveAzResourceLocks
             Return function > Send $LocksRemoved
 }#>
@@ -33,11 +25,10 @@ function RemoveAzResourceLocks {                                                
     Begin {                                                                                 # Begin function
         :RemoveAzureLocks while ($true) {                                                   # Outer loop for managing function
             if (!$Locks) {                                                                  # If $Locks is $null
-                $Locks = GetAzResourceLocks                                                 # Calls GetAzResourceLocks and assigns to $Locks
-                if(!$Locks) {                                                               # If $Locks is $null
-                    Break RemoveAzureLocks                                                  # Breaks :RemoveAzureLocks
-                }                                                                           # End if(!$Locks) | Inner
-            }                                                                               # End if(!$Locks) | Outer
+                Write-Host 'No locks were passed to this function'                          # Write message to screen
+                Start-Sleep(5)                                                              # Pauses all actions for 5 seconds
+                Break RemoveAzureLocks                                                      # Breaks :RemoveAzureLocks
+            }                                                                               # End if(!$Locks) 
             foreach ($_ in $Locks) {                                                        # For each item in $Locks
                 Write-Host $_.Name                                                          # Write message to screen
             }                                                                               # End foreach ($_ in $Locks)
@@ -76,6 +67,7 @@ function RemoveAzResourceLocks {                                                
             }                                                                               # End if ($OperatorConfirm -eq 'Y') 
             else {                                                                          # Else if $Operatorconfirm is not equal to 'y'
                 Write-Host "No changes made"                                                # Message write to screen
+                Start-Sleep(5)                                                              # Pauses all actions for 5 seconds
                 $LocksRemoved = 'n'                                                         # Sets $LocksRemoved to 'n'
                 Return $LocksRemoved                                                        # Returns to calling function with $LocksRemoved
             }                                                                               # End else (if ($OperatorConfirm -eq 'Y') )
