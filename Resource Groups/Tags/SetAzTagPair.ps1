8bf9f9987a558e7bf3e53314a48fe0c1f24451f2
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-04-14 07:38:17 -0400

    Updated to new standard

--- Resource Groups/Tags/SetAzTagPair.ps1
+++ Resource Groups/Tags/SetAzTagPair.ps1
@@ -6,47 +6,76 @@
     None
 } #>
 <# Functions Description: {
-    SetAzTagPair:               Create a tag for multiple uses
+    SetAzTagPair:               Create a tag array for another function
 } #>
-<# SetAzTagPair { 
+<# Variables: { 
     :SetAzureTagPair            Outer loop for function
     :SetAzureTagName            Inner loop for setting the tag name
     :SetAzureTagValue           Inner loop for setting the tag value
+    $CallingFunction            Name of the function that called this function
     $TagNameInput:              Operator input for the tag name
     $TagValueInput:             Operator input for the tag value 
-    $OperatorConfirm:           Operator confirmation on tag name and value 
-}  End SetAzTagPair     
-#>
+    $TagArray:                  Build array of tag name and value
+    $ArrayInput:                Loads $TagNameInput and $TagValue input into $TagArray
+    $OpConfirm:                 Operator confirmation on tag name and value 
+} #>
 <# Process Flow {
     Function
-        Call SetAzTagPair > Get $TagNameInput, $TagValueInput
-            Return function > Send $TagNameInput, $TagValueInput
+        Call SetAzTagPair > Get $TagArray
+        End SetAzTagPair
+            Return SetAzTagPair > Send $TagArray
 #>
-function SetAzTagPair { # Function for setting the tag name and value pair
-    Begin {
-        :SetAzureTagPair while ($true) { # :SetAzureTagPairLoop
-            :SetAzureTagName while ($true) { # :SetAzureTagNameLoop
-                $TagNameInput = Read-Host "Tag Name" # Operator input for the tag name
-                if ($TagNameInput -eq 'exit') { # If $TagNameInput equals 'exit'
-                    Break SetAzureTagPair # Breaks :SetAzureTagPair loop
-                } # End if ($TagNameInput -eq 'exit')
-                $OperatorConfirm = Read-Host $TagNameInput "is correct" # Operator confirmation that the name entered is correct
-                if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes') { # If $OperatorConfirm is equal to 'y' or 'yes'
-                    Break SetAzureTagName # Breaks :setAzureTagName
-                } # End if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes')
-            } # End :SetAzureTagName while ($true)
-            :SetAzureTagValue while ($true) { # :SetAzureTagValue loop
-                $TagValueInput = Read-Host "Tag Value" # Operator input for the tag value
-                if ($TagValueInput -eq 'exit') { # If $TagValueInput equals 'exit'
-                    Break SetAzureTagPair # Breaks :SetAzureTagPair loop
-                } # End if ($TagVlaueInput -eq 'exit')
-                $OperatorConfirm = Read-Host $TagValueInput "is correct" # Operator confirmation that the value entered is correct
-                if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes') { # If $OperatorConfirm is equal to 'y' or 'yes'
-                    Break SetAzureTagValue # Breaks :SetAzureTagName
-                } # End if ($OperatorConfirm -eq 'y' -or $OperatorConfirm -eq 'yes')
-            } # End :SetAzureTagValue while ($true)
-            Return $TagNameInput, $TagValueInput # Returns $TagNameInput and $TagValueInput to the calling function
-        } # End :SetAzureTagPair while ($true)
-        Return # Retruns to calling function with $null
-    } # End begin
-} # End function SetAzTagPair
+function SetAzTagPair {                                                                     # Function for setting the tag name and value pair
+    Begin {                                                                                 # Being function
+        [System.Collections.ArrayList]$TagArray = @()                                       # Creates the blank tag array
+        :SetAzureTagPair while ($true) {                                                    # Outer loop for managing function
+            if ($CallingFunction) {                                                         # If $CallingFunction has a value
+                Write-host 'You are setting the tag for'$CallingFunction                    # Write message to screen
+            }                                                                               # End if ($CallingFunction)
+            :SetAzureTagName while ($true) {                                                # Inner loop for setting the tag name
+                $TagNameInput = Read-Host "Tag Name"                                        # Operator input for the tag name
+                if ($TagNameInput -eq 'exit') {                                             # If $TagNameInput equals 'exit'
+                    Break SetAzureTagPair                                                   # Breaks :SetAzureTagPair
+                }                                                                           # End if ($TagNameInput -eq 'exit')
+                Write-Host 'Use'$TagNameInput'as the tag name'                              # Write message to screen
+                $OpConfirm = Read-Host '[Y] or [N]'                                         # Operator confirmation that the name entered is correct
+                if ($OpConfirm -eq 'y') {                                                   # If $OpConfirm is equal to 'y'
+                    Clear-Host                                                              # Clears the screen
+                    Break SetAzureTagName                                                   # Breaks :SetAzureTagName
+                }                                                                           # End if ($OpConfirm -eq 'y')
+            }                                                                               # End :SetAzureTagName while ($true)
+            :SetAzureTagValue while ($true) {                                               # Inner loop for setting the tag value
+                $TagValueInput = Read-Host "Tag Value"                                      # Operator input for the tag value
+                if ($TagValueInput -eq 'exit') {                                            # If $TagValueInput equals 'exit'
+                    Break SetAzureTagPair                                                   # Breaks :SetAzureTagPair
+                }                                                                           # End if ($TagVlaueInput -eq 'exit')
+                if (!$TagValueInput) {                                                      # If $TagValueInput is $null
+                    Write-Host 'Use a blank tag value'                                      # Write message to screen
+                }                                                                           # End if (!$TagValueInput)
+                else {                                                                      # If $TagValueInput has a value
+                    Write-Host 'Use'$TagNameInput'as the tag name'                          # Write message to screen
+                }                                                                           # End else (if (!$TagValueInput))
+                $OpConfirm = Read-Host '[Y] or [N]'                                         # Operator confirmation that the name entered is correct
+                if ($OpConfirm -eq 'y') {                                                   # If $OpConfirm is equal to 'y'
+                    Clear-Host                                                              # Clears the screen
+                    Break SetAzureTagValue                                                  # Breaks :SetAzureTagName
+                }                                                                           # End if ($OpConfirm -eq 'y')
+            }                                                                               # End :SetAzureTagValue while ($true)
+            $ArrayInput = [PSCustomObject]@{'Name'=$TagNameInput;'Value'=$TagValueInput}    # Creates the item to loaded into array
+            $TagArray.Add($ArrayInput) | Out-Null                                           # Loads item into array, out-null removes write to screen
+            Write-Host 'Add another pair'                                                   # Write message to screen
+            $OpConfirm = Read-Host '[Y] or [N]'                                             # Operator confirmation to add another tag
+            if ($OpConfirm -eq 'n') {                                                       # If $OpConfirm equals 'n'
+                Break SetAzureTagPair                                                       # Breaks :SetAzureTagPair
+            }                                                                               # End if ($OpConfirm -eq 'n')
+            Clear-Host                                                                      # Clear the screen 
+        }                                                                                   # End :SetAzureTagPair while ($true)
+        Clear-Host                                                                          # Clear the screen
+        if ($TagArray) {                                                                    # If $TagArray has a value
+            Return $TagArray                                                                # Returns to calling function with $var
+        }                                                                                   # End if ($TagArray)
+        else {                                                                              # If $TagArray does not have a value
+            Return                                                                          # Returns to calling function with $null
+        }                                                                                   # End else (if ($TagArray))
+    }                                                                                       # End begin
+}                                                                                           # End function SetAzTagPair
