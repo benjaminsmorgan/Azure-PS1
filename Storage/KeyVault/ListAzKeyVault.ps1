8836cceb50c30632688519151b8fd81edbebaf82
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-05-13 07:31:31 -0400

    Updated to new standard

--- Storage/KeyVault/ListAzKeyVault.ps1
+++ Storage/KeyVault/ListAzKeyVault.ps1
@@ -9,7 +9,10 @@
     ListAzKeyVault:             Lists all key vaults in subscription
 } #>
 <# Variables: {      
-        $KVList:                List of all jey vaults in $RGObject
+    :ListAzureKeyVault          Outer loop for managing function
+    $ObjectList:                List of all key vaults
+    $ObjectArray:               Array containing the key vault items
+    $ObjectInput:               $Var used to load items into $ObjectArray
 } #>
 <# Process Flow {
     function
@@ -17,18 +20,27 @@
         End ListAzKeyVault     
             Return Function > Send $null
 }#>
-function ListAzKeyVault { # Lists all key vaults in subscription
-    Begin {
-        #$ErrorActionPreference = 'silentlyContinue' # Disables error reporting
-        $KVList = Get-AzKeyVault # Gets all key vaults and assigns to $KVList
-        if (!$KVList) { # If $KVList returns empty
-            Write-Host "No key vaults found" # Message write to screen
-        } # End if (!$KVList)
-        else { # If $KVList has a value
-            foreach ($_ in $KVList) { # For each item in $KVList
-                Write-Host "VaultName:" $_.VaultName "ResourceGroup:" $_.ResourceGroupName # Write message to screen 
-            } # End else 
-        } # End else (if (!$KVList))
-        Return # Returns to calling function with $null
-    } # End Begin
-} # End function ListAzKeyVault
+function ListAzKeyVault {                                                                   # Function to list a key vault objects
+    Begin {                                                                                 # Begin function
+        :ListAzureKeyVault while ($true) {                                                  # Outer loop for managing function
+            $ObjectList = Get-AzKeyVault                                                    # Gets all key vaults and assigns to $ObjectList
+            [System.Collections.ArrayList]$ObjectArray = @()                                # Creates the object list array
+            foreach ($_ in $ObjectList) {                                                   # For each $_ in $ObjectListList
+                $ObjectInput = [PSCustomObject]@{'Name'=$_.VaultName; `
+                'RG'=$_.ResourceGroupName;'Loc' = $_.Location}                              # Creates the item to loaded into array
+                $ObjectArray.Add($ObjectInput) | Out-Null                                   # Loads item into array, out-null removes write to screen
+                $ObjectNumber = $ObjectNumber + 1                                           # Increments $ObjectNumber by 1
+            }                                                                               # End foreach ($_ in $ObjectList)
+            foreach ($_ in $ObjectArray) {                                                  # For each $_ in $ObjectArray
+                Write-Host 'Vault name:'$_.Name                                             # Write message to screen
+                Write-Host 'RG Name:   '$_.RG                                               # Write message to screen
+                Write-Host 'Location:  '$_.Loc                                              # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+            }                                                                               # End foreach ($_ in $ObjectArray)
+            Pause                                                                           # Pauses all actions for operator input
+            Break ListAzureKeyVault                                                         # Breaks :ListAzureKeyVault
+        }                                                                                   # End :ListAzureKeyVault while ($true)
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function ListAzKeyVault
