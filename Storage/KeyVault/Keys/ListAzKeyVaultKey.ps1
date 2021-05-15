3f7bd303ad7c473496f464c6a5a9b8f770840031
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-05-15 14:37:32 -0400

    Updated to new standard

--- Storage/KeyVault/Keys/ListAzKeyVaultKey.ps1
+++ Storage/KeyVault/Keys/ListAzKeyVaultKey.ps1
@@ -19,27 +19,35 @@
         End ListAzKeyVaultKey
             Return Function > Send $null
 }#>
-function ListAzKeyVaultKey { # Lists all key vault Keys
-    Begin {
-        $KVList = Get-AzKeyVault # Creates lists of all key vaults
-        foreach ($VaultName in $KVList) { # For each key vault in $KVList
-            Write-Host "-----------------------------------" # Write message to screen
-            $KVKeylist = Get-AzKeyVaultKey -VaultName $VaultName.VaultName # Creates a list of all keys in current $VaultName
-            Write-Host "Vault Name: " $VaultName.VaultName # Write message to screen
-            Write-Host "" # Write message to screen
-            foreach ($Name in $KVKeylist) { # For each Key name in $KVKeyList
-                Write-Host "Key Name:   " $Name.Name # Write message to screen
-                Write-Host "Key ID:     " $Name.ID
-                Write-Host "Enabled:    " $Name.Enabled # Write message to screen
-                Write-Host "Updated:    " $Name.Updated # Write message to screen
-                if ($Name.Expires) { # If Selected key has an existing expiration
-                    Write-Host "Expires:    " $Name.Expires # Write message to screen
-                    Write-Host "NotBefore:  " $Name.NotBefore
-                } # End if ($Name.Expires)
-                Write-Host "" # Write message to screen
-            } # End foreach ($Name in $KVKeylist)
-        } # End foreach ($VaultName in $KVList)
-        Write-Host "-----------------------------------" # Write message to screen
-        Return # Retures to calling function with $null
-    } # End Begin 
-} # End function ListAzKeyVaultKey
+function ListAzKeyVaultKey {                                                                # Function to list all key vault keys
+    Begin {                                                                                 # Begin function
+        $KVList = Get-AzKeyVault                                                            # Creates lists of all key vaults
+        foreach ($VaultName in $KVList) {                                                   # For each key vault in $KVList
+            Write-Host '------------------------------------------------------------------' # Write message to screen
+            $KVKeylist = Get-AzKeyVaultKey -VaultName $VaultName.VaultName                  # Creates a list of all keys in current $VaultName
+            Write-Host 'Vault Name: ' $VaultName.VaultName                                  # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
+            if ($KVKeylist) {                                                               # If $KVKeyList has a value
+                foreach ($Name in $KVKeylist) {                                             # For each Key name in $KVKeyList
+                    Write-Host 'Key Name:   ' $Name.Name                                    # Write message to screen
+                    Write-Host 'Key ID:     ' $Name.ID                                      # Write message to screen
+                    Write-Host 'Enabled:    ' $Name.Enabled                                 # Write message to screen
+                    Write-Host 'Updated:    ' $Name.Updated                                 # Write message to screen
+                    if ($Name.Expires) {                                                    # If current item has an existing expiration
+                        Write-Host 'Expires:    ' $Name.Expires                             # Write message to screen
+                        Write-Host 'NotBefore:  ' $Name.NotBefore                           # Write message to screen
+                    }                                                                       # End if ($Name.Expires)
+                    Write-Host ''                                                           # Write message to screen
+                }                                                                           # End foreach ($Name in $KVKeylist)
+            }                                                                               # End if ($KVKeylist)
+            else {                                                                          # If $KVKeylist does not have a value
+                Write-Host 'No Keys in this vault'                                          # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+            }                                                                               # End else (if ($KVKeylist))
+        }                                                                                   # End foreach ($VaultName in $KVList)
+        Write-Host '------------------------------------------------------------------'     # Write message to screen
+        Pause                                                                               # Pauses all action for operator input
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End Begin 
+}                                                                                           # End function ListAzKeyVaultKey
