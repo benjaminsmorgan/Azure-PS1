95f54cf24e5975f7b9d57446728c660b3891a4aa
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-05-17 07:09:48 -0400

    Updated to new standard

--- Storage/KeyVault/Secrets/RemoveAzKeyVaultSecret.ps1
+++ Storage/KeyVault/Secrets/RemoveAzKeyVaultSecret.ps1
@@ -3,74 +3,78 @@
     Remove-AzKeyVaultSecret:    https://docs.microsoft.com/en-us/powershell/module/az.keyvault/remove-azkeyvaultsecret?view=azps-5.1.0
     Get-AzKeyVaultSecret:       https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvaultsecret?view=azps-5.1.0
     Get-AzKeyVault:             https://docs.microsoft.com/en-us/powershell/module/az.keyvault/get-azkeyvault?view=azps-5.1.0
-    Get-AzResourceGroup:        https://docs.microsoft.com/en-us/powershell/module/az.resources/get-azresourcegroup?view=azps-5.1.0
 } #>
 <# Required Functions Links: {
     GetAzKeyVaultSecret:        https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/KeyVault/Secrets/GetAzKeyVaultSecret.ps1
-        GetAzKeyVault:              https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/KeyVault/GetAzKeyVault.ps1
-            GetAzResourceGroup:         https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Resource%20Groups/GetAzResourceGroup.ps1
+    GetAzKeyVault:              https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Storage/KeyVault/GetAzKeyVault.ps1
 } #>
 <# Functions Description: {
     RemoveAzKeyVaultSecret:     Removes key vault secret
     GetAzKeyVaultSecret:        Gets a key vault secret object
     GetAzKeyVault:              Gets key vault object
-    GetAzResourceGroup:         Gets resource group object
 } #>
 <# Variables: {
     :RemoveAzureKeyVaultSecret  Outer loop for managing function
-    $RGObject:                  Resource group object
+    $CallingFunction:           Name of this function or the one that called it
     $KeyVaultObject:            Key vault object
     $KeyVaultSecretObject:      Key vault secret object
-    $ConfirmDelete:             Operator confirmation to remove the secret
+    $OpConfirm:                 Operator confirmation to remove the secret
     GetAzKeyVaultSecret{}       Gets $KeyVaultSecretObject
         GetAzKeyVault{}             Gets $KeyVaultSecret
-            GetAzResourceGroup{}        Gets $RGObject
 } #>
 <# Process Flow {
     Function
         Call RemoveAzKeyVaultSecret > Get $null
             Call GetAzKeyVaultSecret > Get $KeyVaultSecretObject
                 Call GetAzKeyVault > Get $KeyVaultObject
-                    Call GetAzResourceGroup > Get $RGObject
-                    End GetAzResourceGroup
-                        Return GetAzKeyVault > Send $RGObject
                 End GetAzKeyVault
                     Return GetAzKeyVaultSecret > Send $KeyVaultObject
             End GetAzKeyVaultSecret
-                Return RemoveAzKeyVaultSecret > Send $KeyVaultSecretObject  
+                Return RemoveAzKeyVaultSecret > Send $KeyVaultSecretObject, $KeyVaultObject  
         End RemoveAzKeyVaultSecret
             Return Function > Send $null
 }#>
-function RemoveAzKeyVaultSecret {
-    Begin {
-        $ErrorActionPreference='silentlyContinue' # Disables errors
-        $WarningPreference = "silentlyContinue" # Disables key vault warnings
-        :RemoveAzureKeyVaultSecret while ($true) { # Outer loop for managing function
-            if (!$KeyVaultSecretObject) { # If $var is $null
-                $KeyVaultSecretObject = GetAzKeyVaultSecret ($RGObject, $KeyVaultObject) # Calls function and assigns output to $Var
-                if (!$KeyVaultSecretObject) { # If $var is $null
-                    Break RemoveAzureKeyVaultSecret # Breaks :RemoveAzureKeyVaultSecret
-                } # End if (!$KeyVaultSecretObject)
-            } # End if (!$KeyVaultSecretObject)
-            Write-host "The selected secret for removal is:"$KeyVaultSecretObject.Name"in vault:"$KeyVaultSecretObject.VaultName # Write message to screen
-            $ConfirmDelete = Read-Host "Remove this secret [Y] or [N]" # Operator confirmation for deletion
-            if (!($ConfirmDelete -eq 'y')) { # $Confirm delete is not equal to 'y'
-                Break RemoveAzureKeyVaultSecret # Breaks :RemoveAzureKeyVaultSecret
-            } # End if (!($ConfirmDelete -eq 'y'))
-            else { # If $ConfirmDelete is equal to 'y'
-                Write-Host "Removing" $KeyVaultSecretObject.Name # Write message to screen
-                Try { # Try the following
-                    Remove-AzKeyVaultSecret -Name $KeyVaultSecretObject.Name -VaultName $KeyVaultSecretObject.VaultName -Force # Removes the selected key
-                } # End Try
-                catch { # If error on Try
-                    Write-Host "There was an issue removing the selected key" # Write message to screen
-                    Write-Host "You may not have the permissions to remove this key" # Write message to screen
-                    Break RemoveAzureKeyVaultSecret # Breaks :RemoveAzureKeyVaultSecret 
-                } # End catch
-                Write-Host "This secret has been removed" # Write message to screen
-                Break RemoveAzureKeyVaultSecret # Breaks :RemoveAzureKeyVaultSecret
-            } # End else (if (!($ConfirmDelete -eq 'y')))
-        } # End :RemoveAzureKeyVaultSecret while ($true)
-        Return # Returns to calling function with $null
-    } # End Begin
-} # End function RemoveAzKeyVaultSecret
+function RemoveAzKeyVaultSecret {                                                           # Function to remove a key vault secret
+    Begin {                                                                                 # Begin function
+        $ErrorActionPreference='silentlyContinue'                                           # Disables errors
+        $WarningPreference = 'silentlyContinue'                                             # Disables key vault warnings
+        if (!$CallingFunction) {                                                            # If $CallingFunction does not have a value
+            $CallingFunction = 'RemoveAzKeyVaultSecret'                                     # Creates $CallingFunction
+        }                                                                                   # End if (!$CallingFunction)
+        :RemoveAzureKeyVaultSecret while ($true) {                                          # Outer loop for managing function
+            $KeyVaultSecretObject, $KeyVaultObject = GetAzKeyVaultSecret ($CallingFunction) # Calls function and assigns output to $Var
+            if (!$KeyVaultSecretObject) {                                                   # If $KeyVaultSecretObject is $null
+                Break RemoveAzureKeyVaultSecret                                             # Breaks :RemoveAzureKeyVaultSecret
+            }                                                                               # End if (!$KeyVaultSecretObject)
+            Write-host ''                                                                   # Write message to screen
+            Write-host 'Remove:    '$KeyVaultSecretObject.Name                              # Write message to screen
+            Write-Host 'From vault:'$KeyVaultSecretObject.VaultName                         # Write message to screen
+            Write-host ''                                                                   # Write message to screen
+            $OpConfirm = Read-Host '[Y] Yes [N] No'                                         # Operator confirmation to delete 
+            Clear-Host                                                                      # Clears screen
+            if ($OpConfirm -eq 'y') {                                                       # If $OpConfirm equals 'y'
+                Write-Host 'Removing' $KeyVaultSecretObject.Name                            # Write message to screen
+                Try {                                                                       # Try the following
+                    Remove-AzKeyVaultSecret -Name $KeyVaultSecretObject.Name -VaultName `
+                        $KeyVaultSecretObject.VaultName -Force -ErrorAction 'Stop'          # Removes the selected key
+                }                                                                           # End Try
+                catch {                                                                     # If try fails
+                    Write-Host 'There was an issue removing the selected key'               # Write message to screen
+                    Write-Host 'You may not have the permissions to remove this key'        # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
+                    Break RemoveAzureKeyVaultSecret                                         # Breaks :RemoveAzureKeyVaultSecret 
+                }                                                                           # End catch
+                Write-Host 'The secret has been removed'                                    # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break RemoveAzureKeyVaultSecret                                             # Breaks :RemoveAzureKeyVaultSecret
+            }                                                                               # End if ($OpConfirm -eq 'y')
+            else {                                                                          # If $OpConfirm is not equal to 'y'
+                Write-Host 'No action taken'                                                # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break RemoveAzureKeyVaultSecret                                             # Breaks :RemoveAzureKeyVaultSecret
+            }                                                                               # End else (if ($OpConfirm -eq 'y'))
+        }                                                                                   # End :RemoveAzureKeyVaultSecret while ($true)
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function RemoveAzKeyVaultSecret
