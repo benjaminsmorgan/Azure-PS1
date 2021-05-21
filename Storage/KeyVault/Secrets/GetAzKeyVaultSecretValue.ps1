7858891749a33f4ac9b8926e0f96f83fe6c0f906
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-05-21 07:37:58 -0400

    Added additional functionality

--- Storage/KeyVault/Secrets/GetAzKeyVaultSecretValue.ps1
+++ Storage/KeyVault/Secrets/GetAzKeyVaultSecretValue.ps1
@@ -67,10 +67,28 @@ function GetAzKeyVaultSecretValue {                                             
             finally {                                                                       # If try succeeds
                 [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($KVSV)               # Finishes unhashing $KVSV
             }                                                                               # End finally
-            Write-Host 'Secret Name: '$KVSO.Name                                            # Write message to screen
-            Write-Host 'Secret Value:'$KVSV                                                 # Write message to screen
-            Pause                                                                           # Pauses all actions for operator input
-            Break GetAzureKeyVaultSecretVal                                                 # Breaks :GetAzureKeyVaultSecretVal
+            Write-Host '[0] Exit'                                                           # Write message to screen
+            Write-Host '[1] Write to screen'                                                # Write message to screen
+            Write-Host '[2] Pass value to:'$CallingFunction                                 # Write message to screen
+            $OpSelect = Read-Host 'Option [#]'                                              # Operator input for selection option
+            if ($OpSelect -eq '0') {                                                        # If $OpSelect equals '0'
+                Break GetAzureKeyVaultSecretVal                                             # Breaks :GetAzureKeyVaultSecretVal
+            }                                                                               # End if ($OpSelect -eq '0')
+            elseif ($OpSelect -eq '1') {                                                    # Else if $OpSelect equals '1'
+                Write-Host 'Secret Name: '$KVSO.Name                                        # Write message to screen
+                Write-Host 'Secret Value:'$KVSV                                             # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break GetAzureKeyVaultSecretVal                                             # Breaks :GetAzureKeyVaultSecretVal
+            }                                                                               # End elseif ($OpSelect -eq '1')
+            elseif ($OpSelect -eq '2') {                                                    # Else if $OpSelect equals '2'
+                Clear-Host                                                                  # Clears screen
+                Return $KVSV                                                                # Returns to calling function with $var
+            }                                                                               # End elseif ($OpSelect -eq '1')
+            else {                                                                          # All other inputs for $OpSelect
+                Write-Host 'That was not a valid option'                                    # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Clear-Host                                                                  # Clears screen
+            }                                                                               # End else (if ($OpSelect -eq '0'))
         }                                                                                   # End :GetAzureKeyVaultSecretValue while ($true) 
         Clear-Host                                                                          # Clears screen
         Return $null                                                                        # Returns to calling function with $null
