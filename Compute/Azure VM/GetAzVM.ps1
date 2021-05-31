8497037bd5bd120ba2444966769548aa8c24cb24
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-05-31 08:28:56 -0400

    Updated to new standard

--- Compute/Azure VM/GetAzVM.ps1
+++ Compute/Azure VM/GetAzVM.ps1
@@ -15,7 +15,7 @@
     $ListNumber:                $Var used to select $VMObject
     $VMArray:                   Array used to list VM info
     $ArrayInput:                $Var used to load items into array
-    $VMListSelect:              Operator input for selecting $VMObject
+    $OpSelect:              Operator input for selecting $VMObject
     $VMObject:                  VM Object
 } #>
 <# Process Flow {
@@ -28,45 +28,66 @@ function GetAzVM {                                                              
     Begin {                                                                                 # Begin function
         :GetAzureVM while ($true) {                                                         # Outer loop for managing function
             $VMList = Get-AzVM -status                                                      # Gets a list
+            if (!$VMList) {                                                                 # If $VMList is $null
+                Write-Host 'No VMs in this subscription'                                    # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break GetAzureVM                                                            # Breaks :GetAzureVM
+            }                                                                               # End if (!$VMList)
             $ListNumber = 1                                                                 # $Var for selecting the VM
             [System.Collections.ArrayList]$VMArray = @()                                    # $VMArray creation
             foreach ($_ in $VMList) {                                                       # For each item in $var
                 $ArrayInput = [PSCustomObject]@{                                            # Creates the PS custom object used to load info into array
-                    'Number' = $ListNumber; 'Name' = $_.Name; `
-                    'RG' =  $_.ResourceGroupName; 'Status' = $_.PowerState                  # Attributes and their values to load into the array
+                    'Number'=$ListNumber;'Name'=$_.Name;'RG'= $_.ResourceGroupName; `
+                    'Status' = $_.PowerState;'LOC'=$_.Location;'OS'=$_.OsProfile;`
+                    'NIC'=$_.Nic                                                            # Attributes and their values to load into the array
                 }                                                                           # End creating $ArrayInput
                 $VMArray.Add($ArrayInput) | Out-Null                                        # Loads items into the array
                 $ListNumber = $ListNumber + 1                                               # Increments $listNumber up by 1
             }                                                                               # End foreach ($_ in $VMList)
-            Write-Host "Exit:    0"                                                         # Write message to screen
-            Write-Host ""                                                                   # Write message to screen 
-            foreach ($_ in $VMArray) {                                                      # Writes all VMs to screen
-                Write-Host "Number: "$_.Number                                              # Write message to screen 
-                Write-Host "Name:   "$_.Name                                                # Write message to screen 
-                Write-Host "RG:     "$_.RG                                                  # Write message to screen
-                Write-Host "Status: "$_.Status                                              # Write message to screen
-                Write-Host ""                                                               # Write message to screen
-            }                                                                               # End foreach ($_ in $VMList)
             :GetAzureVMName while ($true) {                                                 # Inner loop for selecting VM from list
-                $VMSelect = Read-Host "Please enter the number of the VM"                   # Operator input for the selection
-                if ($VMSelect -eq '0') {                                                    # If $VMSelect is 0
+                Write-Host '[0]  Exit'                                                      # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                foreach ($_ in $VMArray) {                                                  # For each item in $VMArray
+                    $Number = $_.Number                                                     # Number is equal to current item .number
+                    if ($Number -le 9) {                                                    # If $Number is 9 or less
+                        Write-Host "[$Number]      "$_.Name                                 # Write message to screen
+                    }                                                                       # End if ($Number -le 9)
+                    else {                                                                  # If $number is greater than 9
+                        Write-Host "[$Number]     "$_.Name                                  # Write message to screen
+                    }                                                                       # End else (if ($Number -le 9))                     
+                    Write-Host 'RG:      '$_.RG                                             # Write message to screen
+                    Write-Host 'Location:'$_.LOC                                            # Write message to screen
+                    if ($_.OS.WindowsConfiguration) {                                       # If current item .OS.WindowsConfiguration has a value
+                        Write-Host 'OS Type:  Windows'                                      # Write message to screen
+                    }                                                                       # End if ($_.OS.WindowsConfiguration)
+                    elseif ($_.OS.LinuxConfiguration) {                                     # If current item .OS.LinuxConfiguration has a value
+                        Write-Host 'OS Type:  Linux'                                        # Write message to screen
+                    }                                                                       # End elseif ($_.OS.LinuxConfiguration)
+                    Write-Host 'Status:  '$_.Status                                         # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                }                                                                           # End foreach ($_ in $VMList)
+                if ($CallingFunction) {                                                     # If $CallingFunction has a value
+                    Write-Host 'You are selecting the VM for:'$CallingFunction              # Write message to screen
+                }                                                                           # End if ($CallingFunction) 
+                $OpSelect = Read-Host 'Option [#]'                                          # Operator input for the selection
+                Clear-Host                                                                  # Clears screen
+                if ($OpSelect -eq '0') {                                                    # If $OpSelect equals '0'
                     Break GetAzureVM                                                        # Breaks :GetAzureVM
                 }                                                                           # End if ($_Select -eq '0')
-                $VMListSelect = $VMArray | Where-Object {$_.Number -eq $VMSelect}           # Isolates selected VM 
-                if ($VMListSelect) {                                                        # If $VMListSelect has a valud
-                    $VMObject = Get-AzVM -ResourceGroupName $VMListSelect.RG `
-                        -Name $VMListSelect.Name                                            # Pulls full $VMObject
-                    if ($VMObject) {                                                        # If $VMObject has a value
-                        Return $VMObject                                                    # Returns to calling function with $VMObject
-                    }                                                                       # End if ($VMObject)
-                    else {                                                                  # If $VMObject does not have a value
-                        Write-Host 'An error has occured'                                   # Write message to screen
-                        Break GetAzureVM                                                    # Breaks :GetAzureVM
-                    }                                                                       # End else (if ($VMObject))
-                }                                                                           # End if ($VMListSelect)
-                Write-Host "That was not a valid selection"                                 # Write message to screen 
+                elseif ($OpSelect -in $VMArray.Number) {
+                    $OpSelect = $VMArray | Where-Object {$_.Number -eq $OpSelect}           # $OpSelect equals $VMArray where $VMArray.Number equals $OpSelect
+                    $VMObject = Get-AzVM -ResourceGroupName $OpSelect.RG `
+                        -Name $OpSelect.Name                                                # Pulls full $VMObject
+                    Return $VMObject                                                        # Returns to calling function with $VMObject
+                }                                                                           # End elseif ($OpSelect -in $VMArray.Number)
+                else {                                                                      # All other inputs for $OpSelect
+                    Write-Host 'That was not a valid input'                                 # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
+                    Clear-Host                                                              # Clears screen
+                }                                                                           # End else (if ($OpSelect -eq '0'))
             }                                                                               # End :GetAzureVMName while ($true)
         }                                                                                   # End :GetAzureVM while ($true)
-        Return                                                                              # Returns to calling function with $null 
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null 
     }                                                                                       # End Begin
-}                                                                                           # End function GetAzureVMName
+}                                                                                           # End function GetAzVM
