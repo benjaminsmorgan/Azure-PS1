0bccba7139331905ca837222f822013454d6891f
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-03 07:36:00 -0400

    Updated to new standard

--- Networking/VNet/ListAzVirtualNetwork.ps1
+++ Networking/VNet/ListAzVirtualNetwork.ps1
@@ -9,6 +9,7 @@
     ListAzVirtualNetwork:       Lists all virtual networks
 } #>
 <# Variables: {
+    :ListAzureVNetwork          Outer loop for managing function
     $VNetList:                  List of virtual networks
 } #>
 <# Process Flow {
@@ -19,14 +20,25 @@
 }#>
 function ListAzVirtualNetwork {                                                             # Function to list all virtual networks
     Begin {                                                                                 # Begin function
-        $VNetList = Get-AzVirtualNetwork                                                    # Gets a list of all virtual networks
-        foreach ($_ in $VNetList) {                                                         # For each item in $VNetList
-            Write-Host 'Name:    '$_.Name                                                   # Write message to screen
-            Write-Host 'RG:      '$_.ResourceGroupName                                      # Write message to screen
-            Write-Host 'Loc:     '$_.Location                                               # Write message to screen
-            Write-Host 'A Space: '$_.AddressSpace.AddressPrefixes                           # Write message to screen
+        :ListAzureVNetwork while ($true) {                                                  # Outer loop for managing function
+            $VNetList = Get-AzVirtualNetwork                                                # Gets a list of all virtual networks
+            if (!$VNetList) {                                                               # If $VNetList is $null
+                Write-Host 'No virtual networks in this subscription'                       # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break ListAzureVNetwork                                                     # Breaks ListAzureVNetwork
+            }                                                                               # End if (!$VNetList)
+            foreach ($_ in $VNetList) {                                                     # For each item in $VNetList
+                Write-Host ''                                                               # Write message to screen
+                Write-Host 'Name:    '$_.Name                                               # Write message to screen
+                Write-Host 'RG:      '$_.ResourceGroupName                                  # Write message to screen
+                Write-Host 'Loc:     '$_.Location                                           # Write message to screen
+                Write-Host 'A Space: '$_.AddressSpace.AddressPrefixes                       # Write message to screen
+            }                                                                               # End foreach ($_ in $VNetList)
             Write-Host ''                                                                   # Write message to screen
-        }                                                                                   # End foreach ($_ in $VNetList)
-        return                                                                              # Returns to calling function with $null
+            Pause                                                                           # Pauses all actions for operator input
+            Break ListAzureVNetwork                                                         # Breaks ListAzureVNetwork
+        }                                                                                   # End :ListAzureVNetwork while ($true)
+        Clear-Host                                                                          # Clears screen
+        return $null                                                                        # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function ListAzVirtualNetwork
