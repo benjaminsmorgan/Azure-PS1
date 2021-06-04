5326afa168ffbe2aff14696200e598a5bce9b4c8
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-04 07:35:54 -0400

    Updated to new standard

--- Resource Groups/Resource Groups/RemoveAzResourceGroup.ps1
+++ Resource Groups/Resource Groups/RemoveAzResourceGroup.ps1
@@ -20,43 +20,28 @@
 <# Variables: {
     :RemoveAzResourceGroup      Outer loop for managing function
     $RGObject:                  Resource group object
-    $CallingFunction:           Name of this function1
+    $CallingFunction:           Name of this function
+    $OpConfirm:                 Operator confirmation to remove the resource group
+    $Locks:                     List of all locks on resource group and contained resources
+    $LocksRemoved:              Confirmation of $Locks being removed
     GetAzResourceGroup{}        Gets $RGObject
     GetAzRGLocksAll{}           Gets $Locks
     RemoveAzResourceLocks{}     Gets $LocksRemoved
 } #>
 <# Process Flow {
     Function
-        Call ManageAzResourceGroup
-            Call NewAzResourceGroup > Get $RGObject
-                Call GetAzLocation > Get $LocationObject
-                End GetAzLocation
-                    Return NewAzResourceGroup > Send $LocationObject
-            End NewAzResourceGroup
-                Return ManageAzResourceGroup > Send $RGObject
+        Call RemoveAzResourceGroup > Get $null
             Call GetAzResourceGroup > Get $RGObject
             End GetAzResourceGroup
-                Return ManageAzResourceGroup > Send $RGObject
-            Call ListAzRGResources > Get $null
-                Call GetAzResourceGroup > Get $RGObject
-                End GetAzResourceGroup
-                    Return ListAzRGResources > Send $RGObject
-            End ListAzRGResources
-                Return ManageAzResourceGroup > Send $null
-            Call RemoveAzResourceGroup > Get $null
-                Call GetAzResourceGroup > Get $RGObject
-                End GetAzResourceGroup
-                    Return RemoveAzResourceGroup > Send $RGObject
-                Call function GetAzRGLocksAll > Get $Locks
-                End GetAzRGLocksAll
-                    Return RemoveAzResourceGroup > Send $Locks
-                Call Function RemoveAzResourceLocks > Get $LocksRemoved
-                End RemoveAzResourceLocks
-                    Return RemoveAzResourceGroup > Send $LocksRemoved
-            End RemoveAzResourceGroup
-                Return ManageAzResourceGroup > Send $null
-        End Function ManageAzResourceGroup
-                Return Function > Send $RGObject                                    
+                Return RemoveAzResourceGroup > Send $RGObject
+            Call function GetAzRGLocksAll > Get $Locks
+            End GetAzRGLocksAll
+                Return RemoveAzResourceGroup > Send $Locks
+            Call Function RemoveAzResourceLocks > Get $LocksRemoved
+            End RemoveAzResourceLocks
+                Return RemoveAzResourceGroup > Send $LocksRemoved
+        End RemoveAzResourceGroup
+            Return ManageAzResourceGroup > Send $null
 }#> 
 function RemoveAzResourceGroup {                                                            # Function to remove a resource group
     Begin {                                                                                 # Begin funciton
@@ -69,48 +54,58 @@ function RemoveAzResourceGroup {                                                
                     Break RemoveAzureRGObject                                               # Breaks :RemoveAzureRGObject
                 }                                                                           # End if (!$RGObject) | Inner
             }                                                                               # End if (!$RGObject) | Outer
-            Clear-Host                                                                      # Clears screen
-            Write-Host "////////////////////////////WARNING\\\\\\\\\\\\\\\\\\\\\\\\\\\\"    # Write message to screen
-            Write-Host `
-                ""$RGObject.ResourceGroupName "will be deleted, this cannot be undone"      # Write message to screen
-            Write-Host " All resource locks will be removed automatically if confirmed "    # Write message to screen
-            Write-Host " All resources within the resource group will also be deleted  "    # Write message to screen
-            Write-Host "\\\\\\\\\\\\\\\\\\\\\\\\\\\\WARNING////////////////////////////"    # Write message to screen
+            Write-Host '////////////////////////////WARNING\\\\\\\\\\\\\\\\\\\\\\\\\\\\'    # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
+            Write-Host ' This will delete:'$RGObject.ResourceGroupName                      # Write message to screen
+            Write-Host ' This action cannot be undone once completed'                       # Write message to screen
+            Write-Host ' All resource locks will be removed automatically if confirmed '    # Write message to screen
+            Write-Host ' All resources within the resource group will also be deleted  '    # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
+            Write-Host '\\\\\\\\\\\\\\\\\\\\\\\\\\\\WARNING////////////////////////////'    # Write message to screen
             Write-Host ''                                                                   # Write message to screen
             Write-Host 'Delete this resource group'                                         # Write message to screen
-            $OperatorConfirm = Read-Host '[Y] or [N]'                                       # Operator input on confirming deletion of the resource group
+            $OpConfirm = Read-Host '[Y] Yes [N] No'                                         # Operator input on confirming deletion of the resource group
+            Clear-Host                                                                      # Clears screen
             $RGObjectName = $RGObject.ResourceGroupName                                     # Collects the name of the resource group for later use
-            if ($OperatorConfirm -eq "Y") {                                                 # If $OperatorConfirm equals 'y'
-                Write-Host "This resource group has been approved for deletion"             # Write message to screen
-            }                                                                               # End if ($OperatorConfirm -eq 'Y')
-            else {                                                                          # Else if $OperatorConfirm does not equal 'y'
+            if ($OpConfirm -eq 'y') {                                                       # If $OpConfirm equals 'y'
+                    Write-Host 'Checking for locks on:'$RGObjectName.ResourceGroupName      # Write message to screen
+                $Locks = GetAzRGLocksAll ($RGObject)                                        # Calls function and assigns output to $vat
+                if ($Locks) {                                                               # If $Locks has a value
+                    Write-Host 'Removing all locks'                                         # Write message to screen
+                    $LocksRemoved = RemoveAzResourceLocks ($Locks)                          # Calls function and assigns output to $var
+                    if ($LocksRemoved -eq 'n') {                                            # If $LocksRemoved equals 'n'
+                        Break RemoveAzureRGObject                                           # Breaks :RemoveAzureRGObject
+                    }                                                                       # End if ($LocksRemoved -eq 'n')
+                    Write-Host 'All Locks removed'                                          # Write message to screen
+                }                                                                           # End if ($Locks)
+                else {                                                                      # If $Locks does not have a value
+                    Write-Host 'No locks on this resource group'                            # Write message to screen
+                }                                                                           # End if ($Locks)
+                Write-Host $RGObject.ResourceGroupName'is being removed'                    # Write message to screen
+                Write-Host 'this may take a while'                                          # Write message to screen
+                Try {                                                                       # Try the following
+                    Remove-AzResourceGroup -Name $RGObject.ResourceGroupName -Force `
+                        -ErrorAction 'Stop' | Out-Null                                      # Removes the resource group
+                }                                                                           # End Try
+                Catch {                                                                     # If try fails
+                    Clear-Host                                                              # Clears screen
+                    Write-Host 'An error has occured'                                       # Write message to screen
+                    Write-Host 'The resource group was not removed'                         # Write message to screen
+                    Write-Host 'You may not have the permssions to complete this action'    # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
+                    Break RemoveAzureRGObject                                               # Break RemoveAzureRGObject   
+                }                                                                           # End catch
+                Write-Host $RGObjectName'has been removed'                                  # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break RemoveAzureRGObject                                                   # Break RemoveAzureRGObject
+            }                                                                               # End if ($OpConfirm -eq 'y')
+            else {                                                                          # All other inputs for $OpConfirm
+                Write-Host 'No changes have been made'                                      # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
                 Break RemoveAzureRGObject                                                   # Breaks :RemoveAzureRGObject
-            }                                                                               # End else (if ($OperatorConfirm -eq 'y')
-            $Locks = $null                                                                  # Clears any previous use of $Locks
-            $Locks = GetAzRGLocksAll ($RGObject)                                            # Calls function and assigns output to $vat
-            if ($Locks) {                                                                   # If $Locks has a value
-                Write-Host "Removing all locks"...                                          # Write message to screen
-                $LocksRemoved = RemoveAzResourceLocks ($Locks)                              # Calls function
-                if ($LocksRemoved -eq 'n') {                                                # If $LocksRemoved equals 'n'
-                    Break RemoveAzureRGObject                                               # Breaks :RemoveAzureRGObject
-                }                                                                           # End if ($LocksRemoved -eq 'n')
-                Write-Host "Locks removed"                                                  # Write message to screen
-            }                                                                               # End if ($Locks)
-            Write-Host $RGObject.ResourceGroupName"is being removed, this may take a while" # Message write to screen
-            Try {                                                                           # Try the following
-                Remove-AzResourceGroup -Name $RGObject.ResourceGroupName -Force `
-                    -ErrorAction 'Stop' | Out-Null                                          # Removes the resource group
-            }                                                                               # End Try
-            Catch {                                                                         # If try fails
-                Write-Host 'An error has occured'                                           # Write message to screen
-                Write-Host 'The resource group was not removed'                             # Write message to screen
-                Write-Host 'You may not have the permssions to complete this action'        # Write message to screen
-                Break RemoveAzureRGObject                                                   # Break RemoveAzureRGObject   
-            }                                                                               # End catch
-            Write-Host $RGObjectName'has been removed'                                      # Write message to screen
-            Break RemoveAzureRGObject                                                       # Break RemoveAzureRGObject
+            }                                                                               # End else (if ($OpConfirm -eq 'y')
+            
         }                                                                                   # End :RemoveAzureRGObject while ($True)
-        Start-Sleep(5)                                                                      # Pauses actions for 5 seconds
         Clear-Host                                                                          # Clears screen
         Return                                                                              # Returns to calling function with $null
     }                                                                                       # End begin statement
