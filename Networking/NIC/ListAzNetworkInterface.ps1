33dce8975b18fc16003559b067bd08450254b5c4
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-05 10:17:34 -0400

    Create ListAzNetworkInterface.ps1

---
+++ Networking/NIC/ListAzNetworkInterface.ps1
@@ -0,0 +1,104 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzNetworkInterface:     https://docs.microsoft.com/en-us/powershell/module/az.network/get-aznetworkinterface?view=azps-5.4.0
+    Get-AzVirtualNetworkSubnetConfig: https://docs.microsoft.com/en-us/powershell/module/az.network/get-azvirtualnetworksubnetconfig?view=azps-5.4.0
+    Get-AzVirtualNetwork:       https://docs.microsoft.com/en-us/powershell/module/az.network/get-azvirtualnetwork?view=azps-5.4.0
+} #>
+<# Required Functions Links: {
+    None
+} #>
+<# Functions Description: {
+    ListAzNetworkInterface:     Lists network interfaces
+} #>
+<# Variables: {
+    :ListAzureNic               Outer loop for managing function
+    $VNetList:                  List of all Vnets
+    $ObjectArray:               Array used to hold info for nic identification
+    $Vnet:                      The current Vnet name (ListAzureNic)
+    $VnetPFX:                   The current Vnet prefix
+    $VnetRG:                    The current Vnet resource group
+    $SubnetList:                List of all subnets under the current Vnet
+    $Subnet:                    The current subnet ID
+    $SubnetName:                The current subnet name 
+    $SubnetPFX:                 The current subnet prefix
+    $NicList:                   List of all nics under the current subnet
+    $ObjectInput:               $var used to load info into $ObjectArray
+    $VM:                        Current item .VM, used for formatting
+} #>
+<# Process Flow {
+    Function
+        Call ListAzNetworkInterface > Get $null
+        End ListAzNetworkInterface
+            Return function > Send $null
+}#>
+function ListAzNetworkInterface {                                                           # Function to get a network interface
+    Begin {                                                                                 # Begin function
+        :ListAzureNIC while ($true) {                                                       # Outer loop for managing function
+            Write-Host 'Gathering network info, this a take a moment'                       # Write message to screen
+            $VNetList = Get-AzVirtualNetwork                                                # Gets a list of all virtual networks
+            Clear-Host                                                                      # Clears screen
+            if (!$VNetList) {                                                               # If $VNetList is $null
+                Write-Host ''                                                               # Write message to screen
+                Write-Host 'No virtuals networks present in this subscription'              # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break ListAzureNIC                                                          # Breaks :ListAzureNIC
+            }                                                                               # End if (!$VNetList)
+            [System.Collections.ArrayList]$ObjectArray = @()                                # Array that all info is loaded into
+            foreach ($_ in $VNetList) {                                                     # For each object in $VnetList
+                $VNet = $_.Name                                                             # Sets $Vnet as the current object Vnet name
+                $VnetPFX = $_.AddressSpace.AddressPrefixes                                  # Sets $VnetPFX as the current object Vnet prefix
+                $VNetRG = $_.ResourceGroupName                                              # Sets $VnetRG as the current object Vnet resource group
+                Write-Host 'Gathering subnets in:'$VNet' | '$VNet                           # Write message to screen
+                $SubnetList = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $_           # Creates a list of subnets on the current object vnet
+                foreach ($_ in $SubnetList) {                                               # For each item in $SubnetList
+                    $Subnet = $_.ID                                                         # $Subnet equals $currentitem.ID
+                    $SubnetName = $_.Name                                                   # Pulls $CurrentItem.Name into $var
+                    $SubnetPFX = $_.AddressPrefix                                           # Pulls $CurrentItem.AddressPrefix nto $var
+                    Write-Host 'Gathering NICs in:'$SubnetName                              # Write message to screen
+                    $NicList = Get-AzNetworkInterface | Where-Object `
+                        {$_.IpConfigurations.Subnet.ID -eq $Subnet}                         # Gets a list of all nics on subnet
+                    foreach ($_ in $NicList) {                                              # For each item in $NicList
+                        $ObjectInput = [PSCustomObject]@{
+                            'NicName'=$_.Name;'NicRG'=$_.ResourceGroupName;`
+                            'SubName'=$Subnetname;'SubPFX'=$SubnetPFX;'VNetName'=$VNet;`
+                            'VnetPFX'=$VnetPFX;'VnetRG'= $VNetRG;'VM'=$_.VirtualMachine.ID
+                        }                                                                   # Creates the item to loaded into array
+                        $ObjectArray.Add($ObjectInput) | Out-Null                           # Loads item into array, out-null removes write to screen
+                    }                                                                       # End foreach ($_ in $NicList)
+                }                                                                           # End foreach ($_ in $SubnetList)
+            }                                                                               # End foreach ($_ in $VnetList)
+            Clear-Host                                                                      # Clears screen
+            if (!$ObjectArray) {                                                            # If $ObjectArray is $null
+                Write-Host ''                                                               # Write message to screen
+                Write-Host 'No virtuals network interfaces present in this subscription'    # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break ListAzureNIC                                                          # Breaks :ListAzureNIC
+            }                                                                               # End if (!$ObjectArray) 
+            foreach ($_ in $ObjectArray) {                                                  # For each item in $ObjectArray
+                Write-Host 'NIC Name:     '$_.NicName                                       # Write message to screen
+                Write-Host 'NIC RG:       '$_.NicRG                                         # Write message to screen
+                Write-Host 'Subnet Name:  '$_.Subname                                       # Write message to screen
+                Write-Host 'Subnet Prefix:'$_.SubPFX                                        # Write message to screen
+                Write-Host 'VNet Name:    '$_.VnetName                                      # Write message to screen
+                Write-Host 'VNet Prefix:  '$_.VnetPFX                                       # Write message to screen
+                Write-Host 'VNet RG:      '$_.VnetRG                                        # Write message to screen
+                if ($_.VM) {                                                                # If $_.VM has a value
+                    $VM = $_.VM                                                             # VM is equal to current item .VM
+                    $VM = $VM.Split('/')[-1]                                                # Collects the VM name
+                    Write-Host 'Attached VM:  '$VM                                          # Write message to screen
+                    $VM = $null                                                             # Clears $VM                                            
+                }                                                                           # End if ($_.VM)
+                else {                                                                      # If $_.VM does not have a value
+                    Write-Host 'Attached VM:   N/A'                                         # Write message to screen
+                }                                                                           # End else (if ($_.VM))
+                Write-Host ''                                                               # Write message to screen
+            }                                                                               # End foreach ($_ in $ObjectArray)
+            Pause                                                                           # Pauses all actions for operator input
+            Break ListAzureNic                                                              # Breaks :ListAzureNic
+        }                                                                                   # End :ListAzureNIC while ($true)
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function ListAzNetworkInterface
