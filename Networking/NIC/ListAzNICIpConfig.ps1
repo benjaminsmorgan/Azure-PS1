8540947e4aa3fff88d8d86589d2048d20620e333
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-08 07:04:40 -0400

    Updated to new standard

--- Networking/NIC/ListAzNICIpConfig.ps1
+++ Networking/NIC/ListAzNICIpConfig.ps1
@@ -20,61 +20,79 @@
     $NicList                    List of all NICs attached to $CurrentSubnet
     $CurrentNic                 Current NIC
     $NicIPList                  List of all IP configs on $CurrentNic
-    $CurrentNicIPConfig         Current IP config
-    $CurrentPubIP               Attached public IP Sku if attached to $CurrentNicIPConfig
+    $NicCon                     Current IP config
+    $PubIP                      Attached public IP Sku if attached to $NicCon
 } #>
 <# Process Flow {
     function
-        Call ListAzNICIpConfig > Get $NicObject
+        Call ListAzNICIpConfig > Get $null
         End ListAzNICIpConfig
-            Return function > Send $NicObject
+            Return function > Send $null
 }#>
 function ListAzNICIpConfig {                                                                # Function to list NIC IP configs
     Begin {                                                                                 # Begin function
         :ListAzureNICIpConfig while($true) {                                                # Outer loop for managing function
             $VNetList = Get-AzVirtualNetwork                                                # Gets lists of all Vnets
+            if (!$VNetList) {                                                               # If $VNetList is $null
+                Write-Host 'No virtual networks exist in this subscription'                 # Write message to screen
+                Pause                                                                       # Pauses all action for operator input
+                Break ListAzureNICIpConfig                                                  # Breaks :ListAzureNICIpConfig
+            }                                                                               # End if (!$VNetList)
             foreach ($_ in $VNetList) {                                                     # For each item in $var
                 $CurrentVNet = $_                                                           # Pulls current object into $var
                 Write-Host 'Vnet Name:     '$CurrentVNet.Name                               # Write message to screen
                 Write-Host 'Vnet Prefix:   '$CurrentVnet.AddressSpace.AddressPrefixes       # Write message to screen
+                Write-Host ''                                                               # Write message to screen
                 $SubnetList = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork `
                     $CurrentVNet                                                            # Gets a list of all subnets in Vnet
+                if (!$SubnetList) {                                                         # If $SubnetList is $null
+                    Write-Host 'No subnets exist in this virtual network'                   # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                }                                                                           # End if (!$SubnetList)
                 foreach ($_ in $SubnetList) {                                               # For each item in $var
                     $CurrentSubnet = $_                                                     # Pulls current object into $var
-                    Write-Host ' Sub Name:      '$CurrentSubnet.name                        # Write message to screen
-                    Write-Host ' Sub Prefix:    '$CurrentSubnet.AddressPrefix               # Write message to screen
+                    Write-Host 'Sub Name:      '$CurrentSubnet.name                        # Write message to screen
+                    Write-Host 'Sub Prefix:    '$CurrentSubnet.AddressPrefix               # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
                     $NicList = Get-AzNetworkInterface | Where-Object `
                         {$_.IpConfigurations.Subnet.ID -eq $CurrentSubnet.ID}               # Gets a list of all nics attached to subnet
+                    if (!$NicList) {                                                        # If $NicList is $null
+                        Write-Host 'No NICs exist in this subnet'                           # Write message to screen
+                        Write-Host ''                                                       # Write message to screen
+                    }                                                                       # End if (!$NicList)
                     foreach ($_ in $NicList) {                                              # For each item in $var
                         $CurrentNic = $_                                                    # Pulls current object into $var
-                        Write-Host '   Nic Name:      '$CurrentNic.Name                     # Write message to screen
+                        Write-Host 'Nic Name:      '$CurrentNic.Name                        # Write message to screen
+                        Write-Host ''                                                       # Write message to screen
                         $NicIPList = $CurrentNic.IPConfigurations                           # Gets a list of all IP configs on Nic
                         foreach ($_ in $NicIPList) {                                        # For each item in $var
-                            $CurrentNicIPConfig = $_                                        # Pulls current object into $var
-                            if ($CurrentNicIPConfig.PublicIPaddress.ID) {                   # If a public IP sku is attached
-                                $CurrentPubIP = Get-AzPublicIpAddress | Where-Object `
-                                    {$_.ID -eq $CurrentNicIPConfig.PublicIPaddress.ID}      # Pulls the public IP sku info
-                            }                                                               # End if ($CurrentNicIPConfig.PublicIPaddress.ID)
-                            Write-Host '    Config Name:   '$CurrentNicIPConfig.Name        # Write message to screen
-                            Write-Host '    Is Primary:    '$CurrentNicIPConfig.Primary     # Write message to screen
-                            Write-Host '    PrivIPAddress: '`
-                                $CurrentNicIPConfig.PrivateIPAddress                        # Write message to screen
-                            Write-Host '    PrivAllocation:'`
-                                $CurrentNicIPConfig.PrivateIpAllocationMethod               # Write message to screen
-                            if ($CurrentPubIP) {
-                                Write-Host '    PubIPName:     '$CurrentPubIP.Name          # Write message to screen
-                                Write-Host '    PubIPAddress:  '$CurrentPubIP.IpAddress     # Write message to screen
-                                Write-Host '    PubAllocation: '`
-                                    $CurrentPubIP.PublicIpAllocationMethod                  # Write message to screen
-                            }                                                               # End if ($CurrentPubIP) 
-                            $CurrentPubIP = $null                                           # Clears $var
+                            $NicCon = $_                                                    # Pulls current object into $var
+                            if ($NicCon.PublicIPaddress.ID) {                               # If a public IP sku is attached
+                                $PubIP = Get-AzPublicIpAddress | Where-Object `
+                                    {$_.ID -eq $NicCon.PublicIPaddress.ID}                  # Pulls the public IP sku info
+                            }                                                               # End if ($NicCon.PublicIPaddress.ID)
+                            Write-Host 'Config Name:   '$NicCon.Name                        # Write message to screen
+                            Write-Host 'Is Primary:    '$NicCon.Primary                     # Write message to screen
+                            Write-Host 'PrivIPAddress: '$NicCon.PrivateIPAddress            # Write message to screen
+                            Write-Host 'PrivAllocation:'$NicCon.PrivateIpAllocationMethod   # Write message to screen
+                            if ($PubIP) {
+                                Write-Host 'PubIPName:     '$PubIP.Name                     # Write message to screen
+                                Write-Host 'PubIPAddress:  '$PubIP.IpAddress                # Write message to screen
+                                Write-Host 'PubAllocation: '$PubIP.PublicIpAllocationMethod # Write message to screen
+                                $PubIP = $null                                              # Clears $PubIP
+                            }                                                               # End if ($PubIP) 
+                            Write-Host ''                                                   # Write message to screen
                         }                                                                   # End foreach ($_ in $NicIPList)
+                        Write-Host ''                                                       # Write message to screen
                     }                                                                       # End foreach ($_ in $NicList)
+                    Write-Host ''                                                           # Write message to screen
                 }                                                                           # End foreach ($_ in $SubnetList)
-                Write-Host ''                                                               # Write message to screen (Splits the text by Vnets)
+                Write-Host ''                                                               # Write message to screen
             }                                                                               # End foreach ($_ in $VNetList)
+            Pause                                                                           # Pauses all actions for operator input
             Break ListAzureNICIpConfig                                                      # Breaks :ListAzureNICIpConfig
         }                                                                                   # End :ListAzureNICIpConfig while($true)
+        Clear-Host                                                                          # Clears screen
         Return                                                                              # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function ListAzNICIpConfig
