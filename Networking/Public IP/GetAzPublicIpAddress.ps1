9296700dde6def16639fbf984e4395d9390ceeaa
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-13 08:10:20 -0400

    Updated to new standard

--- Networking/Public IP/GetAzPublicIpAddress.ps1
+++ Networking/Public IP/GetAzPublicIpAddress.ps1
@@ -1,6 +1,6 @@
 # Benjamin Morgan benjamin.s.morgan@outlook.com 
 <# Ref: { Mircosoft docs links
-    Get-AzPublicIpAddress:      https://docs.microsoft.com/en-us/powershell/module/az.network/get-azpublicipaddress?view=azps-5.5.0    
+    Get-AzPublicIpAddress:                      https://docs.microsoft.com/en-us/powershell/module/az.network/get-azpublicipaddress?view=azps-5.5.0    
 } #>
 <# Required Functions Links: {
     None
@@ -30,6 +30,12 @@ function GetAzPublicIpAddress {                                                 
     Begin {                                                                                 # Begin function
         :GetAzurePublicIP while ($true) {                                                   # Outer loop for managing function
             $ObjectList = Get-AzPublicIpAddress                                             # Gets a list of all public IP address
+            if (!$ObjectList) {                                                             # If $ObjectList is $null
+                Write-Host 'No public IP skus are present in this subscription'             # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break GetAzurePublicIP                                                      # Breaks :GetAzurePublicIP
+            }                                                                               # End if (!$ObjectList)
             [System.Collections.ArrayList]$ObjectArray = @()                                # Creates array for list to be loaded into
             $ObjectNumber = 1                                                               # Creates #var used for list selection
             foreach ($_ in $ObjectList) {                                                   # For each item in $ObjectList
@@ -37,21 +43,29 @@ function GetAzPublicIpAddress {                                                 
                     $AttachedNIC = ($_.IpConfiguration.Id).Split('/')[-3]                   # Gets the attached NIC name
                     $AttachedNICIPConfig = ($_.IpConfiguration.Id).Split('/')[-1]           # Gets the attached NIC IP config name
                 }                                                                           # End if ($_.IpConfiguration.Id) 
-                $ObjectInput = [PSCustomObject]@{'Number'=$ObjectNumber;`
-                    'Name' = $_.Name;'RG'=$_.ResourceGroupName;'Sku'=$_.Sku.Name;`
-                    'Allocation'=$_.PublicIpAllocationMethod;'Address'=$_.IPAddress;'NIC'=`
-                    $AttachedNIC;'IPconfig'=$AttachedNICIPConfig}                           # Creates the item to loaded into array
+                $ObjectInput = [PSCustomObject]@{                                           # Custom item to load into array
+                    'Number'=$ObjectNumber;'Name'=$_.Name;'RG'=$_.ResourceGroupName;`
+                    'Sku'=$_.Sku.Name;'Allocation'=$_.PublicIpAllocationMethod;`
+                    'Address'=$_.IPAddress;'NIC'=$AttachedNIC;`
+                    'IPconfig'=$AttachedNICIPConfig                                         # Creates the item list to load int array
+                }                                                                           # End $ObjectInput = [PSCustomObject]@
                 $ObjectArray.Add($ObjectInput) | Out-Null                                   # Loads item into array, out-null removes write to screen
                 $ObjectNumber = $ObjectNumber + 1                                           # Increments $var up by 1
                 $AttachedNIC = $null                                                        # Clears $var
                 $AttachedNICIPConfig = $null                                                # Clears $var
             }                                                                               # End foreach ($_ in $ObjectList)
             :SelectAzurePublicIP while ($true) {                                            # Inner loop for selecting the public IP
-                Write-Host "[0] Exit"                                                       # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Write-Host '[0]          Exit'                                              # Write message to screen
                 Write-Host ''                                                               # Write message to screen
                 foreach ($_ in $ObjectArray) {                                              # For each item in $ObjectArray
                     $Number = $_.Number                                                     # Creats $Number (Used for formating)
-                    Write-Host "[$Number]         "$_.Name                                  # Write message to screen
+                    if ($Number -le 9) {                                                    # If $Number is 9 or less
+                        Write-Host "[$Number]         "$_.Name                              # Write message to screen
+                    }                                                                       # End if ($Number -le 9)
+                    else {                                                                  # If $Number is greater than 9
+                        Write-Host "[$Number]        "$_.Name                               # Write message to screen    
+                    }                                                                       # End else if ($Number -le 9)
                     Write-Host 'RG Name:    '$_.RG                                          # Write message to screen
                     Write-Host 'Allocation: '$_.Allocation                                  # Write message to screen
                     Write-Host 'IP Address: '$_.address                                     # Write message to screen
