1a3a4abe9b59b507b955afafb74e390394f3b952
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-13 08:10:01 -0400

    Updated to new standard

--- Networking/Public IP/RemoveAzPublicIPAddress.ps1
+++ Networking/Public IP/RemoveAzPublicIPAddress.ps1
@@ -1,7 +1,7 @@
 # Benjamin Morgan benjamin.s.morgan@outlook.com 
 <# Ref: { Mircosoft docs links
-    Remove-AzPublicIpAddress:   https://docs.microsoft.com/en-us/powershell/module/az.network/remove-azpublicipaddress?view=azps-5.5.0    
-    Get-AzPublicIpAddress:      https://docs.microsoft.com/en-us/powershell/module/az.network/get-azpublicipaddress?view=azps-5.5.0    
+    Remove-AzPublicIpAddress:                   https://docs.microsoft.com/en-us/powershell/module/az.network/remove-azpublicipaddress?view=azps-5.5.0    
+    Get-AzPublicIpAddress:                      https://docs.microsoft.com/en-us/powershell/module/az.network/get-azpublicipaddress?view=azps-5.5.0    
 } #>
 <# Required Functions Links: {
     GetAzPublicIPAddress:       https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Networking/Public%20IP/GetAzPublicIpAddress.ps1
@@ -12,11 +12,11 @@
 } #>
 <# Variables: {      
     :RemoveAzurePublicIP        Outer loop for managing function
-    $CallingFunction:           Name of this function
+    $CallingFunction:           Name of this function, or the one that called it
     $PublicIPObject:            Public IP object
     $AttachedNIC:               Nic object if attached to $PublicIPObject
     $AttachedNICIPConfig:       IP config object on $AttachedNIC
-    $OperatorConfirm:           Operator confirmation to remove the public IP sku
+    $OpConfirm:                 Operator confirmation to remove the public IP sku
 } #>
 <# Process Flow {
     function
@@ -29,46 +29,62 @@
 }#>
 function RemoveAzPublicIPAddress {                                                          # Function for removing a public IP Sku
     Begin {                                                                                 # Begin function
+        if (!$CallingFunction) {                                                            # If $CallingFunction is $null
+            $CallingFunction = 'RemoveAzPublicIPAddress'                                    # Creates $CallingFunction
+        }                                                                                   # End if (!$CallingFunction)
         :RemoveAzurePublicIP while ($true) {                                                # Outer loop for managing function
-            $CallingFunction = 'RemoveAzPublicIPAddress'                                    # Sets $CallingFunction
+            $PublicIPObject = GetAzPublicIpAddress ($CallingFunction)                       # Calls function and assigns output to $var
             if (!$PublicIPObject) {                                                         # If $PublicIPObject is $null
-                $PublicIPObject = GetAzPublicIpAddress ($CallingFunction)                   # Calls function and assigns output to $var
-                if (!$PublicIPObject) {                                                     # If $PublicIPObject is $null
-                    Break RemoveAzurePublicIP                                               # Breaks :RemoveAzurePublicIP
-                }                                                                           # End if (!$PublicIPObject)
+                Break RemoveAzurePublicIP                                                   # Breaks :RemoveAzurePublicIP
             }                                                                               # End if (!$PublicIPObject)
             if ($PublicIPObject.IpConfiguration.Id) {                                       # If PublicIPObject.IpConfiguration.Id has a value
                 $AttachedNIC = ($PublicIPObject.IpConfiguration.Id).Split('/')[-3]          # Gets the attached NIC name
                 $AttachedNICIPConfig = ($PublicIPObject.IpConfiguration.Id).Split('/')[-1]  # Gets the attached NIC IP config name
-                Write-Host 'The selecting IP sku cannot be deleted'                         # Write message to screen
-                Write-Host 'Please detach this sku from the following'                      # Write message to screen
+                Write-Host 'The selected IP sku cannot be deleted'                          # Write message to screen
+                Write-Host 'Please detach this sku from the following:'                     # Write message to screen
+                Write-Host ''                                                               # Write message to screen
                 Write-Host 'Attached NIC:   '$AttachedNIC                                   # Write message to screen
                 Write-Host 'Nic config name:'$AttachedNICIPConfig                           # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
                 Break RemoveAzurePublicIP                                                   # Breaks :RemoveAzurePublicIP
             }                                                                               # End if ($PublicIPObject.IpConfiguration.Id)
             else {                                                                          # If PublicIPObject.IpConfiguration.Id does not have a value
-                Write-Host 'Remove the public IP'$PublicIPObject.name                       # Write message to screen
-                $OperatorConfirm = Read-Host '[Y] or [N]'                                   # Operator confirmation to remove the public IP
-                if ($OperatorConfirm -eq 'y') {                                             # If $OperatorConfirm equals 'y'
+                Write-Host 'Remove the public IP:'$PublicIPObject.name                      # Write message to screen
+                $OpConfirm = Read-Host '[Y] Yes [N] No'                                     # Operator confirmation to remove the public IP
+                Clear-Host                                                                  # Clears screen
+                if ($OpConfirm -eq 'y') {                                                   # If $OpConfirm equals 'y'
                     Try {                                                                   # Try the following
+                        Write-Host 'Removing public IP sku'                                 # Write message to screen
                         Remove-AzPublicIpAddress -Name $PublicIPObject.Name `
                             -ResourceGroupName $PublicIPObject.ResourceGroupName `
-                            -Force -ErrorAction 'Stop'                                      # Removes the selected IP sku
+                            -Force -ErrorAction 'Stop' | Out-Null                           # Removes the selected IP sku
                     }                                                                       # End try
                     catch {                                                                 # If try fails
+                        Clear-Host                                                          # Clears screen
                         Write-Host 'An error has occured'                                   # Write message to screen
+                        Write-Host ''                                                       # Write message to screen
                         Write-Host 'You may not have to the permissions'                    # Write message to screen
+                        Write-Host ''                                                       # Write message to screen
                         Write-Host 'The resource or group maybe locked'                     # Write message to screen
+                        Write-Host ''                                                       # Write message to screen
+                        Pause                                                               # Pauses all actions for operator input
                         Break RemoveAzurePublicIP                                           # Breaks :RemoveAzurePublicIP
                     }                                                                       # End catch
+                    Clear-Host                                                              # Clears screen
                     Write-Host 'The selected public IP sku has been removed'                # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
                     Break RemoveAzurePublicIP                                               # Breaks :RemoveAzurePublicIP
-                }                                                                           # End if ($OperatorConfirm -eq 'y')
-                else {                                                                      # If $OperatorConfirm does not equal 'y'
+                }                                                                           # End if ($OpConfirm -eq 'y')
+                else {                                                                      # If $OpConfirm does not equal 'y'
+                    Write-Host 'No changes have been made'                                  # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
                     Break RemoveAzurePublicIP                                               # Breaks :RemoveAzurePublicIP
-                }                                                                           # End else (if ($OperatorConfirm -eq 'y'))
+                }                                                                           # End else (if ($OpConfirm -eq 'y'))
             }                                                                               # End else (if ($PublicIPObject.IpConfiguration.Id))
         }                                                                                   # End :RemoveAzurePublicIP while ($true)
-        Return                                                                              # Returns to calling function with $null
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function RemoveAzPublicIPAddress
