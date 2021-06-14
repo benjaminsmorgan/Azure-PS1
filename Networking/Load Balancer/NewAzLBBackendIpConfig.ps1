8cabe912d2baabc9e7ceba0e830475e6bd7314f2
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-14 07:28:39 -0400

    Added name verification check

--- Networking/Load Balancer/NewAzLBBackendIpConfig.ps1
+++ Networking/Load Balancer/NewAzLBBackendIpConfig.ps1
@@ -11,6 +11,10 @@
 <# Variables: {      
     :NewAzureLBBEIpConfig       Outer loop for managing function
     :SetAzureLBBEName           Inner loop for setting the Back end name
+    $ValidArray:                Array of valid non first/last characters to load balancer config
+    $Valid1stChar:              Array of valid first characters to load balancer config
+    $ValidLastChar:             Array of valid last characters to load balancer config
+    $BackEndNameArray:         $BackEndNameObject converted to array
     $BackEndNameObject:         Operator input for the Back end name
     $OpConfirm:                 Operator confirmation of the Back end name
     $BackEndIPConfigObject:     Back end IP config object    
@@ -24,19 +28,62 @@
 function NewAzLBBackendIpConfig {                                                           # Function to set up the Back end IP for a load balancer
     Begin {                                                                                 # Begin function
         :NewAzureLBBEIpConfig while ($true) {                                               # Outer loop for managing the function
+            $ValidArray = 'abcdefghijklmnopqrstuvwxyz0123456789-_.'                         # Creates a string of valid characters
+            $ValidArray = $ValidArray.ToCharArray()                                         # Loads all valid characters into array
+            $Valid1stChar = 'abcdefghijklmnopqrstuvwxyz0123456789'                          # Creates a string of valid first character
+            $Valid1stChar = $Valid1stChar.ToCharArray()                                     # Loads all valid characters into array
+            $ValidLastChar = 'abcdefghijklmnopqrstuvwxyz0123456789_'                        # Creates a string of valid last character
+            $ValidLastChar = $ValidLastChar.ToCharArray()                                   # Loads all valid characters into array
             :SetAzureLBBEName while ($true) {                                               # Inner loop for setting the back end name
                 Write-Host 'Enter the load balancer back end name'                          # Write message to screen
                 $BackEndNameObject = Read-Host 'Name'                                       # Operator input for the back end name
+                $BackEndNameArray = $BackEndNameObject.ToCharArray()                        # Loads $BackEndNameArray into array
                 Clear-Host                                                                  # Clears screen
-                Write-Host 'Use:'$BackEndNameObject' as the back end name'                  # Writes message to screen
-                $OpConfirm = Read-Host '[Y] Yes [N] No [E] Exit'                            # Operator confirmation of the back end name
-                Clear-Host                                                                  # Clears screen
-                if ($OpConfirm -eq 'e') {                                                   # If $OpConfirm equals 'e''
-                    Break NewAzureLBBEIpConfig                                              # Breaks :NewAzureLBBEIpConfig
-                }                                                                           # End if ($OpConfirm -eq 'e')
-                if ($OpConfirm -eq 'y') {                                                   # If $OpConfirm equals 'y'
-                    Break SetAzureLBBEName                                                  # Breaks :SetAzureLBBEName
-                }                                                                           # End if ($OpConfirm -eq 'y')
+                if ($BackEndNameObject.Length -ge 81) {                                     # If $BackEndNameObject.Length is greater or equal to 81
+                    Write-Host 'The back end name is to long'                               # Write message to screen
+                    Write-Host 'Max length of the name is 80 characters'                    # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    $BackEndNameObject = $null                                              # Clears $BackEndNameObject
+                }                                                                           # End if ($LBNameObject.Length -ge 80)
+                if ($BackEndNameArray[0] -notin $Valid1stChar) {                            # If 0 position of $BackEndNameArray is not in $Valid1stChar
+                    Write-Host 'The first character of the name must be a letter or number' # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    $BackEndNameObject = $null                                              # Clears $BackEndNameObject
+                }                                                                           # End if ($BackEndNameArray[0] -notin $Valid1stChar)
+                if ($BackEndNameArray[-1] -notin $ValidLastChar) {                          # If last position of $BackEndNameArray is not in $ValidLastChar
+                    Write-Host `
+                        'The last character of the name must be a letter, number or _ '     # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    $BackEndNameObject = $null                                              # Clears $BackEndNameObject
+                }                                                                           # End if ($BackEndNameArray[0] -notin $Valid1stChar)
+                foreach ($_ in $BackEndNameArray) {                                         # For each item in $BackEndNameArray
+                    if ($_ -notin $ValidArray) {                                            # If current item is not in $ValidArray
+                        if ($_ -eq ' ') {                                                   # If current item equals 'space'
+                            Write-Host ''                                                   # Write message to screen    
+                            Write-Host 'Back end name cannot include any spaces'            # Write message to screen
+                        }                                                                   # End if ($_ -eq ' ')
+                        else {                                                              # If current item is not equal to 'space'
+                            Write-Host ''                                                   # Write message to screen    
+                            Write-Host $_' is not a valid character'                        # Write message to screen
+                        }                                                                   # End else (if ($_ -eq ' '))
+                        $BackEndNameObject = $null                                          # Clears $BackEndNameObject
+                    }                                                                       # End if ($_ -notin $ValidArray)
+                }                                                                           # End foreach ($_ in $LBNameArray)
+                if ($BackEndNameObject) {                                                   # $BackEndNameObject has a value
+                    Write-Host 'Use:'$BackEndNameObject' as the back end name'              # Writes message to screen
+                    $OpConfirm = Read-Host '[Y] Yes [N] No [E] Exit'                        # Operator confirmation of the back end name
+                    Clear-Host                                                              # Clears screen
+                    if ($OpConfirm -eq 'e') {                                               # If $OpConfirm equals 'e''
+                        Break NewAzureLBBEIpConfig                                          # Breaks :NewAzureLBBEIpConfig
+                    }                                                                       # End if ($OpConfirm -eq 'e')
+                    if ($OpConfirm -eq 'y') {                                               # If $OpConfirm equals 'y'
+                        Break SetAzureLBBEName                                              # Breaks :SetAzureLBBEName
+                    }                                                                       # End if ($OpConfirm -eq 'y')
+                }                                                                           # End if ($BackEndNameObject)
+                else {                                                                      # If $BackEndNameObject does not have a value
+                    Pause                                                                   # Pauses all actions for operator input
+                    Clear-Host                                                              # Clears screen
+                }                                                                           # End else (if ($BackEndNameObject))
             }                                                                               # End :SetAzureLBBEName while ($true)
             Try {                                                                           # Try the following
                 Write-Host 'Building the load balancer back end config'                     # Write message to screen
