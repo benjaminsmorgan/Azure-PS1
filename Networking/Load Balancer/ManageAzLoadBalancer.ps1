bbfadea2e8e57a4cba0f7c05dea318d86d489c3d
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-27 09:09:24 -0400

    Merged updated functions

--- Networking/Load Balancer/ManageAzLoadBalancer.ps1
+++ Networking/Load Balancer/ManageAzLoadBalancer.ps1
@@ -2140,8 +2140,9 @@ function ManageAzLBNatRuleConfig {                                              
             Write-Host '[0] Exit'                                                           # Write message to screen
             Write-Host '[1] Add Nat Rule Config'                                            # Write message to screen
             Write-Host '[2] List Nat Rule Configs'                                          # Write message to screen
-            Write-Host '[3] Change Nat Rule Config'                                         # Write message to screen
-            Write-Host '[4] Remove Nat Rule Config'                                         # Write message to screen
+            Write-Host '[3] Add VM to Nat Rule Config'                                      # Write message to screen
+            Write-Host '[4] Change Nat Rule Config'                                         # Write message to screen
+            Write-Host '[5] Remove Nat Rule Config'                                         # Write message to screen
             $OpSelect = Read-Host 'Option [#]'                                              # Operator input for the function selection
             Clear-Host                                                                      # Clears screen
             if ($OpSelect -eq '0') {                                                        # If $OpSelect equals '0'    
@@ -2156,13 +2157,17 @@ function ManageAzLBNatRuleConfig {                                              
                 ListAzLBNatRuleConfig                                                       # Calls function
             }                                                                               # End elseif ($OpSelect -eq '2')
             elseif ($OpSelect -eq '3') {                                                    # Else if $OpSelect equals '3'
-                Write-Host 'Change Nat Rule Config'                                         # Write message to screen
-                #ListAzLBFEConfigs                                                           # Calls function
+                Write-Host 'Add VM to Nat Rule Config'                                      # Write message to screen
+                SetAzLBNatRuleVM                                                            # Calls function
             }                                                                               # End elseif ($OpSelect -eq '3')
             elseif ($OpSelect -eq '4') {                                                    # Else if $OpSelect equals '4'
+                Write-Host 'Change Nat Rule Config'                                         # Write message to screen
+                #ListAzLBFEConfigs                                                          # Calls function
+            }                                                                               # End elseif ($OpSelect -eq '4')
+            elseif ($OpSelect -eq '5') {                                                    # Else if $OpSelect equals '5'
                 Write-Host 'Remove Nat Rule Config'                                         # Write message to screen
                 RemoveAzLBNatRuleConfig                                                     # Calls function
-            }                                                                               # End elseif ($OpSelect -eq '4')
+            }                                                                               # End elseif ($OpSelect -eq '5')
             else {                                                                          # All other inputs for $OpSelect
                 Write-Host 'That was not a valid input'                                     # Write message to screen
                 Write-Host ''                                                               # Write message to screen
@@ -2372,7 +2377,7 @@ function AddAzLBNatRuleConfig {                                                 
         Return $null                                                                        # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function AddAzLBNatRuleConfig
-Function ListAzLBNatRuleConfig {                                                            # Function to list all load balancer nat rules
+function ListAzLBNatRuleConfig {                                                            # Function to list all load balancer nat rules
     Begin {                                                                                 # Begin function
         :ListAzureLBNatRule while ($true) {                                                 # Outer loop for managing function
             $LBList = Get-AzLoadBalancer                                                    # Gets a list of all load balancers
@@ -2486,6 +2491,72 @@ Function ListAzLBNatRuleConfig {                                                
         Return $null                                                                        # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function ListAzLBNatRuleConfig
+function SetAzLBNatRuleVM {                                                                 # Function to associate a VM to a nat rule
+    Begin {                                                                                 # Begin function
+        if (!$CallingFunction) {                                                            # If $CallingFunction is $null
+            $CallingFunction  = 'SetAzLBNatRuleVM'                                          # Creates $CallingFunction
+        }                                                                                   # End if (!$CallingFunction)
+        :SetAzureLBNatRuleVM while ($true) {                                                # Outer loop for managing function
+            $LBNatRule, $LoadBalancerObject = GetAzLBNatRuleConfig ($CallingFunction)       # Calls function and assigns output to $var
+            if (!$LBNatRule) {                                                              # If $LBNatRule is $null
+                Break SetAzureLBNatRuleVM                                                   # Breaks :SetAzureLBNatRuleVM
+            }                                                                               # End if (!$LBNatRule)
+            $NicIPConfigObject,$NicObject = GetAzNICIpConfig ($CallingFunction)             # Calls function and assigns output to $var
+            if (!$NicIPConfigObject) {                                                      # If $NicIPConfigObject is $null
+                Break SetAzureLBNatRuleVM                                                   # Breaks :SetAzureLBNatRuleVM
+            }                                                                               # End if (!$NicIPConfigObject)
+            $VMName = $NicObject.VirtualMachine.ID.Split('/')[-1]                           # Isloates the VM name
+            Write-Host 'Make the following change:'                                         # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
+            Write-Host 'Add:'                                                               # Write message to screen
+            Write-Host 'VM Name:      '$VMName                                              # Write message to screen
+            Write-Host 'Nic Name:     '$NicObject.name                                      # Write message to screen
+            Write-Host 'Config Name:  '$NicIPConfigObject.Name                              # Write message to screen
+            Write-Host 'Private IP:   '$NicIPConfigObject.PrivateIPAddress                  # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
+            Write-Host 'To:'                                                                # Write message to screen
+            Write-Host 'Load Balancer:'$LoadBalancerObject.name                             # Write message to screen
+            Write-Host 'Nat Rule:     '$LBNatRule.name                                      # Write message to screen
+            Write-Host 'Nat Protocol: '$LBNatRule.Protocol                                  # Write message to screen
+            Write-Host 'Nat FE Port:  '$LBNatRule.FrontendPort                              # Write message to screen
+            Write-Host 'Nat BE Port:  '$LBNatRule.BackendPort                               # Write message to screen
+            Write-Host 'Nat Idle TO:  '$LBNatRule.IdleTimeoutInMinutes                      # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
+            $OpConfirm = Read-Host '[Y] Yes [N] No'                                         # Operator confirmation to make the change
+            Clear-Host                                                                      # Clears screen
+            if ($OpConfirm -eq 'y') {                                                       # If $OpConfirm equals 'y'
+                Try {                                                                       # Try the following
+                    Write-Host 'Adding the IP config to nat rule'                           # Write message to screen
+                    $NicObject | Set-AzNetworkInterfaceIpConfig -Name `
+                        $NicIPConfigObject.Name -LoadBalancerInboundNatRuleId `
+                        $LBNatRule.ID -ErrorAction 'Stop' | Out-Null                        # Adds the load balancer inbound nat rule
+                }                                                                           # End Try
+                catch {                                                                     # If Try fails
+                    Clear-Host                                                              # Clears screen
+                    Write-Host 'An error has occured'                                       # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
+                    Break SetAzureLBNatRuleVM                                               # Breaks :SetAzureLBNatRuleVM
+                }                                                                           # End catch
+                Write-Host 'Saving nic configuration'                                       # Write message to screen
+                $NicObject | Set-AzNetworkInterface                                         # Saves the nic config
+                Clear-Host                                                                  # Clears screen
+                Write-Host 'The changes have been made'                                     # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break SetAzureLBNatRuleVM                                                   # Breaks :SetAzureLBNatRuleVM
+            }                                                                               # End if ($OpConfirm -eq 'y') 
+            else {                                                                          # All other inputs for $OpSelect
+                Write-Host 'No changes have been made'                                      # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break SetAzureLBNatRuleVM                                                   # Breaks :SetAzureLBNatRuleVM
+            }                                                                               # End else (if ($OpConfirm -eq 'y'))
+        }                                                                                   # End :SetAzureLBNatRuleVM while ($true)
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function SetAzLBNatRuleVM
 Function GetAzLBNatRuleConfig {                                                             # Function to get a load balancer nat rule
     Begin {                                                                                 # Begin function
         :GetAzureLBNatRule while ($true) {                                                  # Outer loop for managing function
