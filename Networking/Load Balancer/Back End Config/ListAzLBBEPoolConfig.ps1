32f6eba6800ba84def64c092c73828e0edc98b0d
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-06-29 07:29:57 -0400

    Create ListAzLBBEPoolConfig.ps1

---
+++ Networking/Load Balancer/Back End Config/ListAzLBBEPoolConfig.ps1
@@ -0,0 +1,113 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzLoadBalancer:                         https://docs.microsoft.com/en-us/powershell/module/az.network/get-azloadbalancer?view=azps-5.5.0 
+    Get-AzLoadBalancerBackendAddressPool:       https://docs.microsoft.com/en-us/powershell/module/az.network/get-azloadbalancerbackendaddresspool?view=azps-6.1.0
+    Get-AzNetworkInterface:                     https://docs.microsoft.com/en-us/powershell/module/az.network/get-aznetworkinterface?view=azps-6.1.0
+    Get-AzNetworkInterfaceIpConfig:             https://docs.microsoft.com/en-us/powershell/module/az.network/get-aznetworkinterfaceipconfig?view=azps-6.1.0
+    Get-AzVM:                                   https://docs.microsoft.com/en-us/powershell/module/az.compute/get-azvm?view=azps-6.1.0
+} #>
+<# Required Functions Links: {
+    None
+} #>
+<# Functions Description: {
+    ListAzLBBEPoolConfig:       Function to list existing load balancer back end pool configs
+} #>
+<# Variables: {      
+    :ListAzureLBBEPoolConfig    Outer loop for managing function
+    $ObjectList:                List of all load balanacers
+    $ObjectArray:               Array used to hold item info
+    $LBObject:                  Current load balancer
+    $PoolConfigList:            List of all back end pools on $LBObject
+    $ObjectInput:               $var used to load info into $ObjectArray
+    $BackEndAddresses:          Current item .backendaddress
+    $BackEndName:               Current back end address
+    $BackEndIPCon:              $BackEndName.NetworkInterfaceIpConfiguration.ID
+    $BackEndNic:                Nic where ipconfiguration.ID is $BackEndIPCon
+    $BackEndConfig:             Nic IP config where ID is $BackEndIPCon
+    $BackEndVM:                 VM where ID is $BackEndNic.VirtuualMachine
+} #>
+<# Process Flow {
+    function
+        Call ListAzLBBEPoolConfig > Get $null
+        End ListAzLBBEPoolConfig
+            Return function > Send $null
+}#>
+function ListAzLBBEPoolConfig {                                                             # Function to list existing load balancer back end pool configs
+    Begin {                                                                                 # Begin function
+        :ListAzureLBBEPoolConfig while ($true) {                                            # Outer loop to manage function
+            Write-Host 'Gathering load balancer info'                                       # Write message to screen
+            Write-Host 'This may take a moment'                                             # Write message to screen
+            $ObjectList = Get-AzLoadBalancer                                                # Generates the load balancer list
+            if (!$ObjectList) {                                                             # If $ObjectList is $null 
+                Clear-Host                                                                  # Clears screen
+                Write-Host 'No load balancers are in this subscription'                     # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break ListAzureLBBEPoolConfig                                               # Breaks ListAzureLBBEPoolConfig
+            }                                                                               # End if (!$ObjectList)
+            [System.Collections.ArrayList]$ObjectArray = @()                                # Creates the load balancer array
+            foreach ($_ in $ObjectList) {                                                   # For each item in $ObjectList
+                $LBObject = $_                                                              # Isolates the current item
+                $PoolConfigList = Get-AzLoadBalancerBackendAddressPool -LoadBalancer $_     # Gets a list of all back end pools on $LBObject
+                foreach ($_ in $PoolConfigList) {                                           # For each item in $PoolConfigList
+                    $ObjectInput = [PSCustomObject]@{                                       # Creates the item to loaded into array
+                        'Name'=$_.Name;'LBName'=$LBObject.Name;`
+                        'RGName'=$LBObject.ResourceGroupName;`
+                        'BackEndAddresses'=$_.LoadBalancerBackendAddresses                  # Gets current item info
+                    }                                                                       # End $ObjectInput = [PSCustomObject]@
+                    $ObjectArray.Add($ObjectInput) | Out-Null                               # Loads item into array
+                }                                                                           # End foreach ($_ in $PoolConfigList)
+            }                                                                               # End foreach ($_ in $ObjectList)
+            Clear-Host                                                                      # Clears screen
+            if (!$ObjectArray) {                                                            # If $ObjectArray is $null    
+                Write-Host 'No load balancer back end configs exist in this subscription'   # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break ListAzureLBBEPoolConfig                                               # Breaks :ListAzureLBBEPoolConfig
+            }                                                                               # End if (!$ObjectArray)
+            foreach ($_ in $ObjectArray) {                                                  # For each $_ in $ObjectArray
+                Write-Host 'Back End Pool: '$_.Name                                         # Write message to screen
+                Write-Host 'LB Name:       '$_.LBName                                       # Write message to screen
+                Write-Host 'LB RG:         '$_.RGName                                       # Write message to screen
+                if ($_.BackEndAddresses) {                                                  # If current item .BackEndAddresses has a value
+                    $BackEndAddresses = $_.BackEndAddresses                                 # Isolates the current item .BackEndAddresses
+                    foreach ($_ in $BackEndAddresses) {                                     # For each item in $BackEndAddresses
+                        $BackEndName = $_                                                   # Isolates the current item
+                        $BackEndIPCon = $BackEndName.NetworkInterfaceIpConfiguration.ID     # Isolates the back end IP configuration ID
+                        $BackEndNic = Get-AzNetworkInterface | Where-Object `
+                            {$_.IpConfigurations.ID -eq $BackEndIPCon}                      # Gets the back end NIC
+                        $BackEndConfig = Get-AzNetworkInterfaceIpConfig `
+                            -NetworkInterface $BackEndNic | `
+                            Where-Object {$_.ID -eq $BackEndIPCon}                          # Gets the config back end IP configuration
+                        $BackEndVM = Get-AzVM | Where-Object `
+                            {$_.ID -eq $BackEndNic.VirtualMachine.ID}                       # Gets the VM info
+                        Write-Host 'VM Name:       '$BackEndVM.Name                         # Write message to screen
+                        Write-Host 'IP Con Name:   '$BackEndConfig.Name                     # Write message to screen
+                        if ($BackEndConfig.PrivateIpAddress) {                              # If $BackEndConfig.PrivateIpAddress has a value
+                            Write-Host 'IP Con Priv IP:'$BackEndConfig.PrivateIpAddress     # Write message to screen
+                        }                                                                   # End if ($BackEndConfig.PrivateIpAddress)
+                        if ($BackEndConfig.PrivateIpAllocationMethod) {                     # If $BackEndConfig.PrivateIpAllocationMethod has a value
+                            $PrivAll = $BackEndConfig.PrivateIpAllocationMethod             # Isolates $BackEndConfig.PrivateIpAllocationMethod      
+                            Write-Host 'IP Con Priv IP:'$PrivAll                            # Write message to screen
+                            $PrivAll = $null                                                # Clears $var    
+                        }                                                                   # End if ($BackEndConfig.PrivateIpAllocationMethod)
+                        if ($BackEndConfig.PublicIpAddress) {                               # If $BackEndConfig.PublicIpAddress has a value
+                            Write-Host 'IP Con Priv IP:'$BackEndConfig.PublicIpAddress      # Write message to screen
+                        }                                                                   # End if ($BackEndConfig.PublicIpAddress)
+                        $BackEndName = $null                                                # Clears $var
+                        $BackEndIPCon = $null                                               # Clears $var
+                        $BackEndNic = $null                                                 # Clears $var
+                        $BackEndConfig = $null                                              # Clears $var
+                        $BackEndVM = $null                                                  # Clears $var
+                    }                                                                       # End foreach ($_ in $BackEndAddresses)
+                    $BackEndAddresses = $null                                               # Clears $var
+                }                                                                           # End if ($_.BackEndAddresses)
+                Write-Host ''                                                               # Write message to screen         
+            }                                                                               # End foreach ($_ in $ObjectArray)
+            Pause                                                                           # Pauses all actions for operator input
+            Break ListAzureLBBEPoolConfig                                                   # Breaks :ListAzureLBBEPoolConfig
+        }                                                                                   # End :ListAzureLBBEPoolConfig while ($true)
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End Begin
+}                                                                                           # End function ListAzLBBEPoolConfig
