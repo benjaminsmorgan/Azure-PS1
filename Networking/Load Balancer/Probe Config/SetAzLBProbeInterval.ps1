c6acf4bfd3677ab963e692c9fee8681a84b23562
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-07-17 07:43:27 -0400

    Added Out-Null to command

--- Networking/Load Balancer/Probe Config/SetAzLBProbeInterval.ps1
+++ Networking/Load Balancer/Probe Config/SetAzLBProbeInterval.ps1
@@ -89,7 +89,8 @@ function SetAzLBProbeInterval {                                                 
                     $LBProbeObject.NumberOfProbes -RequestPath $LBProbeObject.RequestPath `
                     -ErrorAction 'Stop' | Out-Null                                          # Updates the probe interval
                 Write-Host 'Saving the load balancer config'                                # Write message to screen
-                Set-AzLoadBalancer -LoadBalancer $LoadBalancerObject -ErrorAction 'Stop'    # Saves the updated load balancer configuration
+                Set-AzLoadBalancer -LoadBalancer $LoadBalancerObject -ErrorAction 'Stop' `
+                    | Out-Null                                                              # Saves the updated load balancer configuration
             }                                                                               # End Try
             Catch {                                                                         # If Try fails
                 Clear-Host                                                                  # Clears screen
@@ -108,105 +109,3 @@ function SetAzLBProbeInterval {                                                 
         Return $null                                                                        # Returns to calling function with $null
     }                                                                                       # End Begin
 }                                                                                           # End function SetAzLBProbeInterval
-function GetAzLBProbeConfig {                                                               # Function to get an existing load balancer probe config
-    Begin {                                                                                 # Begin function
-        :GetAzureLBProbeConfig while ($true) {                                              # Outer loop to manage function
-            Write-Host 'Gathering load balancer info'                                       # Write message to screen
-            Write-Host 'This may take a moment'                                             # Write message to screen
-            $ObjectList = Get-AzLoadBalancer                                                # Generates the load balancer list
-            if (!$ObjectList) {                                                             # If $ObjectList is $null 
-                Clear-Host                                                                  # Clears screen
-                Write-Host 'No load balancers are in this subscription'                     # Write message to screen
-                Write-Host ''                                                               # Write message to screen
-                Pause                                                                       # Pauses all actions for operator input
-                Break GetAzureLBProbeConfig                                                 # Breaks GetAzureLBProbeConfig
-            }                                                                               # End if (!$ObjectList)
-            $ObjectNumber = 1                                                               # Sets $ObjectNumber to 1
-            [System.Collections.ArrayList]$ObjectArray = @()                                # Creates the load balancer array
-            foreach ($_ in $ObjectList) {                                                   # For each item in $ObjectList
-                $LBObject = $_                                                              # Isolates the current item
-                $ProbeConfigList = Get-AzLoadBalancerProbeConfig -LoadBalancer $_           # Gets a list of all probes on $LBObject
-                foreach ($_ in $ProbeConfigList) {                                          # For each item in $ProbeConfigList
-                    $ObjectInput = [PSCustomObject]@{                                       # Creates the item to loaded into array
-                        'Number'=$ObjectNumber;                                             # Object number
-                        'Name'=$_.Name;                                                     # Rule name
-                        'LBName'=$LBObject.Name;                                            # Load balancer name
-                        'RGName'=$LBObject.ResourceGroupName;                               # Load balancer resource group
-                        'Proto'=$_.Protocol;                                                # Probe protocol
-                        'Port'=$_.Port;                                                     # Probe port
-                        'Int'=$_.IntervalInSeconds;                                         # Probe interval
-                        'Count'=$_.NumberOfProbes;                                          # Probe count
-                        'RPath'=$_.RequestPath;                                             # Request path
-                        'Rule'=$_.LoadBalancingRules.ID                                     # Probe rules if any
-                    }                                                                       # End $ObjectInput = [PSCustomObject]@
-                    $ObjectArray.Add($ObjectInput) | Out-Null                               # Loads item into array
-                    $ObjectNumber = $ObjectNumber + 1                                       # Increments $ObjectNumber by 1
-                }                                                                           # End foreach ($_ in $ProbeConfigList)
-            }                                                                               # End foreach ($_ in $ObjectList)
-            Clear-Host                                                                      # Clears screen
-            if (!$ObjectArray) {                                                            # If $ObjectArray is $null    
-                Write-Host 'No load balancer probe configs exist in this subscription'      # Write message to screen
-                Write-Host ''                                                               # Write message to screen
-                Pause                                                                       # Pauses all actions for operator input
-                Break GetAzureLBProbeConfig                                                 # Breaks :GetAzureLBProbeConfig
-            }                                                                               # End if (!$ObjectArray)
-            :SelectAzureLBProbeConfig while ($true) {                                       # Inner loop to select the load balancer probe config
-                Write-Host '[0]             Exit'                                           # Write message to screen
-                Write-Host ''                                                               # Write message to screen
-                foreach ($_ in $ObjectArray) {                                              # For each $_ in $ObjectArray
-                    $Number = $_.Number                                                     # Number is equal to current item .number
-                    if ($Number -le 9) {                                                    # If $Number is 9 or less
-                        Write-Host "[$Number]            "$_.Name                           # Write message to screen
-                    }                                                                       # End if ($Number -le 9)
-                    else {                                                                  # Else if $Number is greater than 9
-                        Write-Host "[$Number]           "$_.Name                            # Write message to screen
-                    }                                                                       # End else (if ($Number -le 9))
-                    Write-Host 'LB Name:       '$_.LBName                                   # Write message to screen
-                    Write-Host 'LB RG:         '$_.RGName                                   # Write message to screen
-                    Write-Host 'Probe Protocol:'$_.Proto                                    # Write message to screen
-                    Write-Host 'Probe Port:    '$_.Port                                     # Write message to screen
-                    Write-Host 'Probe Interval:'$_.Int                                      # Write message to screen
-                    Write-Host 'Probe Count:   '$_.Count                                    # Write message to screen
-                    if ($_.RPath) {                                                         # If current item .RPath has a value
-                        Write-Host 'Probe RPath:   '$_.Rpath                                # Write message to screen
-                    }                                                                       # End if ($_.RPath)
-                    else {                                                                  # Else if current item .RPath does not have value
-                        Write-Host 'Probe RPath:    N/A'                                    # Write message to screen         
-                    }                                                                       # End else (if ($_.RPath))
-                    Write-Host 'Rule count:    '$_.Rule.Count                               # Write message to screen
-                    Write-Host ''                                                           # Write message to screen         
-                }                                                                           # End foreach ($_ in $ObjectArray)
-                if ($CallingFunction) {                                                     # If $CallingFunction has a value
-                    Write-Host 'You are selecting the probe config for:'$CallingFunction    # Write message to screen
-                    Write-Host ''                                                           # Write message to screen
-                }                                                                           # End if ($CallingFunction)
-                $OpSelect = Read-Host 'Option [#]'                                          # Operator input to select the load balancer
-                if ($OpSelect -eq '0') {                                                    # If $OpSelect equals 0
-                    Break GetAzureLBProbeConfig                                             # Breaks :GetAzureLBProbeConfig
-                }                                                                           # End if ($OpSelect -eq '0')
-                elseif ($OpSelect -in $ObjectArray.Number) {                                # If $OpSelect in $ObjectArray.Number
-                    $OpSelect = $ObjectArray | Where-Object {$_.Number -eq $OpSelect}       # $OpSelect is equal to $ObjectArray where $ObjectArray.Number equals $OpSelect
-                    $LoadBalancerObject = Get-AzLoadBalancer -Name $OpSelect.LBName `
-                        -ResourceGroupName $OPSelect.RGName                                 # Pulls the full load balancer object
-                    $LBProbeObject = Get-AzLoadBalancerProbeConfig -LoadBalancer `
-                        $LoadBalancerObject -Name $OpSelect.Name                            # Pulls the load balancer probe config object
-                    Clear-Host                                                              # Clears screen
-                    if ($CallingFunction -eq 'AddAzLBRuleConfig') {                         # If $CallingFunction equals 'AddAzLBRuleConfig'
-                        Return $LBProbeObject                                               # Returns to calling function with $var    
-                    }                                                                       # End if ($CallingFunction -eq 'AddAzLBRuleConfig')
-                    else {                                                                  # Else if $CallingFunction does not equal 'AddAzLBRuleConfig'
-                        Return $LBProbeObject, $LoadBalancerObject                          # Returns to calling function with $vars
-                    }                                                                       # End else (if ($CallingFunction -eq 'AddAzLBRuleConfig'))
-                }                                                                           # End elseif ($OpSelect -in $ObjectArray.Number) 
-                else {                                                                      # All other inputs for $OpSelect
-                    Write-Host 'That was not a valid input'                                 # Write message to screen
-                    Write-Host ''                                                           # Write message to screen
-                    Pause                                                                   # Pauses all actions for operator input
-                    Clear-Host                                                              # Clears screen
-                }                                                                           # End else (if ($OpSelect -eq '0'))
-            }                                                                               # End :SelectAzureLBProbeConfig while ($true)
-        }                                                                                   # End :GetAzureLBProbeConfig while ($true)
-        Clear-Host                                                                          # Clears screen
-        Return $null                                                                        # Returns to calling function with $null
-    }                                                                                       # End Begin
-}                                                                                           # End function GetAzLBProbeConfig
