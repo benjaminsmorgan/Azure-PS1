c2af1685da3ac864254eee0ac555238d44e62384
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-07-24 06:16:53 -0400

    Create GetAzASG.ps1

---
+++ Networking/Application Security Groups/GetAzASG.ps1
@@ -0,0 +1,111 @@
+# Benjamin Morgan benjamin.s.morgan@outlook.com 
+<# Ref: { Mircosoft docs links
+    Get-AzApplicationSecurityGroup:             https://docs.microsoft.com/en-us/powershell/module/az.network/get-azapplicationsecuritygroup?view=azps-6.2.1
+    Get-AzNetworkInterface:                     https://docs.microsoft.com/en-us/powershell/module/az.network/get-aznetworkinterface?view=azps-6.2.1
+} #>
+<# Required Functions Links: {
+    None
+} #>
+<# Functions Description: {
+    GetAzASG:                   Function to get a application security group
+} #>
+<# Variables: {      
+    :GetAzureASG                Outer loop for managing function
+    :SelectAzureASG             Inner loop for selecting the application security group
+    $ObjectList:                List of all application security groups
+    $ObjectNumber:              $var used to list and select the application security group
+    $ObjectArray:               Array holding all application security group info
+    $ASGID:                     Current item .ID
+    $NICList:                   List of nics using $ASGID
+    $ObjectInput:               $var used to load info into $ObjectArray
+    $Number:                    Current item .number, used for formatting
+    $CallingFunction:           Name of the function that called this one
+    $OpSelect:                  Operator input to select the application security group
+    $ASGObject:                 application security group object
+} #>
+<# Process Flow {
+    function
+        Call GetAzASG > Get $ASGObject
+        End GetAzASG
+            Return function > Send $ASGObject
+}#>
+function GetAzASG {                                                                         # Function to get a application security group
+    Begin {                                                                                 # Begin function
+        :GetAzureASG while ($true) {                                                        # Outer loop for managing function
+            Write-Host 'Gathering application security group info'                          # Write message to screen
+            Write-Host 'This may take a moment'                                             # Write message to screen
+            $ObjectList = Get-AzApplicationSecurityGroup                                    # Gets a list of all application security groups
+            if (!$ObjectList) {                                                             # If $ObjectList is $null
+                Write-Host 'No security groups present in this subscription'                # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break GetAzureASG                                                           # Breaks :GetAzureASG
+            }                                                                               # End if (!$ObjectList)
+            $ObjectNumber = 1                                                               # Sets $ObjectNumber
+            [System.Collections.ArrayList]$ObjectArray = @()                                # Creates object list array
+            foreach ($_ in $ObjectList) {                                                   # For each item in $ObjectList
+                $ASGID = $_.ID                                                              # $ASGID is equal to current item .ID
+                $NICList = Get-AzNetworkInterface | Where-Object `
+                    {$_.IpConfigurations.ApplicationSecurityGroups.ID -eq $ASGID}           # Gets a list of nics in $ASGID
+                $ObjectInput = [PSCustomObject]@{                                           # custom object to add info to $ObjectArray
+                    'Number'=$ObjectNumber;                                                 # Object number
+                    'Name'=$_.Name;                                                         # Rule name
+                    'RG'=$_.ResourceGroupName;                                              # Rule resource group name
+                    'Location'=$_.Location;                                                 # Rule location
+                    'NICCount'=$NICList.Count                                               # Count of nic associated to the current application security group
+                }                                                                           # End $ObjectInput = [PSCustomObject]@
+                $ObjectArray.Add($ObjectInput) | Out-Null                                   # Adds $ObjectInput to $ObjectArray
+                $ObjectNumber = $ObjectNumber + 1                                           # Increments $ObjectNumber up by 1
+                $ASGID = $null                                                              # Clears $var
+                $NicList = $null                                                            # Clears $var
+            }                                                                               # End foreach ($_ in $ObjectList)
+            Clear-Host                                                                      # Clears screen
+            :SelectAzureASG while ($true) {                                                 # Inner loop for selecting the application security group
+                Write-Host '[0]              Exit'                                          # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                foreach ($_ in $ObjectArray) {                                              # For each item in $ObjectArray
+                    $Number = $_.Number                                                     # $Number is equal to current item .number
+                    if ($Number -le 9) {                                                    # If $Number is 9 or less
+                        Write-Host "[$Number]             "$_.Name                          # Write message to screen
+                    }                                                                       # End if ($Number -le 9)
+                    else  {                                                                 # Else if $Number is more than 9
+                        Write-Host "[$Number]            "$_.Name                           # Write message to screen
+                    }                                                                       # End else (if ($Number -le 9))
+                    Write-Host 'Rule RG:        '$_.RG                                      # Write message to screen
+                    Write-Host 'Rule Loc:       '$_.Location                                # Write message to screen
+                    if ($_.NicCount) {                                                      # If $NicCount has a value
+                        Write-Host 'Associated NICS:'$_.NICCount                            # Write message to screen
+                    }                                                                       # End if ($_.NicCount)
+                    else {                                                                  # Else if $NicCount is $null
+                        Write-Host 'Associated NICS: N/A'                                   # Write message to screen
+                    }                                                                       # End else (if ($_.NicCount))
+                    Write-Host ''                                                           # Write message to screen
+                }                                                                           # End foreach ($_ in $ObjectArray)
+                if ($CallingFunction) {                                                     # If $CallingFunction has a value
+                    Write-Host `
+                        'You are selecting the app security group for:'$CallingFunction     # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                }                                                                           # End if ($CallingFunction) 
+                $OpSelect = Read-Host 'Option [#]'                                          # Operator selection of the application security group
+                Clear-Host                                                                  # Clears screen
+                if ($OPSelect -eq '0') {                                                    # If $OpSelect equals '0'
+                    Break GetAzureASG                                                       # Breaks :GetAzureASG
+                }                                                                           # End if ($OPSelect -eq '0')
+                elseif ($OpSelect -in $ObjectArray.Number) {                                # Else if $OpSelect -in $ObjectArray.Number
+                    $OpSelect = $ObjectArray | Where-Object {$_.Number -eq $OpSelect}       # $OpSelect is $ObjectArray where $ObjectArray.Number equals $OpSelect
+                    $ASGObject = Get-AzApplicationSecurityGroup -Name $OpSelect.Name `
+                        -ResourceGroupName $OpSelect.RG                                     # Pulls the full application security group object
+                    Return $ASGObject                                                       # Returns to calling function with application security group object
+                }                                                                           # End elseif ($OpSelect -in $ObjectArray.Number)
+                else  {                                                                     # All other inputs for $OpSelect
+                    Write-Host 'That was not a valid input'                                 # Writes message to screen
+                    Write-Host ''                                                           # Writes message to screen
+                    Pause                                                                   # Pauses all actions for operator input
+                    Clear-Host                                                              # Clears screen
+                }                                                                           # End else (if ($OPSelect -eq '0'))
+            }                                                                               # End :SelectAzureASG while ($true)
+        }                                                                                   # End :GetAzureASG while ($true)
+        Clear-Host                                                                          # Clears screen
+        Return $null                                                                        # Returns to calling function with $null
+    }                                                                                       # End begin
+}                                                                                           # End function GetAzASG
