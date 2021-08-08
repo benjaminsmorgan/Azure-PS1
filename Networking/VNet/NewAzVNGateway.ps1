a0d08c1815887fbbcfd93c81db542d15f7cc3399
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-08-08 09:06:54 -0400

    Added gateway type selection

--- Networking/VNet/NewAzVNGateway.ps1
+++ Networking/VNet/NewAzVNGateway.ps1
@@ -186,9 +186,59 @@ function NewAzVNGateway {                                                       
                 Pause                                                                       # Pauses all actions for operator input
                 Break NewAzureVNGateway                                                     # Breaks :NewAzureVNGateway    
             }                                                                               # End if (!$SubnetObject)
+            :SetAzureGatewayType while ($true) {                                            # Inner loop for setting the gateway type
+                Write-Host 'Select the gateway type'                                        # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                Write-Host '[0]  Exit'                                                      # Write message to screen
+                Write-Host '[1]  Vpn'                                                       # Write message to screen
+                Write-Host '[2]  ExpressRoute'                                              # Write message to screen
+                Write-Host ''                                                               # Write message to screen
+                $OpSelect = Read-Host 'Option [#]'                                          # Operator input to select the gateway type
+                Clear-Host                                                                  # Clears screen
+                if ($Opselect -eq '0') {                                                    # If $OpSelect equals '0'  
+                    Break NewAzureVNGateway                                                 # Breaks :NewAzureVNGateway
+                }                                                                           # End if ($Opselect -eq '0')
+                elseif ($Opselect -eq '1') {                                                # Else if $OpSelect equals '1'
+                    $GatewayType = 'Vpn'                                                    # Sets the gateway type
+                }                                                                           # End elseif ($Opselect -eq '1')
+                elseif ($Opselect -eq '2') {                                                # Else if $OpSelect equals '2'
+                    $GatewayType = 'ExpressRoute'                                           # Sets the gateway type
+                }                                                                           # End elseif ($Opselect -eq '2')
+                else {                                                                      # All other inputs for $OpSelect 
+                    Write-Host 'That was not a valid input'                                 # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    Pause                                                                   # Pauses all actions for operator input
+                    Clear-Host                                                              # Clears screen
+                }                                                                           # End else (if ($Opselect -eq '0'))
+                if ($GatewayType) {                                                         # If $GatewayType has a value                    
+                    :Confirm while ($true) {                                                # Inner loop for confirming the gateway type
+                        Write-Host 'Use:'$GatewayType' as the the gateway type'             # Write message to screen
+                        Write-Host ''                                                       # Write message to screen
+                        $OpConfirm = Read-Host '[Y] Yes [N] No [E] Exit'                    # Operator confirmation of the type
+                        Clear-Host                                                          # Clears screen
+                        if ($OpConfirm -eq 'y') {                                           # If $OpConfirm equals 'y'
+                            Break SetAzureGatewayType                                       # Breaks :SetAzureGatewayType
+                        }                                                                   # End if ($OpConfirm -eq 'y')
+                        elseif ($OpConfirm -eq 'n') {                                       # Else if $OpConfirm equals 'n'
+                            Break Confirm                                                   # Breaks :Break
+                        }                                                                   # End elseif ($OpConfirm -eq 'n') 
+                        elseif ($OpConfirm -eq 'e') {                                       # Else if $OpConfirm equals 'e'
+                            Break NewAzureVNGateway                                         # Breaks :NewAzureVNGateway
+                        }                                                                   # End elseif ($OpConfirm -eq 'e')
+                        else {                                                              # All other inputs for $OpConfirm
+                            Write-Host 'That was not a valid input'                         # Write message to screen
+                            Write-Host ''                                                   # Write message to screen
+                            Pause                                                           # Pauses all actions for operator input
+                            Clear-Host                                                      # Clears screen
+                        }                                                                   # End else (if ($OpConfirm -eq 'y'))
+                    }                                                                       # End :Confirm while ($true)
+                }                                                                           # End if ($GatewayType)
+            }                                                                               # End :SetAzureGatewayType while ($true)   
             :SetAzureGatewaySku while ($true) {                                             # Inner loop for setting the gateway sku
                 Write-Host 'Select the gateway sku'                                         # Write message to screen
                 Write-Host ''                                                               # Write message to screen
+                $Url ='https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-about-vpn-gateway-settings#gwsku'
+                Write-Host $URL                                                             # Write message to screen
                 Write-Host '[0]  Exit'                                                      # Write message to screen
                 Write-Host '[1]  Basic'                                                     # Write message to screen
                 Write-Host '[2]  Standard'                                                  # Write message to screen
