39c9b646024c4cbb39a5c9c2725e2ff429a553c6
Benjamin Morgan <68668946+benjaminsmorgan@users.noreply.github.com>
2021-09-04 10:55:09 -0400

    Added current power state check

--- Compute/Azure VM/StartAzVM.ps1
+++ Compute/Azure VM/StartAzVM.ps1
@@ -1,29 +1,26 @@
 # Benjamin Morgan benjamin.s.morgan@outlook.com 
 <# Ref: { Microsoft docs links
-    Start-AzVM:                 https://docs.microsoft.com/en-us/powershell/module/az.compute/Start-azvm?view=azps-5.4.0
-    Get-AzVM:                   https://docs.microsoft.com/en-us/powershell/module/az.compute/get-azvm?view=azps-5.4.0
+    Start-AzVM:                                 https://docs.microsoft.com/en-us/powershell/module/az.compute/Start-azvm?view=azps-5.4.0
+    Get-AzVM:                                   https://docs.microsoft.com/en-us/powershell/module/az.compute/get-azvm?view=azps-5.4.0
 } #>
 <# Required Functions Links: {
     GetAzVM:                    https://github.com/benjaminsmorgan/Azure-Powershell/blob/main/Compute/Azure%20VM/GetAzVM.ps1
 } #>
 <# Functions Description: {
-    StartAzVM:                  Starts a VM object
-    GetAzVM:                    Gets a VM object
+    StartAzVM:                  Function to start a VM
+    GetAzVM:                    Function to get a VM
 } #>
 <# Variables: {
     :StartAzureVM               Outer loop for managing function
     $CallingFunction:           Name of this function or the one that called it
     $VMObject:                  Virtual machine object
+    $VMStatus:                  $VMObjects current power status
     $OpConfirm:                 Operator confirmation to Start the VM
+    $MSG:                       Last powershell error message
 } #>
 <# Process Flow {
-    Function
-        Call StartAzVM > Get $null
-            Call GetAzVM > Get $VMObject
-            End GetAzVMWin
-                Return StartAzVM > Send $VMObject
-        End StartAzVM
-            Return function > Send $null
+    StartAzVM
+        GetAzVM > Get $VMObject
 }#>
 function StartAzVM {                                                                        # Function to start a VM
     Begin {                                                                                 # Begin function
@@ -35,27 +32,54 @@ function StartAzVM {                                                            
             if (!$VMObject) {                                                               # If $VMObject is $null
                 Break StartAzureVM                                                          # Breaks :StartAzureVM
             }                                                                               # End if (!$VMObject)
+            $VMStatus = (Get-AzVM -Name $VMObject.Name -ResourceGroupName `
+                $VMObject.ResourceGroupName -Status).Statuses[1].Code                       # Pulls $VMObjects power state
+            if ($VMStatus -ne 'PowerState/deallocated') {                                   # If $VMStatus does not equal 'PowerState/deallocated'
+                Write-Host 'This VM is already powered on (Allocated)'                      # Write message ot screen
+                Write-Host ''                                                               # Write message ot screen
+                Write-Host 'No changes have been made'                                      # Write message ot screen
+                Write-Host ''                                                               # Write message ot screen
+                Pause                                                                       # Pauses all actions for operator input
+                Break StartAzureVM                                                          # Breaks :StartAzureVM        
+            }                                                                               # End if ($VMStatus -ne 'PowerState/deallocated')
             Write-Host 'Power on:'$VMObject.name                                            # Write message to screen
+            Write-Host ''                                                                   # Write message to screen
             $OpConfirm = Read-Host '[Y] Yes [N] No'                                         # Operator confirmation to turn on the VM
+            Clear-Host                                                                      # Clears screen
             if ($OpConfirm -eq 'y') {                                                       # If $OpConfirm equals 'y'
                 Try {                                                                       # Try the following
                     Write-Host 'Attempting to power on:'$VMObject.Name                      # Write message to screen
                     Start-AzVM -Name $VMObject.Name -ResourceGroup `
-                    $VMObject.ResourceGroupName -ErrorAction 'Stop'                         # Starts the selected VM
+                    $VMObject.ResourceGroupName -ErrorAction 'Stop' | Out-Null              # Starts the selected VM
                 }                                                                           # End try
                 Catch {                                                                     # If Try fails
-                    Write-Host 'An error occured while'                                     # Write message to screen
-                    Write-Host 'attempting to power on the vm'                              # Write message to screen
-                    Write-Host 'The VM may already be on'                                   # Write message to screen
+                    Clear-Host                                                              # Clears screen
+                    Write-Host 'An error has occured'                                       # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
+                    $MSG = $Error[0]                                                        # Gets the error message
+                    if ($MSG.Exception.InnerException.Body.Message) {                       # If $MSG.Exception.InnerException.Body.Message has a value             
+                        $MSG = $MSG.Exception.InnerException.Body.Message                   # Isolates the error message
+                        Write-Warning $MSG                                                  # Write message to screen
+                        Write-Host ''                                                       # Write message to screen    
+                    }                                                                       # End if ($MSG.Exception.InnerException.Body.Message)
+                    else {                                                                  # Else if $MSG.Exception.InnerException.Body.Message is $null
+                        Write-Warning $MSG                                                  # Write message to screen
+                        Write-Host ''                                                       # Write message to screen        
+                    }                                                                       # End else (if ($MSG.Exception.InnerException.Body.Message))
+                    Write-Host 'No changes have been made'                                  # Write message to screen
+                    Write-Host ''                                                           # Write message to screen
                     Pause                                                                   # Pauses all actions for operator input
                     Break StartAzureVM                                                      # Breaks :StartAzureVM
                 }                                                                           # End catch
+                Clear-Host                                                                  # Clears screen
                 Write-Host $VMObject.Name 'has been powered on'                             # Write message to screen
+                Write-Host ''                                                               # Write message to screen
                 Pause                                                                       # Pauses all actions for operator input
                 Break StartAzureVM                                                          # Breaks :StartAzureVM
             }                                                                               # End if ($OpConfirm -eq 'y')
             else {                                                                          # All other inputs for $OpConfirm
                 Write-Host 'No action taken'                                                # Write message to screen
+                Write-Host ''                                                               # Write message to screen
                 Pause                                                                       # Pauses all actions for operator input
                 Break StartAzureVM                                                          # Breaks :StartAzureVM
             }                                                                               # End else (if ($OpConfirm -eq 'y'))                                                                
